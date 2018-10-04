c82c65e52782ae7bc4f496e227844ae66a9c1503
Stephen Taylor <stephen.taylor.dev@gmail.com>
2018-10-04 05:39:35 -0400

    a few fixes to the TapCap module

--- apps/tapcap-manager/Service/service/StatusService.js
+++ apps/tapcap-manager/Service/service/StatusService.js
@@ -2,30 +2,29 @@
 
 
 /**
- * TapCap current status
- * User TapCap status
+ * TapCap history
+ * User TapCap history in the ranges provided
  *
- * request TapCapHistoryRequestSigned Purchase Request info
- * returns TapCapHistoryResponseSigned
+ * request MessageSigned Purchase Request info
+ * returns TapCapHistoryResponse
  **/
 exports.tapCapHistory = function(request) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-  "signature" : "signature",
-  "response" : {
-    "history" : [ {
-      "coinAmount" : 1.46581298050294517310021547018550336360931396484375,
-      "merchantId" : "merchantId",
-      "fiatAmount" : 6.027456183070403,
-      "timestamp" : 0.80082819046101150206595775671303272247314453125
-    }, {
-      "coinAmount" : 1.46581298050294517310021547018550336360931396484375,
-      "merchantId" : "merchantId",
-      "fiatAmount" : 6.027456183070403,
-      "timestamp" : 0.80082819046101150206595775671303272247314453125
-    } ]
-  }
+  "history" : [ {
+    "coinAmount" : 1.46581298050294517310021547018550336360931396484375,
+    "merchantId" : "merchantId",
+    "fiatAmount" : 6.027456183070403,
+    "coinBalance" : 5.962133916683182377482808078639209270477294921875,
+    "timestamp" : 0.80082819046101150206595775671303272247314453125
+  }, {
+    "coinAmount" : 1.46581298050294517310021547018550336360931396484375,
+    "merchantId" : "merchantId",
+    "fiatAmount" : 6.027456183070403,
+    "coinBalance" : 5.962133916683182377482808078639209270477294921875,
+    "timestamp" : 0.80082819046101150206595775671303272247314453125
+  } ]
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
@@ -40,7 +39,7 @@ exports.tapCapHistory = function(request) {
  * TapCap current status
  * User TapCap status
  *
- * request TapCapQueryRequestSigned TapCap status request
+ * request MessageSigned TapCap status request
  * returns TapCapQueryResponse
  **/
 exports.tapCapStatus = function(request) {
@@ -50,13 +49,7 @@ exports.tapCapStatus = function(request) {
   "balance" : 0.80082819046101150206595775671303272247314453125,
   "weeklyTopup" : 6.02745618307040320615897144307382404804229736328125,
   "token" : {
-    "signature" : "signature",
-    "token" : {
-      "clientAccount" : "clientAccount",
-      "transactionId" : 5.63737665663332876420099637471139430999755859375,
-      "availableBalance" : 5.962133916683182377482808078639209270477294921875,
-      "timestamp" : 2.3021358869347654518833223846741020679473876953125
-    }
+    "signature" : "signature"
   }
 };
     if (Object.keys(examples).length > 0) {
--- apps/tapcap-manager/Service/service/TransactionsService.js
+++ apps/tapcap-manager/Service/service/TransactionsService.js
@@ -1,41 +1,18 @@
 'use strict';
 
-var TapCapDB = require('../data/TapCapDB');
-var web3 = require('./web3')()
-
-//var stringify = require('fast-stable-stringify');
-
-// For localhost
 
 /**
  * Broker: Register new TapCap transaction
  *
- * request TapCapPurchaseBrokerSigned TapCap exchange request
+ * request MessageSigned TapCap exchange request
  * returns inline_response_200
  **/
-exports.tapCapBroker = function (request) {
-  return new Promise(function (resolve, reject) {
-    let purchaseReq = request.purchaseRequest;
-    let usersReq = purchaseReq.clientRequest.request;
-    let usersSig = purchaseReq.clientRequest.signature;
-
-    // First, get users account:
-    let userAccount = web3.eth.accounts.recover(usersReq, usersSig);
-
-    // verify sig with stored token
-    let tokenAccount = 
-
-    // First, check the users balance
-
-    
-    var usersAddress = 
-    // Confirm/update transaction
-    TapCapDB.GetLatestTransaction(userAddress);
-
+exports.tapCapBroker = function(request) {
+  return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-      "errorCode": 0
-    };
+  "errorCode" : 0
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -48,44 +25,15 @@ exports.tapCapBroker = function (request) {
 /**
  * Client: Confirm new TapCap transaction
  *
- * request TapCapTokenSigned TapCap status request
- * returns TapCapPurchaseFinalSigned
+ * request MessageSigned TapCap status request
+ * returns MessageSigned
  **/
-exports.tapCapClient = function (request) {
-  return new Promise(function (resolve, reject) {
-
-    // Confirm/update transaction
-    TapCapDB.GetLatestTransaction(userAddress);
-    // First, confirm current account status.
-
+exports.tapCapClient = function(request) {
+  return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-      "signature": "signature",
-      "brokerSigned": {
-        "signature": "signature",
-        "purchaseRequest": {
-          "cert": "cert",
-          "clientRequest": {
-            "request": {
-              "fiat": 0.80082819046101150206595775671303272247314453125,
-              "currencyCode": 6.02745618307040320615897144307382404804229736328125,
-              "timestamp": 1.46581298050294517310021547018550336360931396484375,
-              "token": {
-                "signature": "signature",
-                "token": {
-                  "clientAccount": "clientAccount",
-                  "transactionId": 5.63737665663332876420099637471139430999755859375,
-                  "availableBalance": 5.962133916683182377482808078639209270477294921875,
-                  "timestamp": 2.3021358869347654518833223846741020679473876953125
-                }
-              }
-            },
-            "signature": "signature"
-          },
-          "coin": 7.061401241503109105224211816675961017608642578125
-        }
-      }
-    };
+  "signature" : "signature"
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
