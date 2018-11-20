ac42f35445408fed7caa96ca7068521a9368780f
Stephen Taylor <stephen.taylor.dev@gmail.com>
2018-11-20 06:18:48 -0500

    update thecontract (works with latest ethers in TheBroker

--- js/TheContract.js
+++ js/TheContract.js
@@ -5,9 +5,15 @@ const { abi } = TheCoinSpec;
 const { address } = TheCoinSpec.networks[3];
 const ropsten = ethers.getDefaultProvider('ropsten');
 
-const TheContract = new ethers.Contract(address, abi, ropsten);
+let theContract = new ethers.Contract(address, abi, ropsten);
 
-export default TheContract;
+export function GetContract() { return theContract; };
+export function ConnectWallet(wallet) {
+	const provider = theContract.provider;
+	const connectedWallet = wallet.connect(provider);
+	theContract = theContract.connect(connectedWallet);
+	return connectedWallet;
+}
 export function ParseSignedMessage(signedMessage) {
 	return [
 		ethers.utils.verifyMessage(signedMessage.message, signedMessage.signature),
--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 import { Convert } from './Conversion';
-import TheContract from './TheContract';
 
 const toCoin = Convert.prototype.toCoin;
 const toHuman = Convert.prototype.toHuman;
 
-export { toCoin, toHuman, TheContract };
+export { toCoin, toHuman };
