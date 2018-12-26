d74892a4e8870cd77e5af736bdac335bc7cb34df
Stephen Taylor <stephen.taylor.dev@gmail.com>
2018-12-26 11:55:55 -0600

    Added intl for header alt messages

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { injectIntl } from 'react-intl';
 import * as styles from './index.module.css';
 import Banner from './headerSmall_static.svg';
 import Logo from './logo.svg';
+import messages from './messages';
 
-export default function Header() {
-  return (
-    <div>
-      <img className={styles.headerLogo} alt="The Coin Website" src={Logo} />
-      <img className={styles.headerImage} alt="" src={Banner} />
-    </div>
-  );
-}
+export default injectIntl(({ intl }) => (
+  <div>
+    <img
+      className={styles.headerLogo}
+      alt={intl.formatMessage(messages.LogoAlt)}
+      src={Logo}
+    />
+    <img
+      className={styles.headerImage}
+      alt={intl.formatMessage(messages.BannerAlt)}
+      src={Banner}
+    />
+  </div>
+));
--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -13,7 +13,6 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-/* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   public render() {
     return (
