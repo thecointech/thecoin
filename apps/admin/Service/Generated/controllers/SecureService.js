c1e07a653955ae38ea45294214820e1b5fbded68
Stephen Taylor <stephen.taylor.dev@gmail.com>
2019-07-01 07:03:38 -0400

    Adding endpoints for google intergration

--- apps/admin/Service/Generated/controllers/SecureService.js
+++ apps/admin/Service/Generated/controllers/SecureService.js
@@ -2,9 +2,34 @@
 
 
 /**
+ * Get the authorization URL to redirect the user to
+ *
+ * no response value expected for this operation
+ **/
+exports.googleAuthUrl = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Retrieve from google drive
+ *
+ * token  
+ * no response value expected for this operation
+ **/
+exports.googleRetrieve = function(token) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
  * Store on  google drive
  *
- * account  
+ * account GoogleUploadPacket 
  * no response value expected for this operation
  **/
 exports.googleStore = function(account) {
--- apps/admin/Service/src/controllers/Secure.js
+++ apps/admin/Service/src/controllers/Secure.js
@@ -3,6 +3,27 @@
 var utils = require('../utils/writer.js');
 var Secure = require('../service/SecureService');
 
+module.exports.googleAuthUrl = function googleAuthUrl (req, res, next) {
+  Secure.googleAuthUrl()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.googleRetrieve = function googleRetrieve (req, res, next) {
+  var token = req.swagger.params['token'].value;
+  Secure.googleRetrieve(token)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
 module.exports.googleStore = function googleStore (req, res, next) {
   var account = req.swagger.params['account'].value;
   Secure.googleStore(account)
