d8b70882bf15c854b44ae4da986d883a5da3efb3
Stephen Taylor <stephen.taylor.dev@gmail.com>
2019-07-28 00:23:03 -0400

    begin standardizing on spaces vs tabs

--- app/containers/Accounts/New/Wizard/Create/CloudStorage/index.tsx
+++ app/containers/Accounts/New/Wizard/Create/CloudStorage/index.tsx
@@ -6,35 +6,37 @@ import { PageProps } from '../PageProps';
 import messages from './messages';
 
 export const CloudStorageStep = () => (
-	<>
+  <>
     <Icon name="cloud upload" />
     <Step.Content>
       <Step.Title><FormattedMessage {...messages.stepHeader} /></Step.Title>
       <Step.Description><FormattedMessage {...messages.stepSubHeader} /></Step.Description>
     </Step.Content>
-	</>
+  </>
 );
 
-export const CloudStoragePage = (props: PageProps) => 
-{
-	const [complete, setComplete] = useState(false);
-	return (
-		<>
-		<Header as="h1">
-			<Header.Content>
-				<FormattedMessage {...messages.header} />
-			</Header.Content>
-			<Header.Subheader>
-				<FormattedMessage {...messages.subHeader} />
-			</Header.Subheader>
-		</Header>
-		<p>
-			<FormattedMessage {...messages.para1} />
-		</p>
-		<p>
-			<GoogleConnect accountName={"my account"} onComplete={() => setComplete(true)} />	
-		</p>
-		<Button disabled={!complete} onClick={props.onComplete}>{props.buttonText}</Button>
-	</>	
-	)
-}
+export const CloudStoragePage = (props: PageProps) => {
+  const [complete, setComplete] = useState(false);
+  const setCompleteCB = React.useCallback(() => {
+    setComplete(true);
+  }, []);
+  return (
+    <>
+    <Header as="h1">
+      <Header.Content>
+        <FormattedMessage {...messages.header} />
+      </Header.Content>
+      <Header.Subheader>
+        <FormattedMessage {...messages.subHeader} />
+      </Header.Subheader>
+    </Header>
+    <p>
+      <FormattedMessage {...messages.para1} />
+    </p>
+    <p>
+      <GoogleConnect accountName={'my account'} onComplete={setCompleteCB} />
+    </p>
+    <Button disabled={!complete} onClick={props.onComplete}>{props.buttonText}</Button>
+  </>
+  );
+};
--- app/containers/Accounts/New/Wizard/Create/OfflineStorage/messages.ts
+++ app/containers/Accounts/New/Wizard/Create/OfflineStorage/messages.ts
@@ -1,12 +1,12 @@
 /*
-	Offline Backup Storage Option
+  Offline Backup Storage Option
  */
 import { defineMessages } from 'react-intl';
 
 export const scope = 'app.containers.Create.Backup';
 
 export default defineMessages({
-	stepHeader: {
+  stepHeader: {
     id: `${scope}.stepHeader`,
     defaultMessage: 'Offline Storage',
   },
@@ -17,38 +17,36 @@ export default defineMessages({
   header: {
     id: `${scope}.header`,
     defaultMessage: 'Offline Storage',
-	},
-	subHeader: {
+  },
+  subHeader: {
     id: `${scope}.subHeader`,
     defaultMessage: 'Protect against accidental loss',
   },
   para1: {
     id: `${scope}.para1`,
     defaultMessage:
-			"It is very important to take precautions against accidental loss.",
-	},
-	para2: {
+      'It is very important to take precautions against accidental loss.',
+  },
+  para2: {
     id: `${scope}.para2`,
     defaultMessage:
-			"Just like your physical wallet, The Coin wallets are not automatically\
-			stored online.  This guarantees the bad guys can't get to them, but just\
-			like your physical wallet you want to take precautions against losing it."
+      'Just like your physical wallet, The Coin wallets are not automatically\
+      stored online.  This guarantees the bad guys can\'t get to them, but just\
+      like your physical wallet you want to take precautions against losing it.',
   },
   printButton: {
-		id: `${scope}.printButton`,
-    defaultMessage: "PRINT"
-	},
-	printMessage: {
+    id: `${scope}.printButton`,
+    defaultMessage: 'PRINT',
+  },
+  printMessage: {
     id: `${scope}.printMessage`,
     defaultMessage:
-      "Print your wallet and store it somewhere secure."
+      'Print your wallet and store it somewhere secure.',
   },
-  
-	downloadMessage: {
+
+  downloadMessage: {
     id: `${scope}.downloadMessage`,
     defaultMessage:
-      "Download your encrypted wallet for storage on a USB."
-	},
-
-
+      'Download your encrypted wallet for storage on a USB.',
+  },
 });
