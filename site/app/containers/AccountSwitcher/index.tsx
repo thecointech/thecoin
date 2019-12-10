94f191fc1c50e83a8449f3d0cf11d95d85847507
Marie Goursolas <goursolas.marie@gmail.com>
2019-12-10 18:06:33 -0500

    Arrange Dropdown User Account Menu

--- site/app/containers/AccountSwitcher/index.tsx
+++ site/app/containers/AccountSwitcher/index.tsx
@@ -5,6 +5,11 @@ import { selectActiveAccount } from "containers/Accounts/Selectors"
 import { selectAccounts } from "@the-coin/components/containers/Account/selector"
 import { DispatchAccounts } from "containers/Accounts/Reducer"
 import { useDispatch } from "react-redux"
+
+import cross from './images/cross.svg';
+import rectangle from './images/greenrectangle.svg';
+//import dot from './images/greendot.svg';
+
 // import { AccountMap } from "@the-coin/components/containers/Account/types"
 
 
@@ -25,21 +30,29 @@ export const AccountSwitcher = () => {
   }, [dispatch])
 
   return (
-    <Dropdown button text={activeAccount} id='createAccountHeader'>
+    <Dropdown button text='My Account' id='createAccountHeader' direction='left'>
       <Dropdown.Menu>
-        <Dropdown.Header>My Account</Dropdown.Header>
-        <Dropdown.Item text='Settings' description='' as={NavLink} to="/accounts/settings" />
-        <Dropdown.Item text='Sign Out' description='' as={NavLink} to="/accounts/signout" />
-        <Dropdown.Divider />
+        <Dropdown.Header>My Accounts</Dropdown.Header>
         {allNames
-          .filter(account => account != activeAccount)
-          .map(name => <Dropdown.Item key={name} text={name} account={name} description='' as={Link} onClick={doSetActive} to="/accounts/" />)
-        }
+            .filter(account => account == activeAccount)
+            .map(name => 
+              <Dropdown.Item>
+                <Dropdown text={name.substring(0, 15)+'...'}>
+                  <Dropdown.Menu direction='right'>
+                  <Dropdown.Item key={name} text='See' account={name} description='' as={Link} onClick={doSetActive} to="/accounts/" />
+                    <Dropdown.Item text='Settings' description='' as={NavLink} to="/accounts/settings" />
+                    <Dropdown.Item text='Sign Out' description='' as={NavLink} to="/accounts/signout" />
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Dropdown.Item>)
+          }
+        {allNames
+            .filter(account => account != activeAccount)
+            .map(name => <Dropdown.Item key={name} text={name} account={name} description='' as={Link} onClick={doSetActive} to="/accounts/" />)
+          }
+        <Dropdown.Item text='Add a New Account' description='' image={{ avatar: false, src: cross }} as={NavLink} to="/addAccount/generate/" />
         <Dropdown.Divider />
-        <Dropdown.Item text='Add Another Account' description='' as={NavLink} to="/addAccount" />
-        <Dropdown.Item text='Remove Accounts' description='' as={NavLink} to="/removeAccount" />
-        <Dropdown.Divider />
-        <Dropdown.Item text='Privacy Policy' />
+        <Dropdown.Item text='Remove Accounts' image={{ avatar: false, src: rectangle }} description='' as={NavLink} to="/removeAccount" />
       </Dropdown.Menu>
     </Dropdown>
   )
