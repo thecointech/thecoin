9fc62b87e692648b9b79c30143219c1b174854cc
Stephen Taylor <stephen.taylor.dev@gmail.com>
2019-12-26 07:04:34 -0500

    big move op to reorganize into libs/apps/apis

--- utilities/src/Conversion.hx
+++
@@ -1,8 +0,0 @@
-@:expose
-@:keep
-class Convert {
-        
-    // Utility fns
-    function toCoin(val:Float) { return Math.floor(val * 1000000); }
-    function toHuman(val:Int) { return val / 1000000; }
-}
