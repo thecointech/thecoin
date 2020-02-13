0fde67d7f5d9e8bcdfa6c05a2e8c689b2358692b
Stephen Taylor <stephen.taylor.dev@gmail.com>
2020-02-13 12:06:20 -0500

    Added additional error reporting to Web3 connections to provide more descriptive error messages for Opera users

--- libs/shared/src/containers/Account/Web3.ts
+++ libs/shared/src/containers/Account/Web3.ts
@@ -38,7 +38,10 @@ async function AddTheCoin(ethereum: any) {
 }
 
 export async function ConnectWeb3() {
+
   const type = getWeb3Type();
+  console.log(`Connecting to Web3: ${type}`);
+
   if (!type)
     throw new Error('Cannot connect');
 
@@ -49,8 +52,10 @@ export async function ConnectWeb3() {
     try {
       // Request account access if needed
       await ethereum.enable();
+      console.log("Ethereum Enabled");
       var provider = new ethers.providers.Web3Provider(web3.currentProvider);
       var signer = provider.getSigner();
+      console.log(`Connecting to signer: ${signer}`);
       if (type === 'Metamask') {
         // add The Coin to the list of tokens displayed by MetaMask
         await AddTheCoin(ethereum);
@@ -58,6 +63,7 @@ export async function ConnectWeb3() {
 
       // Our local/stored version remembers it's address
       var address = await signer.getAddress();
+      console.log(`Got Address: ${address}`);
       const ident: SignerIdent = {
         address,
         _isSigner: true
@@ -66,17 +72,39 @@ export async function ConnectWeb3() {
       return theSigner;
 
     } catch (error) {
+      NotifyUserOfError(error);
       // User denied account access...
-      //this.setState({userMessage: "Cannot connect: user cancelled"});
       return null;
     }
   }
   // Legacy dapp browsers...
   else if (web3) {
+    console.warn("This browser is running an unsupported version of Web3: please update to latest");
     //win.web3 = new Web3(web3.currentProvider);
     // Acccounts always exposed
     //	web3.eth.sendTransaction({/* ... */});
   }
   // Non-dapp browsers...
   return null;
+}
+
+function NotifyUserOfError(error: any) {
+  console.error("Cannot connect: " + JSON.stringify(error));
+  // Can we get more info out of this?
+  try {
+    const erJson = JSON.parse(error)
+    // Found by trial and error
+    if (erJson.error?.code === -32503) {
+      alert("Unable to Connect: Opera wallet is enabled but not connected\n\n(try removing and resetting current pairing)")
+    }
+    else if (erJson?.error?.message) {
+      // Opera may have more diagnostics in here
+      alert("Unable to Connect: \n\n" + erJson?.error?.message)
+    }
+    else {
+      alert("We couldn't connect to your account - we don't know why.  If this is an error, please contact support@thecoin.io");
+    }
+  }
+  finally
+  {}
 }
