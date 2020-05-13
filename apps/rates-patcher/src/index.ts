d964d49266aea3f07953bb92a046f4c362ff2f10
Stephen Taylor <stephen.taylor.dev@gmail.com>
2020-05-13 07:04:19 -0500

    Added bunyan logging Added proper logging to patcher so any issues with scheduled task will hopefully be visible

--- apps/rates-patcher/src/index.ts
+++ apps/rates-patcher/src/index.ts
@@ -8,6 +8,7 @@ import {NextOpenTimestamp} from '@the-coin/utilities/MarketStatus';
 import { ExchangeRate, FXRate, CoinUpdateInterval, RateOffsetFromMarket, FinnhubData, FinnhubRates, FinnhubFxQuotes } from './types';
 import Axios from 'axios';
 import {finnhub_key} from './secret.json';
+import { log } from './logging';
 
 var config = {
     keyFilename: '/src/TheCoin/apps/rates-patcher/thecoincore-212314-6f71b16407ed.json'
@@ -48,7 +49,8 @@ function getCoinRate(ts: number, data1m: FinnhubData, data5m?: FinnhubData) : Ex
             }
         }
     }
-    throw "Not Good:  " + ts;
+    log.fatal(`Could not find coin rate for: ${ts}`)
+    throw new Error("RateNotFound");
 }
 
 // function getFxRate(ts: number) : FXRate {
@@ -68,13 +70,13 @@ function getCoinRate(ts: number, data1m: FinnhubData, data5m?: FinnhubData) : Ex
 //     throw "Not Possible:  " + ts;
 // }
 
-async function getLatest<T>(ex: string, latestTs: number) : Promise<T>
-{
-    const k = datastore.key([ex, latestTs]);
-    const [resp] = await (datastore.get(k)) as T[];
-    console.log("Latest: " + JSON.stringify(resp))
-    return resp;
-}
+// async function getLatest<T>(ex: string, latestTs: number) : Promise<T>
+// {
+//     const k = datastore.key([ex, latestTs]);
+//     const [resp] = await (datastore.get(k)) as T[];
+//     log.trace("Latest: " + JSON.stringify(resp))
+//     return resp;
+// }
 
 async function setRate(kind: string, rate: ExchangeRate|FXRate) {
     const k = datastore.key([kind, rate.ValidUntil]);
@@ -82,35 +84,37 @@ async function setRate(kind: string, rate: ExchangeRate|FXRate) {
         key: k,
         data: rate
     })
-    console.log(r);
+    log.trace(r);
 
     await setLatest(kind, rate);
 }
 
 async function setLatest(kind: string, rate: ExchangeRate|FXRate) {
+    log.debug(`Setting latest for: ${kind}`);
     const k = datastore.key([kind, "latest"]);
     const r = await datastore.save({
         key: k,
         data: rate
     })
-    console.log(r);
+    log.trace(r);
 }
 
-async function fetchLatestCoin(resolution: string, from: number, to: number)
+async function fetchNewCoinRates(resolution: string, from: number, to: number)
 {
+    log.trace("Fetching SPX rates");
     const fromInt = Math.round(from / 1000);
     const toInt = Math.floor(to / 1000);
-    console.log("Please let me through");
     const url = `https://finnhub.io/api/v1/stock/candle?symbol=%5EGSPC&resolution=${resolution}&token=${finnhub_key}&from=${fromInt}&to=${toInt}`
-    console.log("Throug Yaya");
     var r = await Axios.get<FinnhubData>(url);
+    log.debug(`Fetched SPX rates: ${r?.statusText} - ${r?.data?.t?.length} results`);
     return r.data
 }
 
-async function fetchLatestFxRate()
+async function fetchNewFxRates()
 {
+    log.trace("Fetching FX rates");
     var r = await Axios.get<FinnhubRates>(`https://finnhub.io/api/v1/forex/rates?token=${finnhub_key}`);
-    
+    log.debug(`Fetched FX rates: ${r?.statusText} - ${r?.data?.base} base`);
     const {data} = r;
     const USD = data.quote["USD"];
     // Map to USD
@@ -121,37 +125,37 @@ async function fetchLatestFxRate()
     return data
 }
 
-async function fetchLastWorkingTS() : Promise<number>
+async function fetchLastUpdateTS() : Promise<number>
 {
     const k = datastore.key([0, "latest"]);
     const [data] = await datastore.get(k)
-    console.log("Updating from: " + new Date(data.ValidUntil));
     return data.ValidUntil;
 }
 
-async function quickstart() {
+async function update() {
 
-    console.log("Starting");
-    const latestTs = await fetchLastWorkingTS();
+    log.trace("Starting");
+    const latestTs = await fetchLastUpdateTS();
+    log.debug("Current value valid until " + new Date(latestTs));
     let now = Date.now();
 
     if (latestTs > now)
     {
-        console.log("Nothing to do: sleeping");
+        log.debug(`Nothing to do: ${new Date(now)} < ${new Date(latestTs)}`);
         return;
     }
 
-    const new1m = await fetchLatestCoin("1", latestTs - (90 * 1000), now);
-    //const new5m = await fetchLatestCoin("5");
-    const newFxRate = await fetchLatestFxRate();
+    const new1m = await fetchNewCoinRates("1", latestTs - (90 * 1000), now);
+    //const new5m = await fetchNewCoinRates("5");
+    const newFxRate = await fetchNewFxRates();
 
-    const latestCoin = await getLatest<ExchangeRate>("0", latestTs);
-    const latestCad = await getLatest<FXRate>("124", latestTs);
+    //const latestCoin = await getLatest<ExchangeRate>("0", latestTs);
+    //const latestCad = await getLatest<FXRate>("124", latestTs);
 
-    const latestBuy = latestCoin.Buy * latestCad.Rate;
-    const latestSell = latestCoin.Sell * latestCad.Rate;
+    //const latestBuy = latestCoin.Buy * latestCad.Rate;
+    //const latestSell = latestCoin.Sell * latestCad.Rate;
 
-    let validUntil = latestCoin.ValidUntil;
+    let validUntil = latestTs;
     while (validUntil < now)
     {
         const coinRate = getCoinRate(validUntil, new1m);
@@ -160,8 +164,8 @@ async function quickstart() {
         const buy = coinRate.Buy * fxRate;
         const sell = coinRate.Sell * fxRate;
     
-        console.log(`old: ${latestBuy} new: ${buy}`);
-        console.log(`old: ${latestSell} new: ${sell}`);
+        log.trace(`New CAD buy rate: ${buy}`);
+        log.trace(`New CAD sell rate: ${sell}`);
     
         // We modify the coin rate to always be the best version
         // compared to the old buy/sell
@@ -176,7 +180,7 @@ async function quickstart() {
     
         validUntil = validUntil + CoinUpdateInterval
         const nextOpen = await NextOpenTimestamp(new Date(validUntil), 90 * 1000)
-        console.log(`NextValid: ${new Date(validUntil)} -> ${new Date(nextOpen)}`);
+        log.debug(`NextValid: ${new Date(validUntil)} -> ${new Date(nextOpen)}`);
         validUntil = nextOpen;
         coinRate.ValidUntil = nextOpen;
         //fxRate.ValidUntil = nextOpen;
@@ -189,4 +193,4 @@ async function quickstart() {
         });
     }
 }
-quickstart();
+update();
