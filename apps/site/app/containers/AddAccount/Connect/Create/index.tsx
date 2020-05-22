0b4aee50515a07dbd9e1b58482c5a67f4f0204f6
Marie Goursolas <goursolas.marie@gmail.com>
2020-05-22 17:46:53 -0400

    Install 3Box and add to AddAccount

--- apps/site/app/containers/AddAccount/Connect/Create/index.tsx
+++ apps/site/app/containers/AddAccount/Connect/Create/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Connect } from "../../Connect"
 import { FormattedMessage, useIntl } from "react-intl"
 import messages from "./messages"
@@ -11,10 +11,35 @@ const BounceLink = () =>
     <FormattedMessage {...messages.createTransfer} />
   </Link>
 
+
+async function create3Box(){
+  console.log("-----create3Box")
+  const Box = require('3box')
+  const provider = await Box.get3idConnectProvider() // recomended provider
+  const box = await Box.openBox('0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658', provider)  
+  console.log(box)
+  //const space = await box.openSpace('TheCoin')
+  //console.log("-----SPACE",space)
+  //const profile = await Box.getProfile("0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658")
+  //console.log("-----PROFILE",profile)
+}
+
 export const Create = () => {
   const intl = useIntl();
   const web3Type = getWeb3Type();
+  const [count] = useState(0);
 
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+
+    async function get3BoxProfile() {
+      document.title = `You clicked ${count} times`;
+      await create3Box();
+    }
+    get3BoxProfile();
+
+  });
+  
   return (
     <Container id="formCreateAccountStep1">
       <Header as="h1">
