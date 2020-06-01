9c67c1edeeb64842d36d403b565cb8be64157201
Marie Goursolas <goursolas.marie@gmail.com>
2020-06-01 14:18:40 -0400

    WIP: Integration 3Box Profile

--- apps/site/app/containers/AddAccount/Connect/Create/index.tsx
+++ apps/site/app/containers/AddAccount/Connect/Create/index.tsx
@@ -13,15 +13,33 @@ const BounceLink = () =>
 
 
 async function create3Box(){
-  console.log("-----create3Box")
   const Box = require('3box')
   const provider = await Box.get3idConnectProvider() // recomended provider
   const box = await Box.openBox('0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658', provider)  
-  console.log(box)
-  //const space = await box.openSpace('TheCoin')
-  //console.log("-----SPACE",space)
+
+  //const IdentityWallet = require('identity-wallet')
+  //console.log(IdentityWallet)
+  const space = await box.openSpace('TheCoin')
+  await space.syncDone
+  //const spaceList = await Box.listSpaces('0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658')
+  //const spaceData = await Box.getSpace('0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658')
+  //console.log("-----SPACELIST",spaceData)
+  //await space.public.set('favorite-nft', '0x123...')
+  //await space.public.set('favorite-nft2', '0x124...')
+  //await space.public.set('favorite-nft3', '0x125...')
+  //const spaceData = await space.public.all()
+  //await space.private.set('item-to-buy', '0x123...')
+  //await space.private.set('number-to-buy', 22)
+  //await space.private.set('employees', '"employees":[{"firstName":"John", "lastName":"Doe"},{"firstName":"Anna", "lastName":"Smith"},{"firstName":"Peter", "lastName":"Jones"}]')
+  //await space.private.set('item-to-buy[1]', "<div>content</div>")
+  //const spacePrivateData = await space.private.all()
+
+  //console.log("-----SPACEDATA",spaceData,spacePrivateData)
+
   //const profile = await Box.getProfile("0xf3B7C73bec2B9A0Af7EEA1fe2f76973D6FBfE658")
-  //console.log("-----PROFILE",profile)
+  await box.public.set('name', 'Test')
+  const profilePublic = await box.public.all()
+  console.log("-----PROFILE",profilePublic)
 }
 
 export const Create = () => {
@@ -29,16 +47,14 @@ export const Create = () => {
   const web3Type = getWeb3Type();
   const [count] = useState(0);
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
+  //useEffect(() => {
 
-    async function get3BoxProfile() {
-      document.title = `You clicked ${count} times`;
-      await create3Box();
-    }
-    get3BoxProfile();
+    //async function get3BoxProfile() {
+      //await create3Box();
+    //}
+    //get3BoxProfile();
 
-  });
+  //});
   
   return (
     <Container id="formCreateAccountStep1">
