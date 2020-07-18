713699253309aca33d471e3b00ee6ec7b8c93187
Stephen Taylor <stephen.taylor.dev@gmail.com>
2020-07-18 17:04:55 -0500

    Finally pushed in rest of the Timestamps fix

--- libs/utils-ts/src/firestore/release.ts
+++ libs/utils-ts/src/firestore/release.ts
@@ -1,7 +1,10 @@
-import { Firestore } from '@google-cloud/firestore';
+import { Firestore, Timestamp as TimestampRelease } from '@google-cloud/firestore';
 import { SetFirestore } from './firestore';
+import { Timestamp } from './timestamp';
 
 export async function init() {
   const db = new Firestore();
   SetFirestore(db);
+
+  Timestamp.init(TimestampRelease);
 }
--- libs/utils-ts/src/firestore/server.ts
+++ libs/utils-ts/src/firestore/server.ts
@@ -1,9 +1,11 @@
-import { Firestore } from '@google-cloud/firestore';
+import { Firestore, Timestamp as TimestampServer } from '@google-cloud/firestore';
 import { SetFirestore } from './firestore';
-export { Timestamp } from '@google-cloud/firestore';
+import { Timestamp } from './timestamp';
 // Create a new client
 
 export async function init() {
   const db = new Firestore();
   SetFirestore(db);
+
+  Timestamp.init(TimestampServer);
 }
