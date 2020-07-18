652b5b3f795755618a37edb8951c923275584b44
Marie Goursolas <goursolas.marie@gmail.com>
2020-07-17 21:01:45 -0400

    First Part of Home Page

--- apps/site/app/containers/HomePage/index.tsx
+++ apps/site/app/containers/HomePage/index.tsx
@@ -13,58 +13,39 @@ import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Button } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom';
-import phone from './images/phoneAndMoney.svg';
-import interac from './images/interacLogo.svg';
 
 import { Subscribe } from '../Subscribe';
 import styles from './index.module.css';
-import { hasAccount } from '@the-coin/shared/utils/detection';
-import { useAccounts } from '@the-coin/shared/containers/AccountMap';
+import landscapeSkyCloudsHigh from './images/1landscapeGreen/cloudsBackgroundHigh.svg';
+import landscapeSkyCloudsLeft from './images/1landscapeGreen/cloudsLeft.svg';
+import landscapeSkyCloudsRight from './images/1landscapeGreen/cloudsRight.svg';
+import landscapeGreenPart from './images/1landscapeGreen/landscape.svg'
 
 
 export const HomePage = () => {
-  //const multicodec = require('multicodec');
-  //const Box = require('3box');
-  //const IdentityWallet = require('identity-wallet')
 
-  const accounts = useAccounts();
-  const userHasAccount = hasAccount(accounts);
-  const accountButtonText = userHasAccount ? "My Accounts" : "Get Started";
   return (
     <React.Fragment>
-      <h2 className={styles.h2Home}>
-        Combining all the benefits of <br />Investment &amp; Spending
-      </h2>
+      <h1 className={styles.h1Home}>The future is brighter</h1>
+
       <Grid divided="vertically">
         <Grid.Row columns={2} id='homeZone' className={styles.mainWrapper}>
           <Grid.Column id={styles.infosZone}>
             <div className={styles.headingWrapper}>
-              <ol className={styles.listHome}>
-                <li>Pay Bills and e-Transfer with &nbsp;&nbsp;
-                  <img
-                    className={styles.interac}
-                    alt="interac"
-                    src={interac}
-                  /></li>
-                <li>Grow your money (average <b>rate of 9.8%*</b>) </li>
-                <li>Pay <b>No Bank Fees</b></li>
-              </ol>
-              <p>No engagement, no obligations.</p>
-              <p>*Based on historical returns.**</p>
-              <p>**We can't guarantee the future, all anyone can do is look at history to see what is likely.</p>
-
-              <Button as={NavLink} to="/howItWorks" content='More Info' primary size='massive' id='knowMore' />
-              <Button as={NavLink} to={"/accounts"} content={accountButtonText} secondary size='massive' id='createAccount' />
+              
+              <p>Save, invest and spend money with TheCoin, get 100% of benefits and save our Planet.</p>
+              <Button as={NavLink} to="/Accounts" content='Start Now' primary size='massive' id='knowMore' />
             </div>
-          </Grid.Column>
-          <Grid.Column className={styles.phoneApp} id={styles.phoneZone}>
-            <img
-              alt="mobile interac"
-              src={phone}
-            />
           </Grid.Column>
         </Grid.Row>
       </Grid>
+      <div className={styles.landscape}>
+        <img className={styles.cloudsHigh} src={landscapeSkyCloudsHigh} />
+        <img className={styles.cloudsLeft} src={landscapeSkyCloudsLeft} />
+        <img className={styles.cloudsRight} src={landscapeSkyCloudsRight} />
+        <img className={styles.greenery} src={landscapeGreenPart} />
+      </div>
+
       <Subscribe />
     </React.Fragment>
   );
