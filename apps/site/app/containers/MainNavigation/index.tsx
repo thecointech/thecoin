ac4e79cf45ce3e4a2562e45ee3e227ae100fc04c
Marie Goursolas <goursolas.marie@gmail.com>
2020-08-12 20:02:15 -0400

    Menu for mobile

--- apps/site/app/containers/MainNavigation/index.tsx
+++ apps/site/app/containers/MainNavigation/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Container } from 'semantic-ui-react';
+import { Menu, Container, Responsive, Segment } from 'semantic-ui-react';
 import HeaderLink from '../../components/HeaderLink';
 import styles from './index.module.css';
 import { AccountSwitcher } from 'containers/AccountSwitcher';
@@ -8,37 +8,57 @@ import { FormattedMessage } from 'react-intl';
 class Navigation extends React.Component {
   render() {
     return (
-      <div className={styles.navContainer}>
-        <Container>
-          <Menu secondary className={styles.mainMenu} id="mainMenu">
-            <HeaderLink to="/" exact>
-              <FormattedMessage id="site.MainNavigation.home"
-                defaultMessage="Home"
-                description="Title for the Home entry in the menu"
-                values={{ what: 'react-intl' }}/>
-            </HeaderLink>
-            <HeaderLink to="/howItWorks">
-              <FormattedMessage id="site.MainNavigation.indepth"
-                defaultMessage="In-depth"
-                description="Title for the In-depth entry in the menu"
-                values={{ what: 'react-intl' }}/>
-            </HeaderLink>
-            <HeaderLink to="/FAQ">
-              <FormattedMessage id="site.MainNavigation.wedomore"
-                defaultMessage="We do more"
-                description="Title for the We do more entry in the menu"
-                values={{ what: 'react-intl' }}/>
-            </HeaderLink>
-            <HeaderLink to="/FAQ">
-              <FormattedMessage id="site.MainNavigation.yourbenefits"
-                defaultMessage="Your benefits"
-                description="Title for the Your benefits entry in the menu"
-                values={{ what: 'react-intl' }}/>
-            </HeaderLink>
-          </Menu>
-          <AccountSwitcher />
-        </Container>   
-      </div>
+      <Container>
+      <Responsive as={Segment} {...Responsive.onlyComputer}>
+          <div className={styles.navContainer} id="mainMenuContainer">
+              <Menu secondary className={styles.mainMenu} >
+                  <HeaderLink to="/" exact>
+                    <FormattedMessage id="site.MainNavigation.home"
+                      defaultMessage="Home"
+                      description="Title for the Home entry in the menu"
+                      values={{ what: 'react-intl' }}/>
+                  </HeaderLink>
+                  <HeaderLink to="/howItWorks">
+                    <FormattedMessage id="site.MainNavigation.indepth"
+                      defaultMessage="In-depth"
+                      description="Title for the In-depth entry in the menu"
+                      values={{ what: 'react-intl' }}/>
+                  </HeaderLink>
+                  <HeaderLink to="/FAQ">
+                    <FormattedMessage id="site.MainNavigation.wedomore"
+                      defaultMessage="We do more"
+                      description="Title for the We do more entry in the menu"
+                      values={{ what: 'react-intl' }}/>
+                  </HeaderLink>
+                  <HeaderLink to="/FAQ">
+                    <FormattedMessage id="site.MainNavigation.yourbenefits"
+                      defaultMessage="Your benefits"
+                      description="Title for the Your benefits entry in the menu"
+                      values={{ what: 'react-intl' }}/>
+                  </HeaderLink>
+
+                <Responsive as={Segment} {...Responsive.onlyMobile}>
+
+                </Responsive> 
+              </Menu>
+              <AccountSwitcher />
+          </div>
+
+          </Responsive>
+          <Responsive as={Segment} {...Responsive.onlyMobile}>
+            <div className={styles.navContainer} id="mainMenuContainer">
+                <Menu secondary className={styles.mainMenu} >
+                    <HeaderLink to="/FAQ">
+                      <FormattedMessage id="site.MainNavigation.yourbenefits"
+                        defaultMessage="Your benefits"
+                        description="Title for the Your benefits entry in the menu"
+                        values={{ what: 'react-intl' }}/>
+                    </HeaderLink>
+                </Menu>
+                <AccountSwitcher />
+              </div>
+          </Responsive> 
+      </Container>   
     );
   }
 }
