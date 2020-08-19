6a416da98b2a0eb164e221413cfe1ddc465e77c9
Marie Goursolas <goursolas.marie@gmail.com>
2020-08-19 19:47:33 -0400

    Page healthier begin

--- apps/site/app/containers/HowItWorks/index.tsx
+++ apps/site/app/containers/HowItWorks/index.tsx
@@ -7,104 +7,47 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Grid, Header } from 'semantic-ui-react';
-import messages from './messages';
-import check from './images/check.svg';
-import exchange from './images/exchange.svg';
-import moneyhand from './images/moneyhand.svg';
-import save from './images/save.svg';
-import user from './images/user.svg';
 
 import styles from './styles.module.css';
 
 export function HowItWorks() {
   return (
     <div className={styles.wrapper}>
-      <Grid className={styles.center}>
-        <Header as="h1">
+      <Grid className={styles.content} columns='equal' textAlign='left' verticalAlign='middle' stackable>
+        <Header as="h1" className={styles.center}>
           <Header.Content>
-            <div className={styles.h1}>
-              <FormattedMessage {...messages.header} />
-            </div>
+            Earth’s Healthier
           </Header.Content>
-          <Header.Subheader>
-            <div className={styles.h2}>
-              <FormattedMessage {...messages.subHeader} />
-            </div>
-          </Header.Subheader>
         </Header>
+          <p className={styles.center}>
+            Our non-profits mission is to fully use a neglected resource to fight climate change - your bank account.
+          </p>
+          <Grid.Row>
+            <Grid.Column>
+              <h2>$1000</h2><br />
+              <FormattedMessage id="site.healthier.majorbanks"
+                  defaultMessage="The major banks take an average of $1000 in profits each year from every Canadian."
+                  description="The major banks take an average of $1000 in profits each year from every Canadian."/>
+              <hr align="left"/>
+              <h2>90%</h2>
+              <FormattedMessage id="site.healthier.invest"
+                  defaultMessage="We invest our clients’ accounts and give them 90% of the profit."
+                  description="We invest our clients’ accounts and give them 90% of the profit."/>
+            </Grid.Column>
 
-        <Grid.Row className={styles.center}>
-          <Grid.Column width={1}>
-            <img className={styles.icons} src={user} />
-          </Grid.Column>
-          <Grid.Column width={8} className={styles.step1Wrap}>
-            <div className={styles.step1}>
-              <FormattedMessage {...messages.step1} />
-            </div>
-            <div className={styles.step1Sub}>
-              <FormattedMessage {...messages.step1Sub} />
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row className={styles.center}>
-          <Grid.Column width={1}>
-            <img className={styles.icons} src={save} />
-          </Grid.Column>
-          <Grid.Column width={8} className={styles.step1Wrap}>
-            <div className={styles.step1}>
-              <FormattedMessage {...messages.step2} />
-            </div>
-            <div className={styles.step1Sub}>
-              <FormattedMessage {...messages.step2Sub} />
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row className={styles.center}>
-          <Grid.Column width={1}>
-            <img className={styles.icons} src={exchange} />
-          </Grid.Column>
-          <Grid.Column width={8} className={styles.step1Wrap}>
-            <div className={styles.step1}>
-              <FormattedMessage {...messages.step3} />
-            </div>
-            <div className={styles.step1Sub}>
-              <FormattedMessage {...messages.step3Sub} />
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row className={styles.center}>
-          <Grid.Column width={1}>
-            <img className={styles.icons} src={check} />
-          </Grid.Column>
-          <Grid.Column width={8} className={styles.step1Wrap}>
-            <div className={styles.step1}>
-              <FormattedMessage {...messages.step4} />
-            </div>
-            <div className={styles.step1Sub}>
-              <FormattedMessage {...messages.step4Sub} />
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row className={styles.center}>
-          <Grid.Column width={1}>
-            <img className={styles.icons} src={moneyhand} />
-          </Grid.Column>
-          <Grid.Column width={8} className={styles.step1Wrap}>
-            <div className={styles.step1}>
-              <FormattedMessage {...messages.step5} />
-            </div>
-            <div className={styles.step1Sub}>
-              <FormattedMessage {...messages.step5Sub} />
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-      <Grid verticalAlign="middle" className={styles.wrapper}></Grid>
+            <Grid.Column>
+              <h2>$100</h2><br />
+              <FormattedMessage id="site.healthier.lifestyle"
+                  defaultMessage="It only costs about $100 per person to offset the CO2 for our current lifestyle."
+                  description="It only costs about $100 per person to offset the CO2 for our current lifestyle."/>
+              <hr align="left"/>
+              <h2>1/10</h2>
+              <FormattedMessage id="site.healthier.carbon"
+                  defaultMessage="The remaining 1/10th is used to pay off our carbon debt"
+                  description="The remaining 1/10th is used to pay off our carbon debt"/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
     </div>
   );
 }
