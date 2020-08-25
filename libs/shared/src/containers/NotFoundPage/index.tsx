bf821b97ce8ade3f0b8a35a752f64a53b4d4bbe8
Marie Goursolas <goursolas.marie@gmail.com>
2020-08-25 19:39:20 -0400

    404 page

--- libs/shared/src/containers/NotFoundPage/index.tsx
+++ libs/shared/src/containers/NotFoundPage/index.tsx
@@ -6,15 +6,29 @@
 
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
+import { Button, Header } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import illustration from './images/illust_pagenotfound.svg'
 
 export function NotFoundPage() {
   return (
-    <article>
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    </article>
+    <>
+      <Header as='h5'>
+        <FormattedMessage id="site.NotFoundPage.aboveTheTitle"
+                        defaultMessage="PAGE NOT FOUND"
+                        description="Text above the title for the 404 page"/>
+      </Header>
+      <Header as='h3'>
+        <FormattedMessage id="site.NotFoundPage.title"
+                        defaultMessage="Sorry, we couldn’t find the page you are looking for"
+                        description="Title for the 404 page"/>
+      </Header>
+      <Button as={NavLink} to="/" content='' primary size='massive' >
+          <FormattedMessage id="site.NotFoundPage.button"
+              defaultMessage="Home"
+              description="Go back to homepage button for the 404 page"/>
+      </Button>
+      <img src={illustration} />
+    </>
   );
 }
