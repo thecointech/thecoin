6be103f3bef9c77f96f19d0d96cda56d070629f6
Marie Goursolas <goursolas.marie@gmail.com>
2020-09-15 18:25:37 -0400

    Remove the ast option to get our format

--- apps/site/internals/scripts/format-intl.js
+++ apps/site/internals/scripts/format-intl.js
@@ -1,10 +1,6 @@
 /**
  * This script will format the internationalization messages from yarn extract.
  */
-type CompileFn = <T = Record<string, MessageDescriptor>>(
-  msgs: T
-) => Record<string, string>
-
 exports.compile = function (msgs) {
   const results = {}
   for (const [id, msg] of Object.entries(msgs)) {
