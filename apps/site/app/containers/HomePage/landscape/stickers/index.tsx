d45c15dd9900f5d86297b9e46fc0c43e00accacd
MarieTheCoin <marie@thecoin.io>
2020-10-08 12:05:39 -0400

    Fix missing css file

--- apps/site/app/containers/HomePage/landscape/stickers/index.tsx
+++ apps/site/app/containers/HomePage/landscape/stickers/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
 import { FormattedMessage } from 'react-intl';
 
-import styles from './index.module.css';
+import styles from './styles.module.css';
 
 export const Stickers = () => {
   return (
