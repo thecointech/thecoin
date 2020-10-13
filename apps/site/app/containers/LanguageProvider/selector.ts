56fd398efe27d03735436e58595f7896490c67f8
MarieTheCoin <marie@thecoin.io>
2020-10-13 18:27:07 -0400

    Clean Selector

--- apps/site/app/containers/LanguageProvider/selector.ts
+++ apps/site/app/containers/LanguageProvider/selector.ts
@@ -1,25 +1,10 @@
-import { createSelector, createStructuredSelector } from 'reselect';
 import { ApplicationRootState } from 'types';
 import { initialState } from './reducer';
-import { ContentState } from './types';
 
 /**
- * Direct selector to the languageToggle state domain ////Test
+ * Direct selector to the languageToggle state domain
  */
 const selectLocale = (state: ApplicationRootState) =>
   state.language ? state.language : initialState;
 
-/**
- * Select the language locale
- */
-const makeSelectLocale = () =>
-createSelector(selectLocale, languageState => languageState.locale);
-
-
-// Map RootState to your StateProps
-const mapStateToProps = createStructuredSelector<ApplicationRootState, ContentState>({
-    // All the keys and values are type-safe
-    locale: makeSelectLocale(),
-});
-
-export { selectLocale, makeSelectLocale, mapStateToProps };
+export { selectLocale };
