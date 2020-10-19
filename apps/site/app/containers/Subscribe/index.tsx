d85e123c35d39eafa170a74693e1dbfd8bd9d012
MarieTheCoin <marie@thecoin.io>
2020-10-19 19:54:38 -0400

    Change names to be easier to read

--- apps/site/app/containers/Subscribe/index.tsx
+++ apps/site/app/containers/Subscribe/index.tsx
@@ -7,9 +7,9 @@ import { FormattedMessage } from 'react-intl';
 
 
 export const Subscribe = () => {
-  const [errorinfos, setErrorinfos] = useState(true);
-  const [confirminfos, setConfirminfos] = useState(true);
-  const [validinfos, setValidinfos] = useState(true);
+  const [errorInfos, setErrorInfos] = useState(true);
+  const [confirmInfos, setConfirmInfos] = useState(true);
+  const [validInfos, setValidInfos] = useState(true);
  
   const [email, setEmail] = React.useState('');
   const onInputChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.currentTarget.value), [setEmail]);
@@ -17,9 +17,9 @@ export const Subscribe = () => {
     e?.preventDefault();
 
     if (email.indexOf('@') < 0) {
-      setValidinfos(false);
-      setErrorinfos(true);
-      setConfirminfos(true);
+      setValidInfos(false);
+      setErrorInfos(true);
+      setConfirmInfos(true);
     } else {
       const api = GetNewsletterApi();
       const result = await api.newsletterSignup({
@@ -27,13 +27,13 @@ export const Subscribe = () => {
         confirmed: false,
       }) as any;
       if (!result.success) {
-        setValidinfos(true);
-        setErrorinfos(false);
-        setConfirminfos(true);
+        setValidInfos(true);
+        setErrorInfos(false);
+        setConfirmInfos(true);
       } else {
-        setValidinfos(true);
-        setErrorinfos(true);
-        setConfirminfos(false);
+        setValidInfos(true);
+        setErrorInfos(true);
+        setConfirmInfos(false);
       }
     }
   }, [email]);
@@ -48,17 +48,17 @@ export const Subscribe = () => {
       </span>
       <span className={styles.search}>
         <div>
-          <Message color='orange' hidden={validinfos}>
+          <Message color='orange' hidden={validInfos}>
             <FormattedMessage id="site.subscribe.email.invalid" 
                                     defaultMessage="Please enter a valid email" 
                                     description="Message we give a user when the subscription failed" />
           </Message>
-          <Message color='red' hidden={errorinfos}>
+          <Message color='red' hidden={errorInfos}>
             <FormattedMessage id="site.subscribe.email.error" 
                                     defaultMessage="Signup failed: please contact support@thecoin.io" 
                                     description="Message we give a user when the subscription failed (already subscribed or server)" />
           </Message>
-          <Message color='olive' hidden={confirminfos}>
+          <Message color='olive' hidden={confirmInfos}>
             <FormattedMessage id="site.subscribe.email.success" 
                                     defaultMessage="Success: check your emails" 
                                     description="Message we give a user when the subscription is a success" />
