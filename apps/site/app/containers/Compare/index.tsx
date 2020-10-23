d1faf0f6f646674a2ba22bec8d65f03e0fa8df95
MarieTheCoin <marie@thecoin.io>
2020-10-22 20:39:03 -0400

    Style that graph page

--- apps/site/app/containers/Compare/index.tsx
+++ apps/site/app/containers/Compare/index.tsx
@@ -3,8 +3,8 @@
  */
 
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { Form, Grid, Header, InputOnChangeData } from 'semantic-ui-react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Form, Grid, Header, InputOnChangeData, Button } from 'semantic-ui-react';
 import { CreateAccountBanner } from 'containers/CreateAccountBanner';
 import styles from './styles.module.css';
 import { useState } from 'react';
@@ -22,46 +22,86 @@ export function Compare() {
       setDuration(data.value);
     }
   }
+
+  let intl = useIntl();
+  const labelStartingValue = intl.formatMessage({ id: 'site.compare.label.rangeStarting', defaultMessage:'Starting value:'});
+  const labelStartingValueCurrency = intl.formatMessage({ id: 'site.compare.label.rangeStartingCurrency', defaultMessage:'CA$'});
+  const labelDurationValue = intl.formatMessage({ id: 'site.compare.label.rangeDuration', defaultMessage:'Duration:'});
+  const labelDurationYear = intl.formatMessage({ id: 'site.compare.label.rangeDurationYear', defaultMessage:'Years'});
+
+
   return (
       <>
-        <div className={styles.wrapper} id={styles.wedomore}>
+        <div className={styles.wrapper}>
             <Grid className={styles.content} columns='equal' textAlign='center' stackable>
               <Grid.Row>
                 <Grid.Column>
                     <Header as="h2">
                         <FormattedMessage 
-                              id="site.wedomore.title" 
-                              defaultMessage="We Do More"
-                              description="Main title for the We Do More page" />
+                              id="site.compare.title" 
+                              defaultMessage="How much will you make?"
+                              description="Main title for the How much will you make? graph page" />
                     </Header>
+
                   </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
                   <Grid.Column>
-                  <Form.Input id={styles.myRange} className={styles.slider}
-                      label={`Starting value: ${starting} CA$`}
-                      min={100}
-                      max={100000}
-                      name='starting'
-                      onChange={handleChange}
-                      step={100}
-                      type='range'
-                      value={starting}
-                    />
-                    </Grid.Column>
-                  </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column>
-                  <Form.Input
-                      label={`Duration: ${duration} years `}
-                      min={1}
-                      max={60}
-                      name='duration'
-                      onChange={handleChange}
-                      step={1}
-                      type='range'
-                      value={duration}
-                    />
+                        <FormattedMessage 
+                              id="site.compare.description" 
+                              defaultMessage="Your chequing account will be with you longer than your retirement savings will. This is what that could look like."
+                              description="Main description for the How much will you make? graph page" />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+              <Grid columns={3} stackable>
+                <Grid.Row stretched>
+                  <Grid.Column textAlign='center'>
+                    <div id={styles.variablesContainer}>
+                          <Form.Input id="rangeStarting"
+                              label={labelStartingValue+` ${starting} `+labelStartingValueCurrency}
+                              min={100}
+                              max={100000}
+                              name='starting'
+                              onChange={handleChange}
+                              step={100}
+                              type='range'
+                              value={starting}
+                            />
+                          <Form.Input id="rangeDuration"
+                              label={labelDurationValue+` ${duration} `+labelDurationYear}
+                              min={1}
+                              max={60}
+                              name='duration'
+                              onChange={handleChange}
+                              step={1}
+                              type='range'
+                              value={duration}
+                            />
+
+                            <Button secondary>
+                              <FormattedMessage id="site.compare.button" 
+                                                defaultMessage="Show Me" 
+                                                description="Button for the graph page" />
+                            </Button>
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column textAlign='left'>
+                    <div id={styles.graphContainer}>
+                      <Header as="h4">
+                          <FormattedMessage 
+                                id="site.compare.graph.title" 
+                                defaultMessage="Your possible benefits"
+                                description="Graph title for the How much will you make? graph page" />
+                        </Header>
+                        <FormattedMessage 
+                                id="site.compare.graph.description" 
+                                defaultMessage="There is a 95% chance you're return will be in that area."
+                                description="Graph description for the How much will you make? graph page" />
+                                
+                        <img src="./images/Group576.png" />
+                      </div>
                     </Grid.Column>
                   </Grid.Row>
             </Grid>
