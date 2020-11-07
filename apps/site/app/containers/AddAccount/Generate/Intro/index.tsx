ec16d687d41be871d89023861b541c2a26f5209e
MarieTheCoin <marie@thecoin.io>
2020-11-06 19:14:07 -0500

    First page for create account process

--- apps/site/app/containers/AddAccount/Generate/Intro/index.tsx
+++ apps/site/app/containers/AddAccount/Generate/Intro/index.tsx
@@ -1,38 +1,77 @@
-import React from "react"
-import { Header, Button, List } from "semantic-ui-react"
-import { FormattedMessage } from "react-intl"
-import messages from "./messages"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Header, Button, Grid, Container } from "semantic-ui-react";
+import { FormattedMessage } from "react-intl";
+import { Link } from "react-router-dom";
+
+import illustration from './images/illust_flowers.svg';
+import styles from './styles.module.css';
 
 
 export const Intro = () => {
   return (
-    <>
-      <Header as="h1">
-        <Header.Content>
-          <FormattedMessage {...messages.header} />
-        </Header.Content>
-        <Header.Subheader>
-          <FormattedMessage {...messages.subHeader} />
-        </Header.Subheader>
-        <Header.Subheader>
-          <FormattedMessage {...messages.subHeader2} />
-        </Header.Subheader>
+    <Container className={styles.content}>
+
+      <Header as='h5'>
+        <FormattedMessage 
+          id="site.account.generate.aboveTheTitle"
+          defaultMessage="WHY ARE WE DIFFERRENT?"
+          description="Description for the information page before the create account form"
+            />
       </Header>
-        <p><FormattedMessage {...messages.intro} /></p>
-        <p><FormattedMessage {...messages.articles} /></p>
-        <List>
-          <List.Item>
-            <Link to="/FAQ/my-account" target="_blank">How to make a good password</Link>
-          </List.Item>
-          <List.Item>
-            <Link to="/FAQ/my-account" target="_blank">Where is my account stored?</Link>
-          </List.Item>
-        </List>
-        <p><FormattedMessage {...messages.infoAdvantages} /></p>
-      <Button as={Link} to="/addAccount/generate" id="buttonCreateAccountStep1">
-        <FormattedMessage {...messages.newAccount} />
+      <Header as="h2">
+          <FormattedMessage 
+            id="site.account.generate.header"
+            defaultMessage="Welcome to TheCoin"
+            description="Title for the information page before the create account form"
+             />
+      </Header>
+        <p><FormattedMessage 
+            id="site.account.generate.info"
+            defaultMessage="Accounts with TheCoin are a little different to your regular bank. We are built on the blockchain, which means that we don't store your account keys - you do!
+              If you've never used a blockchain account, some of these differences can seem a bit strange. We encourage you to read the short intro articles below if you would like to learn more."
+            description="Content for the information page before the create account form"
+              />
+        </p>
+        <Grid columns='equal'>
+          <Grid.Row>
+            <Grid.Column>
+              <Link to="/FAQ/my-account" target="_blank">
+                <FormattedMessage 
+                  id="site.account.generate.button.goodPassword"
+                  defaultMessage="How to make a good password"
+                  description="Description for the information page before the create account form"
+                    />
+                </Link>
+            </Grid.Column>
+            <Grid.Column>
+              <Link to="/FAQ/my-account" target="_blank">
+                <FormattedMessage 
+                  id="site.account.generate.button.accountStored"
+                  defaultMessage="Where is my account stored?"
+                  description="Description for the information page before the create account form"
+                    />
+                </Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <p>
+          <FormattedMessage 
+                id="site.account.generate.proud"
+                defaultMessage="We are very proud to combine the cutting-edge security and  performance of blockchain\
+                with the the conveniences of traditional banking."
+                description="Text underneath the buttons for the information page before the create account form"
+            />
+        </p>
+      <Button as={Link} to="/addAccount/generate" primary size="huge">
+        <FormattedMessage 
+                id="site.account.generate.button.go"
+                defaultMessage="Create a New Account"
+                description="The create account button for the information page before the create account form"
+            />
       </Button>
-    </>
+      <div className={styles.illustration}>
+        <img src={illustration} />
+      </div>
+    </Container>
   )
 }
