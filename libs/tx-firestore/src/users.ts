082dd467543b3a2ed1a80a7cd7d3aea6f497cdac
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-01-05 13:06:42 -0600

    add warning message on invalid access

--- libs/tx-firestore/src/users.ts
+++ libs/tx-firestore/src/users.ts
@@ -4,7 +4,14 @@ import { GetFirestore } from "../../utils-ts/build/firestore";
 //
 // TODO: migrate fn's from utilities to here
 
+// List all users.  This function will fail on client apps (ie - electron)
+// See error message for details
 export async function fetchAllUsers() {
-  const allUsers = await GetFirestore().collection("User").listDocuments();
+  const userCollection = GetFirestore().collection("User");
+  if (!userCollection.listDocuments) {
+    // If we want to do this we could provide an alternative implementation (?)
+    throw new Error("Attempting to use server-side API from client library");
+  }
+  const allUsers = await userCollection.listDocuments();
   return allUsers.map(user => user.id);
 }
