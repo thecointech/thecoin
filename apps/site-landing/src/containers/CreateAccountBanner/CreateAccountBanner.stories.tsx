e306b0aedaeb895dae2aa0fc478e2c004e4e1332
MarieTheCoin <marie@thecoin.io>
2021-01-05 15:53:44 -0500

    Rename and move stories where they belong

--- apps/site-landing/src/containers/CreateAccountBanner/CreateAccountBanner.stories.tsx
+++ apps/site-landing/src/containers/CreateAccountBanner/CreateAccountBanner.stories.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { CreateAccountSmall } from '../src/containers/HomePage/createAccountSmall';
-import { CreateAccountBanner, TypeCreateAccountBanner } from '../src/containers/CreateAccountBanner';
+import { CreateAccountSmall } from '../HomePage/createAccountSmall';
+import { CreateAccountBanner, TypeCreateAccountBanner } from '.';
 
 import { MemoryRouter } from 'react-router';
 import { Provider, ProviderProps } from 'react-redux';
 import history from '@the-coin/shared/build/utils/history';
 import {configureAppStore} from '@the-coin/shared/build/configureStore';
-import {createReducer} from '../../admin/app/reducers';
+import createReducer from '../../reducers';
+import { AnyAction } from 'redux';
 
 
 const store = configureAppStore(createReducer, undefined, history);
@@ -24,11 +25,11 @@ const Template: Story<ProviderProps> = () => <CreateAccountSmall />;
 const TemplateBigPeople: Story<ProviderProps> = () => <CreateAccountBanner Type={ TypeCreateAccountBanner.People } />;
 const TemplateBigPlant: Story<ProviderProps> = () => <CreateAccountBanner Type={ TypeCreateAccountBanner.Plants } />;
 
-export const Small = Template.bind({});
+export const Small: Story<ProviderProps<AnyAction>> = Template.bind({});
 Small.args = {};
 
-export const BigPeople = TemplateBigPeople.bind({});
+export const BigPeople: Story<ProviderProps<AnyAction>> = TemplateBigPeople.bind({});
 BigPeople.args = {};
 
-export const BigPlants = TemplateBigPlant.bind({});
+export const BigPlants: Story<ProviderProps<AnyAction>> = TemplateBigPlant.bind({});
 BigPlants.args = {};
