4bb6a4a5cfaf761ebe3ffb3074003c36f821b9f8
MarieTheCoin <marie@thecoin.io>
2021-01-05 15:32:44 -0500

    Rename and move the stories where they belong

--- apps/site-landing/src/containers/HomePage/landscape/Landscape.stories.tsx
+++ apps/site-landing/src/containers/HomePage/landscape/Landscape.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { LandscapeGreaterThanMobile } from '../src/containers/HomePage/landscape/LandscapeGreaterThanMobile';
-import { LandscapeMobile } from '../src/containers/HomePage/landscape/LandscapeMobile';
+import { LandscapeGreaterThanMobile } from './LandscapeGreaterThanMobile';
+import { LandscapeMobile } from './LandscapeMobile';
 
 import { MemoryRouter } from 'react-router';
 import { Provider, ProviderProps } from 'react-redux';
 import history from '@the-coin/shared/build/utils/history';
 import {configureAppStore} from '@the-coin/shared/build/configureStore';
-import {createReducer} from '../../admin/app/reducers';
-
+import createReducer from '../../../reducers';
+import { AnyAction } from 'redux';
 
 const store = configureAppStore(createReducer, undefined, history);
 
@@ -23,8 +23,8 @@ export default {
 const Template: Story<ProviderProps> = () => <LandscapeGreaterThanMobile mainTitle={"The future is brighter"} mainDescription={"Save, invest and spend money with TheCoin, get 100% of benefits and save our Planet."} />;
 const TemplateMobile: Story<ProviderProps> = () => <LandscapeMobile mainTitle={"The future is brighter"} mainDescription={"Save, invest and spend money with TheCoin, get 100% of benefits and save our Planet."} />;
 
-export const Desktop = Template.bind({});
+export const Desktop: Story<ProviderProps<AnyAction>> = Template.bind({});
 Desktop.args = {};
 
-export const Mobile = TemplateMobile.bind({});
+export const Mobile: Story<ProviderProps<AnyAction>> = TemplateMobile.bind({});
 Mobile.args = {};
