1a7873a7c58e7e5152dee5c24d6016dd65150540
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-01-05 13:02:52 -0600

    uncommented lsitDocuments.  This function is not available on client apis (ie - electron) but admin app is still compiling so 'shrugs'

--- libs/tx-firestore/src/users.ts
+++ libs/tx-firestore/src/users.ts
@@ -5,6 +5,6 @@ import { GetFirestore } from "../../utils-ts/build/firestore";
 // TODO: migrate fn's from utilities to here
 
 export async function fetchAllUsers() {
-  const allUsers = await GetFirestore().collection("User").get();
-  return allUsers.docs.map(user => user.id);
+  const allUsers = await GetFirestore().collection("User").listDocuments();
+  return allUsers.map(user => user.id);
 }
