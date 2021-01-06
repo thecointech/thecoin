79be1ca1427623564661f395557c98bc6f5f39ad
MarieTheCoin <marie@thecoin.io>
2021-01-06 10:54:12 -0500

    Fixes for advantages and footer

--- apps/site-landing/src/containers/HomePage/advantages/index.tsx
+++ apps/site-landing/src/containers/HomePage/advantages/index.tsx
@@ -21,7 +21,7 @@ export const Advantages = () => {
   return (
     <React.Fragment>
       <div id={styles.advantages}>
-        <Grid columns='equal' textAlign='center' verticalAlign='middle' stackable>
+        <Grid columns='equal' textAlign='center' verticalAlign='middle' stackable id={styles.advantageContent}>
           <Grid.Row>
             <Grid.Column width={2}>
               <h2>0</h2><h4>%</h4><br />
