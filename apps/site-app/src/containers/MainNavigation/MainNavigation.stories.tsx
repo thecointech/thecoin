6d4eafd8a0e4f5e6a5863760235e75be2f4b4e69
MarieTheCoin <marie@thecoin.io>
2021-01-12 16:56:32 -0500

    Add the decorator for storybook in the global preview

--- apps/site-app/src/containers/MainNavigation/MainNavigation.stories.tsx
+++ apps/site-app/src/containers/MainNavigation/MainNavigation.stories.tsx
@@ -2,19 +2,12 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import MainNavigation from '.';
-import { MemoryRouter } from 'react-router';
-import { Provider, ProviderProps } from 'react-redux';
-import history from '@the-coin/shared/build/utils/history';
-import {configureAppStore} from '@the-coin/shared/build/configureStore';
-import createReducer from '../../reducers';
+import { ProviderProps } from 'react-redux';
 import { AnyAction } from 'redux';
-
-const store = configureAppStore(createReducer, undefined, history);
 
 export default {
   title: 'App/MainNavigation',
   component: MainNavigation,
-  decorators: [(Story) => <Provider store={store}><MemoryRouter><Story/></MemoryRouter></Provider>],
   argTypes: {}
 } as Meta;
 
