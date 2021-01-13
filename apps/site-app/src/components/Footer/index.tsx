b74b22c64a247c1b39465f47a6595df378854591
MarieTheCoin <marie@thecoin.io>
2021-01-13 16:07:54 -0500

    Create FooterShared and use it in site-landing and site-app footers + Cleaning

--- apps/site-app/src/components/Footer/index.tsx
+++ apps/site-app/src/components/Footer/index.tsx
@@ -1,64 +1,23 @@
 import React from 'react';
 
-import styles from './styles.module.less';
-
-import { FormattedMessage } from 'react-intl';
-import { Grid, StrictGridColumnProps } from 'semantic-ui-react';
-
-import facebook from './images/facebook.svg';
-import twitter from './images/twitter.svg';
-import instagram from './images/instagram.svg';
+import { FooterShared } from '@the-coin/site-base/containers/FooterShared';
 import getWindowDimensions from '@the-coin/site-base/components/WindowDimensions';
 import { breakpointsValues } from '@the-coin/site-base/components/ResponsiveTool';
 
-const registered = {  id:"app.footer.registered",
-                      defaultMessage:"The Coin Collaborative Canada is a registered non-profit",
-                      description:"Registered Non profit phrase in footer"};
-
-const copyright = {   id:"app.footer.copyright",
-                      defaultMessage:"Copyright 2020. TheCoin. All Right Reserved.",
-                      description:"Copyright phrase in footer"};
+import styles from './styles.module.less';
 
 export const Footer = () => {
   const windowDimension = getWindowDimensions();
   const breakpointTablet = breakpointsValues.tablet;
-  const registeredContent = <FormattedMessage {...registered} />;
-  const socialLinksContent = <><img src={facebook} /><img src={twitter} /><img src={instagram} /></>;
-  const copyrightContent = <>&#169; &nbsp;<FormattedMessage {...copyright} /></>;
-
-  let alignForLeftColumn = "left" ;
-  let alignForCenterColumn = "center";
-  let alignForRightColumn = "right";
-  let leftColumn = registeredContent;
-  let centerColumn = socialLinksContent;
-  let rightColumn = copyrightContent;
   let classForContainer = styles.desktopContent;
 
   // If Small Screen / Mobile
   if (windowDimension.width <= breakpointTablet){
-    alignForLeftColumn = "center";
-    alignForCenterColumn = "center";
-    alignForRightColumn = "center";
-    leftColumn = socialLinksContent;
-    centerColumn = registeredContent;
-    rightColumn = copyrightContent;
     classForContainer = styles.mobileContent;
   }
   return (
     <div id={styles.footerContainer} className={classForContainer}>
-      <Grid columns='equal' centered verticalAlign='top' stackable>
-          <Grid.Row>
-            <Grid.Column textAlign={alignForLeftColumn as StrictGridColumnProps["textAlign"]}>
-              {leftColumn}
-            </Grid.Column>
-            <Grid.Column textAlign={alignForCenterColumn as StrictGridColumnProps["textAlign"]}>
-              {centerColumn}
-            </Grid.Column>
-            <Grid.Column textAlign={alignForRightColumn as StrictGridColumnProps["textAlign"]}>
-              {rightColumn}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+      <FooterShared />
     </div>
   );
 }
---
+++ libs/site-base/src/containers/FooterShared/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { FormattedMessage } from 'react-intl';
+import { Grid, StrictGridColumnProps } from 'semantic-ui-react';
+
+import facebook from './images/facebook.svg';
+import twitter from './images/twitter.svg';
+import instagram from './images/instagram.svg';
+import getWindowDimensions from '../../components/WindowDimensions';
+import { breakpointsValues } from '../../components/ResponsiveTool';
+
+
+const registered = {  id:"shared.footer.registered",
+                      defaultMessage:"The Coin Collaborative Canada is a registered non-profit",
+                      description:"Registered Non profit phrase in footer"};
+
+const copyright = {   id:"shared.footer.copyright",
+                      defaultMessage:"Copyright 2020. TheCoin. All Right Reserved.",
+                      description:"Copyright phrase in footer"};
+
+
+export const FooterShared = () => {
+
+  const windowDimension = getWindowDimensions();
+  const breakpointTablet = breakpointsValues.tablet;
+  const registeredContent = <FormattedMessage {...registered} />;
+  const socialLinksContent = <><img src={facebook} /><img src={twitter} /><img src={instagram} /></>;
+  const copyrightContent = <>&#169; &nbsp;<FormattedMessage {...copyright} /></>;
+
+  let alignForLeftColumn = "left" ;
+  let alignForCenterColumn = "center";
+  let alignForRightColumn = "right";
+  let leftColumn = registeredContent;
+  let centerColumn = socialLinksContent;
+  let rightColumn = copyrightContent;
+
+  // If Small Screen / Mobile
+  if (windowDimension.width <= breakpointTablet){
+    alignForLeftColumn = "center";
+    alignForCenterColumn = "center";
+    alignForRightColumn = "center";
+    leftColumn = socialLinksContent;
+    centerColumn = registeredContent;
+    rightColumn = copyrightContent;
+  }
+
+  return (
+    <>
+      <Grid columns='equal' centered verticalAlign='top' stackable className={ `x2spaceBefore` }>
+        <Grid.Row>
+          <Grid.Column textAlign={alignForLeftColumn as StrictGridColumnProps["textAlign"]}>
+            {leftColumn}
+          </Grid.Column>
+          <Grid.Column textAlign={alignForCenterColumn as StrictGridColumnProps["textAlign"]}>
+            {centerColumn}
+          </Grid.Column>
+          <Grid.Column textAlign={alignForRightColumn as StrictGridColumnProps["textAlign"]}>
+            {rightColumn}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+}
