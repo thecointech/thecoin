41ca9f1b7dd539be30731e8c2b1b06b554769e52
MarieTheCoin <marie@thecoin.io>
2021-01-20 19:43:26 -0500

    Add fake sidebar

--- apps/site-app/src/containers/MainRouter/index.tsx
+++ apps/site-app/src/containers/MainRouter/index.tsx
@@ -7,8 +7,73 @@ import { Accounts } from 'containers/Accounts';
 import { AddAccount } from 'containers/AddAccount';
 import { Congratulations } from 'containers/AddAccount/Congratulations';
 import { GAuth } from 'containers/StoreOnline/Google/gauth';
+import { RUrl } from '@the-coin/utilities/RUrl';
+import { useSidebar } from '@the-coin/shared/containers/PageSidebar/actions';
+import { SidebarMenuItem, MapMenuItems } from '@the-coin/shared/containers/PageSidebar/types';
+
 
 export const MainRouter = (props: { location: Location }) => {
+
+
+  const ConstantSidebarItems: SidebarMenuItem[] = 
+  [
+    {
+      link: {
+        to: false,
+        name: 'Profile',
+        header: { avatar: "https://sadanduseless.b-cdn.net/wp-content/uploads/2019/07/yawning-rabbits4.jpg", 
+                  primaryDescription: "The quick brown fox jumps over the lazy dog.", 
+                  secondaryDescription: "Description2" },
+      },
+    },
+    {
+      link: {
+        name: "Home",
+        to: new RUrl("/"),
+        icon: "home",
+      }
+    },
+    {
+      link: {
+        name: "Top up Balance",
+        to: new RUrl("/balance"),
+        icon: "arrow circle up",
+      }
+    },
+    {
+      link: {
+        name: "Make a Payment",
+        to: new RUrl("/payment"),
+        icon: "arrow circle right",
+      }
+    },
+    {
+      link: {
+        name: "Settings",
+        to: new RUrl("/settings"),
+        icon: "setting",
+      }
+    },
+    {
+      link: {
+        name: "Help",
+        to: new RUrl("/help"),
+        icon: "life ring",
+      }
+    }
+  ];
+  
+  const generateSidebarItems = () : SidebarMenuItem[] => 
+    MapMenuItems(ConstantSidebarItems, "/");
+  
+    const SIDEBAR_KEY = "RootItems"
+    
+    const sidebar = useSidebar();
+    React.useEffect(() => {
+      sidebar.addGenerator(SIDEBAR_KEY, generateSidebarItems);
+      return () => sidebar.removeGenerator(SIDEBAR_KEY);
+    }, [sidebar])
+  
   return (
     <Switch location={props.location}>
       <Route path="/gauth" component={GAuth} />
