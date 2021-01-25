d73e9ebd41863a7b9d0bc6264b82e1d442e879a2
MarieTheCoin <marie@thecoin.io>
2021-01-25 18:30:28 -0500

    Put boxes where they belong on mobile

--- apps/site-app/src/containers/ColumnRight/Bottom/index.tsx
+++ apps/site-app/src/containers/ColumnRight/Bottom/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { ClimateImpact } from "@the-coin/shared/build/containers/Widgets/ClimateImpact";
+import getWindowDimensions from '@the-coin/site-base/components/WindowDimensions';
+import { breakpointsValues } from '@the-coin/site-base/components/ResponsiveTool';
 import styles from "./styles.module.less";
 
 export const ColumnRightBottom = () => {
+
+  let classForColumn = styles.columnRightBottom;
+  const windowDimension = getWindowDimensions();
+  const breakpointTablet = breakpointsValues.tablet;
+  
+  // If Small Screen / Mobile
+  if (windowDimension.width <= breakpointTablet){
+    classForColumn = styles.columnRightBottomMobile;
+  }
+
   return (
-    <div className={styles.columnRightBottom}>
+    <div className={classForColumn}>
         <ClimateImpact />
     </div>
   )
--- apps/site-app/src/containers/ColumnRight/Top/index.tsx
+++ apps/site-app/src/containers/ColumnRight/Top/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { BalanceAndProfit } from "@the-coin/shared/build/containers/Widgets/BalanceAndProfit";
+import getWindowDimensions from '@the-coin/site-base/components/WindowDimensions';
+import { breakpointsValues } from '@the-coin/site-base/components/ResponsiveTool';
 import styles from "./styles.module.less";
 
 export const ColumnRightTop = () => {
-  //const { locale } = useSelector(selectLocale);
+  
+  let classForColumn = styles.columnRightTop;
+  const windowDimension = getWindowDimensions();
+  const breakpointTablet = breakpointsValues.tablet;
+  
+  // If Small Screen / Mobile
+  if (windowDimension.width <= breakpointTablet){
+    classForColumn = styles.columnRightTopMobile;
+  }
+
   return (
-    <div className={styles.columnRightTop}>
+    <div className={classForColumn}>
         <BalanceAndProfit />
     </div>
   )
