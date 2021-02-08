193fab8f6a7663d19336aa3cd673619bfd8fb2ab
MarieTheCoin <marie@thecoin.io>
2021-02-08 18:14:39 -0500

    CSS for tabs in app

--- apps/site-app/src/containers/MakePayments/index.tsx
+++ apps/site-app/src/containers/MakePayments/index.tsx
@@ -18,7 +18,7 @@ import { Transfer } from 'containers/Accounts/Transfer';
 import * as React from 'react';
 import illustration from './images/icon_payment_big.svg';
 
-import { Header, Tab } from 'semantic-ui-react';
+import { Grid, Header, Tab } from 'semantic-ui-react';
 import { FormattedMessage } from 'react-intl';
 
 
@@ -33,20 +33,28 @@ const description = { id:"app.makepayments.description",
 export const MakePayments = (props: any, routerProps:AccountPageProps) => {
   const account = props.account;
   const panes = [
-    { menuItem: 'e-Transfert', render: () => <div className={"appContainer"}><Redeem {...props} account={account} /></div> },
-    { menuItem: 'Bills', render: () => <div className={"appContainer"}><BillPayments {...props} account={account} /></div> },
-    { menuItem: 'Another Coin Account', render: () => <div className={"appContainer"}><Transfer {...props} account={account} /></div> },
-    { menuItem: 'Templates', render: () => <div className={"appContainer"}>Templates</div> },
+    { menuItem: 'e-Transfert', render: () => <div className={"appContainer topRightFlat"}><Redeem {...props} account={account} /></div> },
+    { menuItem: 'Bills', render: () => <div className={"appContainer topRightFlat"}><BillPayments {...props} account={account} /></div> },
+    { menuItem: 'Another Coin Account', render: () => <div className={"appContainer topRightFlat"}><Transfer {...props} account={account} /></div> },
+    { menuItem: 'Templates', render: () => <div className={"appContainer topRightFlat"}>Templates</div> },
   ]
   return (
     <React.Fragment>
-      <img src={illustration} />
-      <Header as="h5" className={ `appTitles` }>
-          <FormattedMessage {...title} />
-          <Header.Subheader>
-            <FormattedMessage  {...description} />
-          </Header.Subheader>
-      </Header>
+      <Grid className={ `x2spaceBefore x4spaceAfter` } >
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <img src={illustration} />
+          </Grid.Column>
+          <Grid.Column width={13}>
+            <Header as="h5" className={ `appTitles` }>
+                <FormattedMessage {...title} />
+                <Header.Subheader>
+                  <FormattedMessage  {...description} />
+                </Header.Subheader>
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
       <Tab panes={panes} renderActiveOnly={true}/>
       <RecentTransactions {...props} {...routerProps} />
     </React.Fragment>
