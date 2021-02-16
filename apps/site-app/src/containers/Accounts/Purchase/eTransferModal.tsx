5466f769f8f31be47492d9ebdd62021c310e2ed2
MarieTheCoin <marie@thecoin.io>
2021-02-16 12:29:16 -0500

    Remove that messages file

--- apps/site-app/src/containers/Accounts/Purchase/eTransferModal.tsx
+++ apps/site-app/src/containers/Accounts/Purchase/eTransferModal.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent } from 'react';
 import { ModalOperation } from '@the-coin/shared/containers/ModalOperation';
 import { CopyToClipboard } from '@the-coin/site-base/components/CopyToClipboard';
 import { Grid } from 'semantic-ui-react';
-import messages from './messages';
 import { FormattedMessage } from 'react-intl';
 import styles from './styles.module.less';
 
@@ -13,6 +12,19 @@ interface Props {
   onCloseDlg: () => void;
 }
 
+const getTransferCodeHeader = { id:"app.accounts.purchase.getTransferCodeHeader",
+                defaultMessage:"Please wait..."}; 
+const haveTransferCodeHeader = { id:"app.accounts.purchase.haveTransferCodeHeader",
+                defaultMessage:"Your details:" };
+const fetchTransferCode = { id:"app.accounts.purchase.fetchTransferCode",
+                defaultMessage:"We are generating your individual e-Transfer recipient info"}; 
+const yourTransferCode = { id:"app.accounts.purchase.yourTransferCode",
+                defaultMessage:"The following is your personalized e-Transfer info:" };
+const yourTransferRecipient = { id:"app.accounts.purchase.yourTransferRecipient",
+                defaultMessage:"Recipient:"}; 
+const yourTransferSecret = { id:"app.accounts.purchase.yourTransferSecret",
+                defaultMessage:"Secret:" };
+
 export const ETransferModal: FunctionComponent<Props> = (props: Props) =>
 {
   const {xferRecipient, xferSecret} = props;
@@ -20,17 +32,17 @@ export const ETransferModal: FunctionComponent<Props> = (props: Props) =>
   (
     <ModalOperation
       isOpen={props.showDlg}
-      header={messages.haveTransferCodeHeader}
+      header={haveTransferCodeHeader}
       okCallback={props.onCloseDlg}
       progressPercent={1}
     >
       <Grid>
         <Grid.Row>
-          <FormattedMessage {...messages.yourTransferCode} />
+          <FormattedMessage {...yourTransferCode} />
         </Grid.Row>
         <Grid.Row columns="two">
           <Grid.Column className={styles.fixedColumn}>
-            <FormattedMessage {...messages.yourTransferRecipient} />
+            <FormattedMessage {...yourTransferRecipient} />
           </Grid.Column>
           <Grid.Column className={styles.floatColumn}>
           <CopyToClipboard payload={xferRecipient}>
@@ -40,7 +52,7 @@ export const ETransferModal: FunctionComponent<Props> = (props: Props) =>
         </Grid.Row>
         <Grid.Row columns="two">
           <Grid.Column className={styles.fixedColumn}>
-            <FormattedMessage {...messages.yourTransferSecret} />
+            <FormattedMessage {...yourTransferSecret} />
           </Grid.Column>
           <Grid.Column className={styles.floatColumn}>
             <CopyToClipboard payload={xferSecret}>
@@ -53,10 +65,10 @@ export const ETransferModal: FunctionComponent<Props> = (props: Props) =>
   ) : (
     <ModalOperation
       isOpen={props.showDlg}
-      header={messages.getTransferCodeHeader}
+      header={getTransferCodeHeader}
       okCallback={props.onCloseDlg}
     >
-      <FormattedMessage {...messages.fetchTransferCode} />
+      <FormattedMessage {...fetchTransferCode} />
     </ModalOperation>
   )
 }
