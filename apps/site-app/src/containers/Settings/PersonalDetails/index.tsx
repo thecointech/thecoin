dc5eca9561626cbbbd8900d2505ee1d0c14567a9
MarieTheCoin <marie@thecoin.io>
2021-02-16 19:49:52 -0500

    Add the images and add the missing fields

--- apps/site-app/src/containers/Settings/PersonalDetails/index.tsx
+++ apps/site-app/src/containers/Settings/PersonalDetails/index.tsx
@@ -1,34 +1,40 @@
 import { ButtonTertiary } from '@the-coin/site-base/components/Buttons';
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Form, Header } from 'semantic-ui-react';
+import { Form, Header, Radio } from 'semantic-ui-react';
 
-const title = { id:"app.settings.title",
+const title = { id:"app.settings.personaldetails.title",
                 defaultMessage:"Personal Details",
                 description:"Title for the page setting / tab personal details in the app" };
-const name = { id:"app.settings.name",
+const titleCurrency = { id:"app.settings.personaldetails.titleCurrency",
+                defaultMessage:"Currency",
+                description:"Title for the currency section of the page setting / tab personal details in the app" };
+const descriptionCurrency = { id:"app.settings.personaldetails.descriptionCurrency",
+                defaultMessage:"Your can choose the currency you want us to display:",
+                description:"Title for the currency section of the page setting / tab personal details in the app" };
+const name = { id:"app.settings.personaldetails.name",
                 defaultMessage:"First & Last Name",
                 description:"Name field for the page setting / tab personal details in the app" };
-const email = { id:"app.settings.email",
+const email = { id:"app.settings.personaldetails.email",
                 defaultMessage:"Email",
                 description:"Email field for the page setting / tab personal details in the app" };
-const dob = { id:"app.settings.dob",
+const dob = { id:"app.settings.personaldetails.dob",
                 defaultMessage:"Date Of Birth",
                 description:"Date of birth field for the page setting / tab personal details in the app" };
-const phone = { id:"app.settings.phone",
+const phone = { id:"app.settings.personaldetails.phone",
                 defaultMessage:"Phone Number",
                 description:"Phone field for the page setting / tab personal details in the app" };
-const address = { id:"app.settings.address",
+const address = { id:"app.settings.personaldetails.address",
                 defaultMessage:"Full Address",
                 description:"Address field for the page setting / tab personal details in the app" };
-const button = { id:"app.settings.button",
+const button = { id:"app.settings.personaldetails.button",
                 defaultMessage:"Save",
                 description:"Content for the page setting / tab personal details in the app" };
 
 export const PersonalDetails = () => {
   return (
     <div>
-      <Header as="h5">
+      <Header as="h5" className={ `appTitles` }>
         <FormattedMessage {...title} />
       </Header>
       <Form>
@@ -54,6 +60,26 @@ export const PersonalDetails = () => {
           className={"borderTop borderBottom"}
           label={<FormattedMessage {...address} />}
           name="address"/>
+
+      <Header as="h5" className={ `appTitles` }>
+        <FormattedMessage {...titleCurrency} />
+        <Header.Subheader>
+              <FormattedMessage  {...descriptionCurrency} />
+            </Header.Subheader>
+      </Header>
+          
+        <Form.Field>
+          <Radio
+            label='CAD'
+            name='currency'
+            value='cad'
+          /> &nbsp;&nbsp;&nbsp;&nbsp;
+          <Radio
+            label='THE'
+            name='currency'
+            value='the'
+          />
+          </Form.Field>
 
           <ButtonTertiary>
               <FormattedMessage {...button} />
