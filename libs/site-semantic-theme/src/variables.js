93f3644be4134580b5a523ab9fc79a07e8148af2
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-02-19 17:10:23 -0600

    move less-var-loader to be local to semantic-theme

--- libs/site-semantic-theme/src/variables.js
+++ libs/site-semantic-theme/src/variables.js
@@ -1,6 +1,5 @@
-//var variables = require('!!../../../../tools/less-var-loader!./variables.less');
 // We can't type this, you just have to look it up in source!
-const LessVars = require('!../../../tools/less-var-loader.js?resolveVariables!./semantic/globals/site.variables');
+const LessVars = require('!../internal/less-var-loader.js?resolveVariables!./semantic/globals/site.variables');
 
 module.exports = {
   LessVars: LessVars
