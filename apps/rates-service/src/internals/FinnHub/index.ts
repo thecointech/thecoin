f48e63ef497f6561cb7b4c123a4fdba2ea97f86f
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-02-23 09:16:06 -0600

    WIP: Move LanguageProvider and run tests

--- apps/rates-service/src/internals/FinnHub/index.ts
+++ apps/rates-service/src/internals/FinnHub/index.ts
@@ -2,27 +2,31 @@ import { FinnhubData, FinnhubRates, hasError } from "./types";
 import Axios, { AxiosResponse } from 'axios';
 import { finnhub_key } from './secret.json';
 
+function throwError(r: AxiosResponse): never {
+  throw new Error(`Fetch failed: ${r.statusText} : ${
+    hasError(r.data)
+      ? r.data.error
+      : JSON.stringify(r.data)
+  }`);
+}
+
 function validate<T>(r: AxiosResponse<T>, testMember: keyof T, testValue: any) {
   if (r.status != 200 ||
       !r.data ||
       r.data[testMember] != testValue
   ) {
-    throw new Error(`Fetch failed: ${r.statusText} : ${
-      hasError(r.data)
-        ? r.data.error
-        : JSON.stringify(r.data)
-    }`);
+    throwError(r);
   }
 }
 
+const validateStatus = (status: number) => status < 500;
+
 export async function fetchNewCoinRates(resolution: string, from: number, to: number) {
-  //log.trace(`Fetching SPX rates from ${new Date(from)} until now`);
   const fromInt = Math.round(from / 1000);
   const toInt = Math.floor(to / 1000);
-  const url = `https://finnhub.io/api/v1/stock/candle?symbol=%5EGSPC&resolution=${resolution}&token=${finnhub_key}&from=${fromInt}&to=${toInt}`
-  var r = await Axios.get<FinnhubData>(url);
-  validate(r, "s", "ok")
-  //log.debug(`Fetched SPX rates: ${r?.statusText} - ${r?.data?.t?.length} results`);
+  const url = `https://finnhub.io/api/v1/stock/candle?symbol=%5EGSPC&resolution=${resolution}&token=${finnhub_key}&from=${fromInt}&to=${toInt}`;
+  const r = await Axios.get<FinnhubData>(url, {validateStatus});
+  validate(r, "s", "ok");
   return r.data
 }
 
