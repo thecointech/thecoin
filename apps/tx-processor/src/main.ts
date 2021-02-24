7ba7bcf42b00aa86da84abc02654747b3f89e33a
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-02-24 06:09:52 -0600

    Remove JSON secrets from tx-processor

--- apps/tx-processor/src/main.ts
+++ apps/tx-processor/src/main.ts
@@ -2,7 +2,7 @@ import { signIn, InitContract } from "@the-coin/tx-processing";
 import { init as LogInit, log } from "@the-coin/logging";
 import { RbcStore, RbcApi } from "@the-coin/rbcapi";
 import { ConfigStore } from "@the-coin/store";
-import { GetWallet } from './wallet';
+import { getWallet } from '@the-coin/utilities/Wallets';
 import { initBrowser } from "@the-coin/rbcapi/action";
 import { ProcessUnsettledDeposits } from "@the-coin/tx-processing/deposit/service";
 import { processUnsettledETransfers } from "@the-coin/tx-processing/etransfer/service";
@@ -15,7 +15,7 @@ async function initialize() {
   RbcStore.initialize();
   ConfigStore.initialize();
 
-  const wallet = await GetWallet();
+  const wallet = await getWallet('BrokerCAD');
   if (!wallet) {
     throw new Error("Couldn't load wallet'");
   }
