48b0b1a1b3cc1736ea0af370c8111949fdd788fd
MarieTheCoin <marie@thecoin.io>
2021-03-01 17:09:52 -0500

    Update index.tsx

--- libs/shared/src/containers/Widgets/BalanceAndProfit/index.tsx
+++ libs/shared/src/containers/Widgets/BalanceAndProfit/index.tsx
@@ -1,22 +1,35 @@
+import { useActiveAccount } from "../../AccountMap";
 import React from "react";
 import { Header, Icon } from "semantic-ui-react";
 import illustration from "./images/illust_balance.svg";
 import styles from "./styles.module.less";
+import { getFxRate, useFxRates } from "../../FxRate";
+import { calculateProfit } from "../../Account/profit";
+import { toHuman } from "../../../../../utils-ts/build/Conversion";
 
 export const BalanceAndProfit = () => {
+  const activeAccount = useActiveAccount();
+  const { rates } = useFxRates();
+  const { buy, fxRate } = getFxRate(rates, 0);
+  const balance = activeAccount?.balance ? activeAccount?.balance : 0
+  const history = activeAccount?.history ? activeAccount?.history : [];
+  
+  const cadBalance = toHuman(buy * balance * fxRate, true);
+  const profit = calculateProfit(balance, history, rates);
+  const profitDisplay = (profit < 1 ? "< .1" : profit.toString())
   return (
     <div className={ `${styles.balanceAndProfit} x2spaceAfter` }>
         <Header as="h5">
             Balance
         </Header>
         <img src={illustration} />
-        <div>10 000</div>
+        <div>{cadBalance}</div>
         <div>CAD</div>
         <hr  />
         <Header as="h5">
             Profit
         </Header>
-        <div><Icon name='arrow up' />9%</div>
+        <div><Icon name='arrow up' />{profitDisplay}%</div>
     </div>
   )
 }
