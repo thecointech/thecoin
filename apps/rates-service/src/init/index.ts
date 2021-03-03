b3b94b3584f9ef02ab757d1a97b8165454d4e485
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-03-03 06:42:44 -0600

    tweaked naming of services/vs ports to be a bit clearer.  Updated fxRates to query local API when necessary

--- apps/rates-service/src/init/index.ts
+++ apps/rates-service/src/init/index.ts
@@ -1,8 +1,8 @@
-import { DateTime, Duration, Settings } from "luxon";
-import { init as initlog} from '@the-coin/logging';
+import { Settings } from "luxon";
+import { init as initlog, log } from '@the-coin/logging';
 import { init as initdb } from '@the-coin/utilities/firestore';
 import { initLatest } from '../internals/rates/latest';
-import { SeedWithRandomRates } from "./seed";
+import { seed } from "./seed";
 
 export async function init() {
   // Init luxon to use the right timezone
@@ -14,21 +14,11 @@ export async function init() {
 
   // Assume development is dev:live
   if (process.env.NODE_ENV === 'development') {
-    // Seed our DB for a year, values set for a day.
-    const from = DateTime
-      .local()
-      .minus({years: 1.1})
-      .set({
-        hour: 9,
-        minute: 31,
-        second: 30,
-        millisecond: 0
-      });
-
-    const validityInterval = Duration.fromObject({days: 1});
-    SeedWithRandomRates(from, validityInterval)
+    await seed();
   }
 
   // Once we are all ready to go, warm up our cache of latest data.
   await initLatest();
+
+  log.trace('Initialization complete')
 }
--- apps/rates-service/src/main.ts
+++ apps/rates-service/src/main.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import { RegisterRoutes } from './routes/routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './api/swagger.json';
-import { ServicePorts } from '@the-coin/utilities/ServiceAddresses';
+import { DevLivePort, Service } from '@the-coin/utilities/ServiceAddresses';
 import { init } from './init';
 
 const app = express();
-const port = process.env.PORT ?? ServicePorts.THE_CORE;
+const port = process.env.PORT ?? DevLivePort(Service.RATES);
 
 RegisterRoutes(app);
 
