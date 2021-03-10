de48a6a56e3593abe4951ada9df0831477789b26
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-03-10 15:58:33 -0600

    update to match new naming scheme

--- apps/broker-service/src/exchange/Wallet.ts
+++ apps/broker-service/src/exchange/Wallet.ts
@@ -1,7 +1,7 @@
 
-import { getContract, getWallet } from '@the-coin/utilities/Wallets';
+import { getContract, getSigner } from '@the-coin/utilities/blockchain';
 
 const walletName = 'BrokerTransferAssistant';
-export const GetWallet = () => getWallet(walletName);
+export const GetWallet = () => getSigner(walletName);
 export const GetContract = () => getContract(walletName);
 
