55be249fddf933b97b7197e0deadd8b68c7c2f78
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-04-21 17:20:16 -0500

    testing - simpler immerReducer for LanguageSwitcher

--- libs/shared/src/containers/LanguageProvider/reducer.ts
+++ libs/shared/src/containers/LanguageProvider/reducer.ts
@@ -3,12 +3,13 @@
  * LanguageProvider reducer
  *
  */
-import { TheCoinReducer, GetNamedReducer } from '../../store/immerReducer';
+import { TheCoinReducer } from '../../store/immerReducer';
 import { useInjectReducer } from 'redux-injectors';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { LanguageProviderState, DEFAULT_LOCALE, IActions, Locale } from './types';
 import { ApplicationBaseState } from '../../types';
+import { createReducerFunction, createActionCreators } from 'immer-reducer';
 
 export const initialState: LanguageProviderState = {
   locale: DEFAULT_LOCALE as Locale,
@@ -23,8 +24,13 @@ export class LanguageProviderReducer extends TheCoinReducer<LanguageProviderStat
     }
 }
 
-export const useLanguageProvider = () => {
-  const { reducer, actions } = GetNamedReducer(LanguageProviderReducer, LANGUAGE_KEY, initialState);
+export const useLanguageProvider = (initial?: LanguageProviderState) => {
+  const reducer: any = createReducerFunction(LanguageProviderReducer, initial ?? initialState);
   useInjectReducer({ key: LANGUAGE_KEY, reducer });
+}
+export const useLanguageProviderApi = () => {
+  const actions = createActionCreators(LanguageProviderReducer);
+  //const { reducer, actions } = GetNamedReducer(LanguageProviderReducer, LANGUAGE_KEY, initialState);
+  //useInjectReducer({ key: LANGUAGE_KEY, reducer });
   return (bindActionCreators(actions, useDispatch()) as IActions) as IActions;
 };
