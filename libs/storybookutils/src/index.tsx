45ca128e47a6eabf3c00658ac29aca6bd2d421d4
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-04-27 11:15:44 -0500

    tweak to allow setting store directly

--- libs/storybookutils/src/index.tsx
+++ libs/storybookutils/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@thecointech/shared/store';
+import { ApplicationBaseState } from '@thecointech/shared/types';
 import { combineReducers, ReducersMapObject } from 'redux';
 import { buildAccountStoreReducer, AccountMapState } from '@thecointech/shared/containers/AccountMap';
 
-export const withStore = () => {
-
-  const createReducer = (injectedReducers?: ReducersMapObject) => combineReducers({...injectedReducers});
-  const store = configureStore(createReducer);
+export function withStore(initialState?: ApplicationBaseState) {
+  const createReducer = (injectedReducers?: ReducersMapObject) =>
+    injectedReducers
+      ? combineReducers({...injectedReducers})
+      : (state: ApplicationBaseState) => state ?? initialState;
+  const store = configureStore(createReducer, initialState);
   return (Story: React.ElementType) => <Provider store={store}><Story /></Provider>
 }
 
-export const withAccounts = (initialState: AccountMapState) => {
+export function withAccounts(initialState: AccountMapState) {
   const createReducer = (injectedReducers?: ReducersMapObject) => {
     const { accountStoreReducer, rest } = buildAccountStoreReducer(injectedReducers, initialState);
     return combineReducers({
