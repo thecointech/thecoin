e4dbedeb4ba60026c830aea575f2832d0ebb913d
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2021-04-30 16:29:11 -0400

    Update index.tsx

--- apps/admin/app/containers/BrokerCAD/VerifyAccount/index.tsx
+++ apps/admin/app/containers/BrokerCAD/VerifyAccount/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Form, Header, Button, List, Message } from 'semantic-ui-react';
 import { UxAddress } from '@thecointech/shared/components/UxAddress';
