4b30cdb239bed268f7e2f930d134382715b22eb6
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2021-06-07 18:29:23 -0400

    Fix FAQ menu

--- apps/site-landing/src/containers/HelpDocs/FaqMenu.tsx
+++ apps/site-landing/src/containers/HelpDocs/FaqMenu.tsx
@@ -1,35 +1,55 @@
 import React from "react";
-import { FAQDocument } from "components/Prismic/types";
-import { Renderer } from "./Renderer/Renderer";
+//import { Renderer } from "./Renderer/Renderer";
 import { Link } from 'react-router-dom';
 import styles from "./styles.module.less";
 import { List, Rail } from "semantic-ui-react";
+import { breakpointsValues, GreaterThanMobileSegment, MobileSegment } from "@thecointech/shared/components/ResponsiveTool";
+import { useWindowDimensions } from "@thecointech/shared/components/WindowDimensions";
 
 type Props = {
     categories: any
   }
   
-function displayEntry(index: number, name: string, subentries: FAQDocument[] ){
+function displayEntry(index: number, name: string ){
     return <List.Item key={index}><Link to={"/faq/theme-"+((name)?.split("-"))[0].replace(/ /g, '')}>{((name.split("-"))[1])}</Link>
-                <List>
-                    { 
+                { /* <List> */ }
+                    { /*
                         subentries.map((entrySubentries) => ( 
                             <List.Item key={entrySubentries.id}>
                                 <Renderer r={entrySubentries.data.question} />
-                            </List.Item> ))
+                            </List.Item> ))*/
                     }
-                </List>
+                { /* <List> */ }
             </List.Item>
 }
 
+
 export const FaqMenu = ({ categories }: Props) => {
+
+    const windowDimension = useWindowDimensions();
+    const breakpointComputer = breakpointsValues.computer;
+    if (windowDimension.width <= breakpointComputer){
+        console.log("---SMALL---");
+    }
     return (
-        <Rail internal position='right'>
-            <div className={styles.menuFaq}>
-                <List divided relaxed className={"x10spaceBefore x8spaceAfter"}>
-                    { Object.entries(categories).map((entry, index) => ( displayEntry(index, entry[0], entry[1] as FAQDocument[] ))) }
-                </List>
-            </div>
-        </Rail>
+        <>
+            <GreaterThanMobileSegment>
+                <Rail position='right'>
+                    <div id={styles.menuFaq}>
+                        <List divided relaxed size={"massive"} className={"x10spaceBefore x8spaceAfter"}>
+                            { Object.entries(categories).map((entry, index) => ( displayEntry(index, entry[0] ))) }
+                        </List>
+                    </div>
+                </Rail>
+            </GreaterThanMobileSegment>
+
+            <MobileSegment>
+                <div id={styles.menuFaq}>
+                    <List divided relaxed size={"massive"} className={"x10spaceBefore x8spaceAfter"}>
+                        { Object.entries(categories).map((entry, index) => ( displayEntry(index, entry[0] ))) }
+                    </List>
+                </div>
+            </MobileSegment>
+        </>
     )
 }
