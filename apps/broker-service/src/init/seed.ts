2b0d59839548ecfb53bcd488ec873d9458c0ebb4
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-06-07 12:21:10 -0500

    Changed all instances of `signMessage` to inbuild `sign`

--- apps/broker-service/src/init/seed.ts
+++ apps/broker-service/src/init/seed.ts
@@ -6,6 +6,8 @@ import { createReferree, createReferrer } from "@thecointech/broker-db/referrals
 import { setUserVerified, getUserVerified } from "@thecointech/broker-db/user";
 import { getSigner } from "@thecointech/accounts";
 import { DateTime } from "luxon";
+import { sign } from "@thecointech/utilities/SignedMessages";
+
 //
 // When we seed the DB, we initialize the DB with 1 verified client, and 1 referred
 export async function seed() {
@@ -24,7 +26,7 @@ export async function seed() {
 async function setVerified(clientAddress: string) {
   const brokerCad = await getSigner("BrokerCAD");
   const address = NormalizeAddress(clientAddress);
-  const signature = await brokerCad.signMessage(address);
+  const signature = await sign(address, brokerCad);
   // Account created 1 year, 1 day ago.
   const ts = DateTime.now().minus({years: 1, days: 1});
   await setUserVerified(signature, address, ts);
