e894fdbd7b0425e6c084613731c1311c018d85ae
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-06-18 11:36:16 -0500

    remove electron dependency from tx-gmail

--- apps/admin/app/init/index.ts
+++ apps/admin/app/init/index.ts
@@ -2,16 +2,28 @@ import { init } from '@thecointech/logging';
 import { RbcStore } from "@thecointech/rbcapi";
 import { ConfigStore } from '@thecointech/store';
 import { initBrowser } from '@thecointech/rbcapi';
+import { initialize } from '@thecointech/tx-gmail';
+import { shell } from 'electron';
 
 //
 // Initialize (most of) the application
 // Does not initialize accounts or contract
-export function Initialize()
-{
-  RbcStore.initialize({adapter: "leveldb"});
-  ConfigStore.initialize({adapter: "leveldb"});
+export function Initialize() {
+  RbcStore.initialize({ adapter: "leveldb" });
+  ConfigStore.initialize({ adapter: "leveldb" });
 
   init("admin");
 
-  initBrowser({headless: true});
+  initBrowser({ headless: true });
+
+  initialize(initGmail);
+}
+
+function initGmail(authUrl: string) {
+  if (shell)
+    shell.openExternal(authUrl);
+  else {
+    const start = (process.platform === 'darwin' ? 'open' : process.platform === 'win32' ? 'start' : 'xdg-open');
+    require('child_process').exec(start + ' ' + authUrl);
+  }
 }
