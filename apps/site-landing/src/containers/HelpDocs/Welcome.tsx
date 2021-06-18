592c378f52c447d4d70df6b2dc7160eebb912122
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2021-06-18 17:51:45 -0400

    Add blog in the menu

--- apps/site-landing/src/containers/HelpDocs/Welcome.tsx
+++ apps/site-landing/src/containers/HelpDocs/Welcome.tsx
@@ -15,10 +15,8 @@ export const Welcome = (props: Props) => {
   const starred =  props.faqs.filter(faq => faq.data.show_on_faq_home);
   return (
     <div>
-      <Header as="h2" className={"x10spaceBefore"}>
-        <Header.Content>
+      <Header as="h2" textAlign={"center"} className={"x10spaceBefore"}>
           <FormattedMessage {...messages.header} />
-        </Header.Content>
       </Header>
       <FaqList menu={props.menu} faqs={starred} />
     </div>
