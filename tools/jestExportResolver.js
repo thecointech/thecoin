d1d3cf290b3357c0550bafe5a81b7cc7c01afb9b
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-06-21 16:14:44 -0500

    fixed jest in shared/site-base/tx-processing

--- tools/jestExportResolver.js
+++ tools/jestExportResolver.js
@@ -1,7 +1,7 @@
 // temporary workaround while we wait for https://github.com/facebook/jest/issues/9771
 const resolver = require('enhanced-resolve').create.sync({
   conditionNames: ['require', 'node', 'default'],
-  extensions: ['.js', '.json', '.node', '.ts']
+  extensions: ['.js', '.json', '.node', '.ts', '.tsx']
 })
 
 module.exports = function (request, options) {
