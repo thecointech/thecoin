e23475e11f3c8185b2edc6bd36be7808d0771614
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-06-24 10:55:00 -0500

    Take2: Fixing local dates in MarketStatus

--- libs/utils-ts/src/MarketStatus.test.ts
+++ libs/utils-ts/src/MarketStatus.test.ts
@@ -1,10 +1,10 @@
 import { GetCalendar, NextOpenTimestamp } from './MarketStatus';
-import { DateTime } from 'luxon';
+import { DateTime, Settings } from 'luxon';
 
-const jan1st2019 = DateTime.fromObject({year: 2019, month: 1, day: 1, zone: "America/New_York"});
+Settings.defaultZoneName = "America/New_York";
+const jan1st2019 = DateTime.fromObject({year: 2019, month: 1, day: 1});
 
-test("Returns Valid Calendar", async () => {
-
+it("Returns Valid Calendar", async () => {
 	const calendar1 = await GetCalendar(jan1st2019.toJSDate());
 	expect(calendar1).toHaveProperty('days');
 
@@ -13,31 +13,29 @@ test("Returns Valid Calendar", async () => {
 	expect(calendar2).toHaveProperty('days');
 });
 
-test("Returns now if the market is currently open", async () => {
-
-	const testCurrOpen = DateTime.fromObject({year: 2019, month: 3, day: 29, hour: 12, minute: 30, zone: "America/New_York"});
+it("Returns now if the market is currently open", async () => {
+	const testCurrOpen = DateTime.fromObject({year: 2019, month: 3, day: 29, hour: 12, minute: 30});
 	expect(testCurrOpen.weekday == 5); // Check is friday
 	const nts = await NextOpenTimestamp(testCurrOpen.toJSDate());
 	expect(nts).toBe(testCurrOpen.toMillis());
 });
 
-test("Gives valid offset time", async () => {
+it("Gives valid offset time", async () => {
   const nts = await NextOpenTimestamp(jan1st2019.toJSDate());
-  const dt = DateTime.fromMillis(nts).setZone('America/New_York');
+  const dt = DateTime.fromMillis(nts);
 	expect(dt.toString()).toMatch("2019-01-02T09:32:00.000-05:00");
 });
 
-test("Gives valid future time without offset", async () => {
+it("Gives valid future time without offset", async () => {
 	const nts = await NextOpenTimestamp(jan1st2019.toJSDate(), 0);
-	const dt = DateTime.fromMillis(nts).setZone('America/New_York');
+	const dt = DateTime.fromMillis(nts);
 	expect(dt.toString()).toMatch("2019-01-02T09:30:00.000-05:00");
 });
 
-test("Getting next date when month ends on weekend", async () => {
-
-	const testMonthIncr = new Date("2019-03-29 16:30");
-	expect(testMonthIncr.getDay() == 4); // Check is saturday
-	const nts = await NextOpenTimestamp(testMonthIncr);
-	const ndate = new Date(nts);
-	expect(ndate.toUTCString()).toMatch("Mon, 01 Apr 2019 13:32:00 GMT");
+it("Gets next date when month ends on weekend", async () => {
+	const testMonthIncr = DateTime.fromSQL("2019-03-29 16:30");
+	expect(testMonthIncr.day == 5);
+	const nts = await NextOpenTimestamp(testMonthIncr.toJSDate());
+	const ndate = DateTime.fromMillis(nts);
+	expect(ndate.toString()).toMatch("2019-04-01T09:32:00.000-04:00");
 });
