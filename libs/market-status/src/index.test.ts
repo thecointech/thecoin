974c362f7034a859d1fdd930106a12efa3404a6c
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-06-29 15:11:30 -0500

    init commit, move MarketStatus to it's own lib

---
+++ libs/market-status/src/index.test.ts
@@ -0,0 +1,44 @@
+import { getCalendar, nextOpenTimestamp } from '.';
+import { DateTime, Settings } from 'luxon';
+import { describe } from '@thecointech/jestutils';
+
+Settings.defaultZoneName = "America/New_York";
+const jan1st2019 = DateTime.fromObject({year: 2019, month: 1, day: 1});
+
+describe("Live MarketStatus tests", () => {
+  it("Returns Valid Calendar", async () => {
+    const calendar1 = await getCalendar(jan1st2019);
+    expect(calendar1).toHaveProperty('days');
+
+    // Ensure cache returns valid value again
+    const calendar2 = await getCalendar(jan1st2019);
+    expect(calendar2).toHaveProperty('days');
+  });
+
+  it("Returns now if the market is currently open", async () => {
+    const testCurrOpen = DateTime.fromObject({year: 2019, month: 3, day: 29, hour: 12, minute: 30});
+    expect(testCurrOpen.weekday == 5); // Check is friday
+    const nts = await nextOpenTimestamp(testCurrOpen);
+    expect(nts).toBe(testCurrOpen.toMillis());
+  });
+
+  it("Gives valid offset time", async () => {
+    const nts = await nextOpenTimestamp(jan1st2019);
+    const dt = DateTime.fromMillis(nts);
+    expect(dt.toString()).toMatch("2019-01-02T09:32:00.000-05:00");
+  });
+
+  it("Gives valid future time without offset", async () => {
+    const nts = await nextOpenTimestamp(jan1st2019, 0);
+    const dt = DateTime.fromMillis(nts);
+    expect(dt.toString()).toMatch("2019-01-02T09:30:00.000-05:00");
+  });
+
+  it("Gets next date when month ends on weekend", async () => {
+    const testMonthIncr = DateTime.fromSQL("2019-03-29 16:30");
+    expect(testMonthIncr.day == 5);
+    const nts = await nextOpenTimestamp(testMonthIncr);
+    const ndate = DateTime.fromMillis(nts);
+    expect(ndate.toString()).toMatch("2019-04-01T09:32:00.000-04:00");
+  });
+}, !!process.env.TRADIER_API_KEY)
