763adf6516070c7f657a6a7437e24918837b1971
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2021-07-06 19:24:54 -0400

    Site landing Translations + clean

--- apps/site-landing/src/containers/HomePage/landscape/stickers/index.tsx
+++ apps/site-landing/src/containers/HomePage/landscape/stickers/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Grid, Header } from 'semantic-ui-react';
-import { FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage } from 'react-intl';
 
 import styles from './styles.module.less';
 import { Link } from 'react-router-dom';
@@ -9,26 +9,26 @@ export type Props = {
     Mobile: boolean;
 }
 
-const titleLeft = { id:"site.homepage.landscape.stickers.left.title", 
-                    defaultMessage:"You’re Wealthier",
-                    description:"The title for the homepage landscape left sticker"};
-const descriptionLeft = {   id:"site.homepage.landscape.stickers.left.description", 
-                            defaultMessage:"Your money is always earning. Make it earn for you.",
-                            description:"Description for sticker for the homepage landscape left sticker"};
-const linkLeft = {  id:"site.homepage.landscape.stickers.left.link", 
-                    defaultMessage:"Learn More",
-                    description:"Link name for sticker for the homepage landscape left sticker"};
-
-const titleRight = { id:"site.homepage.landscape.stickers.right.title", 
-                    defaultMessage:"Earth’s Healthier",
-                    description:"The title for the homepage landscape right sticker"};
-const descriptionRight = {   id:"site.homepage.landscape.stickers.right.description", 
-                            defaultMessage:"Be part of the solution. We offset our clients’ CO2.",
-                            description:"Description for sticker for the homepage landscape right sticker"};
-const linkRight = {  id:"site.homepage.landscape.stickers.right.link", 
-                    defaultMessage:"Learn More",
-                    description:"Link name for sticker for the homepage landscape right sticker"};
-
+const translations = defineMessages({
+    titleLeft : {
+      defaultMessage: 'You’re Wealthier',
+      description: 'site.homepage.landscape.stickers.left.title: The title for the homepage landscape left sticker'},
+    descriptionLeft : {
+      defaultMessage: 'Your money is always earning. Make it earn for you.',
+      description: 'site.homepage.landscape.stickers.left.description: Description for sticker for the homepage landscape left sticker'},
+    linkLeft : {
+      defaultMessage: 'Learn More',
+      description: 'site.homepage.landscape.stickers.left.link: Link name for sticker for the homepage landscape left sticker'},
+    titleRight : {
+      defaultMessage: 'Earth’s Healthier',
+      description: 'site.homepage.landscape.stickers.right.title: The title for the homepage landscape right sticker'},
+    descriptionRight : {
+      defaultMessage: 'Be part of the solution. We offset our clients’ CO2.',
+      description: 'site.homepage.landscape.stickers.right.description: Description for sticker for the homepage landscape right sticker'},
+    linkRight : {
+      defaultMessage: 'Learn More',
+      description: 'site.homepage.landscape.stickers.right.link: Link name for sticker for the homepage landscape right sticker'}
+  });
 
 export const Stickers = (props: Props) => {
 
@@ -42,22 +42,22 @@ export const Stickers = (props: Props) => {
             <Grid.Row columns={3}>
                 <Grid.Column className={ `${styles.card} x6spaceLeft` }>
                 <Header as='h4'>
-                    <FormattedMessage {...titleLeft} />
+                    <FormattedMessage {...translations.titleLeft} />
                 </Header>
                 <p>
-                    <FormattedMessage {...descriptionLeft} />
+                    <FormattedMessage {...translations.descriptionLeft} />
                 </p>
 
-                <Link to="/compare"><FormattedMessage {...linkLeft} /></Link>
+                <Link to="/compare"><FormattedMessage {...translations.linkLeft} /></Link>
                 </Grid.Column>
                 <Grid.Column className={ `${styles.card} x6spaceLeft` }>
                     <Header as='h4'>
-                        <FormattedMessage {...titleRight} />
+                        <FormattedMessage {...translations.titleRight} />
                     </Header>
                     <p>
-                        <FormattedMessage {...descriptionRight} />
+                        <FormattedMessage {...translations.descriptionRight} />
                     </p>
-                    <Link to="/wedomore"><FormattedMessage {...linkRight} /></Link>
+                    <Link to="/wedomore"><FormattedMessage {...translations.linkRight} /></Link>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
