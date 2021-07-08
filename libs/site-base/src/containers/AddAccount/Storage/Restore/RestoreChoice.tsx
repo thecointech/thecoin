f0b7b003f43dd0e355e65e081f142ddaddb8c7d9
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-08 15:07:51 -0500

    move all storage-based functionality into single folder

---
+++ libs/site-base/src/containers/AddAccount/Storage/Restore/RestoreChoice.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FormattedMessage, MessageDescriptor } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { Header } from 'semantic-ui-react';
+
+type Props = {
+  link: string,
+  imgSrc: string,
+  txt: MessageDescriptor
+}
+
+export const RestoreChoice = ({link, imgSrc, txt}: Props) =>
+  <Link to={link}>
+    <img src={imgSrc} />
+    <Header as={"h4"}><FormattedMessage {...txt} /></Header>
+  </Link>
