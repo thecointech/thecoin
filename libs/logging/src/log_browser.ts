14d1afa0f6c8fe47ec3995019b21ecb262c161e9
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-13 16:31:51 -0500

    tested logging with jest

--- libs/logging/src/log_browser.ts
+++ libs/logging/src/log_browser.ts
@@ -2,6 +2,6 @@ import { BunyanLogger } from './logger';
 import { init_browser } from './browser';
 
 export const log: BunyanLogger = init_browser(process.env.LOG_NAME ?? process.env.CONFIG_NAME ?? "dev");
-log.trace('Web logging initialized');
 if (process.env.LOG_LEVEL)
   log.level(Number(process.env.LOG_LEVEL));
+log.trace('Web logging initialized');
