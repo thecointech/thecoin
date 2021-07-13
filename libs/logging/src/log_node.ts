37b9c309f23477e44d5992af90c3f94169bfcaaa
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-13 16:31:51 -0500

    tested logging with jest

--- libs/logging/src/log_node.ts
+++ libs/logging/src/log_node.ts
@@ -2,6 +2,6 @@ import { BunyanLogger } from './logger';
 import { init_node } from './node';
 
 export const log: BunyanLogger = init_node(process.env.LOG_NAME ?? process.env.CONFIG_NAME ?? "dev");
-log.trace('Node logging initialized');
 if (process.env.LOG_LEVEL)
   log.level(Number(process.env.LOG_LEVEL));
+log.trace('Node logging initialized');
