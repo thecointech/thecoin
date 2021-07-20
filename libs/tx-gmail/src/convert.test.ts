373eb55045bc3b4314b20d1924a94e318ff0df01
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-20 14:54:34 -0500

    more test fixes

--- libs/tx-gmail/src/convert.test.ts
+++ libs/tx-gmail/src/convert.test.ts
@@ -1,20 +1,14 @@
 import { toDepositData } from "./convert";
-import { log } from '@thecointech/logging';
 //@ts-ignore Import directly from mocks to skip auth part.
 import emails from '../../../__mocks__/googleapis/emails.get.json';
 
 it('processes emails correctly', async () => {
-  const error = jest.spyOn(log, 'error').mockImplementation();
   for (const email of emails) {
     const eTransfer = toDepositData(email.data);
-    if (eTransfer) {
+    expect(eTransfer).toBeTruthy();
     // ensure these are all test emails;
-    expect(eTransfer?.name).toBe('TEST');
-
+    expect(['Not found', 'Some Person']).toContain(eTransfer!.name);
     // ensure we have sourceID;
     expect(eTransfer?.id).toBeTruthy();
-    }
-    //expect(eTransfer).not.toBeNull();
   }
-  expect(error).toHaveBeenCalledTimes(2);
 })
