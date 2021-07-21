74813b9fc72d71e9ff6146bc5dbb767dad1c5a28
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-21 16:28:27 -0500

    fixed basic render warning

--- apps/admin/src/app.tsx
+++ apps/admin/src/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById('react-app'));
 }
 
 render();
--- apps/admin/src/renderer.ts
+++ apps/admin/src/renderer.ts
@@ -26,9 +26,5 @@
  * ```
  */
 
-import './index.css';
-
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
-
 // Add this to the end of the existing file
 import './app';
