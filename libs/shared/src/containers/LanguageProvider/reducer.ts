42b3801131470fc1a3bd05d26c81d421447cf007
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-30 12:31:32 -0500

    removed commented code

--- libs/shared/src/containers/LanguageProvider/reducer.ts
+++ libs/shared/src/containers/LanguageProvider/reducer.ts
@@ -1,12 +1,7 @@
 /*
- *
  * LanguageProvider reducer
- *
  */
 import { BaseReducer } from '../../store/immerReducer';
-// import { useInjectReducer } from 'redux-injectors';
-// import { useDispatch } from 'react-redux';
-// import { bindActionCreators } from 'redux';
 import { LanguageProviderState, DEFAULT_LOCALE, IActions, Locale } from './types';
 import { ApplicationBaseState } from '../../types';
 
@@ -22,11 +17,3 @@ export class LanguageProviderReducer extends BaseReducer<IActions, LanguageProvi
       this.draftState.locale = locale;
     }
 }
-
-// const { reducer, actions} = LanguageProviderReducer.buildReducers(LanguageProviderReducer, initialState);
-// export const useLanguageProvider = () => {
-//   useInjectReducer({ key: LANGUAGE_KEY, reducer });
-// }
-// export const useLanguageProviderApi = () => {
-//   return (bindActionCreators(actions, useDispatch()));
-// };
