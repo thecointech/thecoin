cbdc1bb847226b58dc90810cbe9587ba51e3bb71
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2021-08-05 10:02:21 -0400

    Update index.tsx

--- libs/site-base/src/containers/AddAccount/CreateExistingSwitch/index.tsx
+++ libs/site-base/src/containers/AddAccount/CreateExistingSwitch/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header, Divider } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
-import { isWeb3Enabled } from '@thecointech/shared/utils/detection';
+//import { isWeb3Enabled } from '@thecointech/shared/utils/detection';
 import styles from './styles.module.less';
 import { Decoration } from '../Decoration';
 import { defineMessages, FormattedMessage } from 'react-intl';
@@ -29,14 +29,14 @@ const translations = defineMessages({
 export const CreateExistingSwitch = (props: Props) => {
 
   const {url} = props;
-  const doConnect = isWeb3Enabled();
-  const createUrl = doConnect
-    ? `${url}connect/create`
-    : `${url}generate/intro`
+  //const doConnect = isWeb3Enabled();
+  //const createUrl = doConnect
+  //  ? `${url}connect/create`
+  //  : `${url}generate/intro`
 
-  const existingUrl = doConnect
-    ? `${url}connect/exist`
-    : `${url}restore/`
+  //const existingUrl = doConnect
+  //  ? `${url}connect/exist`
+  //  : `${url}restore/`
 
   return (
     <>
@@ -47,11 +47,11 @@ export const CreateExistingSwitch = (props: Props) => {
         <Header as="h1" className={` x12spaceAfter`}>
           <FormattedMessage {...translations.title} />
         </Header>
-        <ButtonPrimary as={NavLink} to={createUrl} className={styles.button}>
+        <ButtonPrimary as={NavLink} to={`${url}generate/intro`} className={styles.button}>
           <FormattedMessage {...translations.buttonNewAccount} />
         </ButtonPrimary>
         <Divider horizontal>Or</Divider>
-        <ButtonSecondary as={NavLink} to={existingUrl} className={styles.button}>
+        <ButtonSecondary as={NavLink} to={`${url}restore/`} className={styles.button}>
           <FormattedMessage {...translations.buttonAccount} />
         </ButtonSecondary>
       </div>
