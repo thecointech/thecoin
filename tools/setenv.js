17abe9a1975afc512fe9c596778156bd621e8cba
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-08-27 09:36:15 -0500

    split env variables into public/private to prevent duplication, and allow CI to build (if not to deploy)

--- tools/setenv.js
+++ tools/setenv.js
@@ -1,29 +1,41 @@
 const { basename, join } = require('path');
 const { existsSync } = require('fs');
 
-function getEnvFile(cfgName) {
+function getEnvFiles(cfgName) {
   const filename = cfgName || process.env.CONFIG_NAME || (
     process.env.NODE_ENV == "production"
       ? "prod"
       : "development"
   );
-
+  const files = [];
   // Does the user have files on the system
   const systemFolder = process.env.THECOIN_ENVIRONMENTS;
   if (systemFolder) {
-    const systemFile = join(systemFolder, `${filename}.env`);
-    if (existsSync(systemFile)) return systemFile;
+    const systemFile = join(systemFolder, `${filename}.private.env`);
+    if (existsSync(systemFile)) files.push(systemFile);
   }
 
   // If none found, is there any in the local repo folder?
-  const repoFile = join(__dirname, '..', 'environments', `${filename}.env`);
-  if (existsSync(repoFile)) return repoFile;
+  const repoFile = join(__dirname, '..', 'environments', `${filename}.public.env`);
+  if (existsSync(repoFile)) files.push(repoFile);
+
 
   // None found, throw
-  throw new Error(`Missing cfg file for: ${cfgName} (${repoFile})`);
+  if (files.length == 0) {
+    throw new Error(`Missing cfg files for: ${cfgName} (${repoFile})`);
+  }
+
+  // Beta versions share a lot with non-beta environments, so we merge them together
+  if (cfgName.endsWith("beta")) {
+    const nonBeta = getEnvFiles(cfgName.slice(0, -4))
+    files.push(...getEnvFiles(nonBeta))
+  }
 }
 
-require('dotenv').config({path: getEnvFile()});
+// Load all environment files.
+const de = require('dotenv')
+const files = getEnvFiles();
+files.forEach(path => de.config({path}))
 
 //  Set default name for logging
 if (!process.env.LOG_NAME) {
