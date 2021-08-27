82f286e7c08b241508cc9bbd649aa68ed8dcd7e0
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-08-27 15:58:51 -0500

    disable OOO evalution of eTransfers, as it confused the logs

--- apps/tx-processor/src/etransfer/index.ts
+++ apps/tx-processor/src/etransfer/index.ts
@@ -2,15 +2,17 @@ import { getIncompleteActions } from '@thecointech/broker-db';
 import { TheCoin } from '@thecointech/contract';
 import { Processor } from '@thecointech/tx-etransfer';
 import { RbcApi } from '@thecointech/rbcapi';
+import type { TypedActionContainer } from '@thecointech/tx-statemachine';
 
 export async function processUnsettledETransfers(contract: TheCoin, bank: RbcApi)
 {
   let incomplete = await getIncompleteActions("Sell");
   const processor = Processor(contract, bank);
 
-  const resumedActions = incomplete.map(async ic => {
-    return processor.execute(null, ic);
-  })
-
-  return Promise.all(resumedActions);
+  const r: TypedActionContainer<"Sell">[] = []
+  for (const action of incomplete) {
+    const executed = await processor.execute(null, action);
+    r.push(executed);
+  }
+  return r;
 }
