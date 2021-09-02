4a875f13e3422365d0a5e8aa47697a0ba1c91c76
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-09-02 10:57:29 -0500

    fix the build (not tested with devlive/prodtest etc)

--- __mocks__/mock_node.js
+++ __mocks__/mock_node.js
@@ -7,16 +7,22 @@
 
 const Module = require('module').Module;
 const ehr = require('enhanced-resolve');
+const fs = require('fs');
+const path = require('path');
+
+const allMocks = fs.readdirSync(__dirname).map(f => path.parse(f).name);
+
 
 //
 // Allows using CONFIG_NAME-specific implementations returned
 // from packages, and can also include mocks from __mocks__ for
 // specified modules
 //
-function enhancedModuleResolve(...toMock) {
+function enhancedModuleResolve(toMock) {
   // NOTE: __dirname is where I keep some external mocks
   const mockResolver = ehr.create.sync({
       extensions: ['.js', '.json', '.node', '.ts', '.tsx'],
+      conditionNames: ['require', 'node', 'default'],
       modules: [__dirname, 'node_modules']
     })
   const coinResolver = ehr.create.sync({
@@ -24,6 +30,8 @@ function enhancedModuleResolve(...toMock) {
     extensions: ['.js', '.json', '.node', '.ts', '.tsx'],
   })
 
+  const canMock = request => allMocks.find(m => request.startsWith(m))
+  const shouldMock = request => (toMock?.length === 0 || toMock?.find(m => request.startsWith(m)))
   // NOTE: If we redirect all resolutions, breakpoints stop working
   // Just redirect my own packages (and whichever external API's
   // I want to have mocked) and everything works correctly
@@ -31,14 +39,14 @@ function enhancedModuleResolve(...toMock) {
   Module._resolveFilename = (request, parent, isMain) => {
     return request.startsWith('@thecointech') // our packages include their own mocks
       ? coinResolver(parent.path, request)
-      : (toMock.length == 0 || toMock.find(m => request.startsWith(m))) // any explicitly mocked external packages
+      : canMock(request) && shouldMock(request) // any explicitly mocked external packages
         ? mockResolver(parent.path, request)
         : oldResolve(request, parent, isMain) // fallback to default resolver
   }
 }
 
-const useAllMocks = () => enhancedModuleResolve();
-const useSomeMocks = (...modules) => enhancedModuleResolve(...modules);
+const useAllMocks = () => enhancedModuleResolve([]);
+const useSomeMocks = (toMock) => enhancedModuleResolve(toMock);
 
 ////////////////////////////////////////////////////////////////
 
@@ -56,7 +64,7 @@ switch (process.env.CONFIG_NAME) {
     // Dev live is internally connected, but all external connections are mocked
     //
     console.warn('--- Injecting external TC mocks ---');
-    useSomeMocks("googleapis", "google-auth-library");
+    useSomeMocks(["googleapis", "google-auth-library"]);
     break;
   default:
     //
