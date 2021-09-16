6163cd462968d832fc2df6d46cfe2258ee86f80e
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-09-16 16:03:50 -0500

    Improved validity test showed our sample data was actually not valid

--- libs/utils-ts/src/VerifiedSale.ts
+++ libs/utils-ts/src/VerifiedSale.ts
@@ -6,7 +6,8 @@ import { BuildVerifiedAction } from "./VerifiedAction";
 
 // We cannot use the following characters in the question/answer
 // Invalid characters: < or >, { or }, [ or ], %, &, #, \ or "
-export const invalidChars = /[\s\<\>\{\}\[\]\%\&\#\\\"]/g;
+export const invalidQnA     = /[\<\>\{\}\[\]\%\&\#\\\"\s]/g;
+export const invalidMessage = /[\<\>\{\}\[\]\%\&\#\\\"]/g;
 
 export const BuildVerifiedSale = async (
   eTransfer: ETransferPacket,
