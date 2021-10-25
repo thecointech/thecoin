d63e17f370521c615bec6add3eb54b331a472e4f
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-10-25 11:24:15 -0500

    deploy to production

--- ceramic/update-ipfs.ps1
+++
@@ -1,7 +0,0 @@
-$ENV:RESOURCE_ID='...'
-
-az container export -g $ENV:RESOURCE_ID --name ipfs-daemon --file ipfs-daemon.yaml
-az container create -g $ENV:RESOURCE_ID -f ./ipfs-daemon.yaml
-
-az container export -g $ENV:RESOURCE_ID --name js-ceramic --file js-ceramic.yaml
-az container create -g $ENV:RESOURCE_ID -f ./js-ceramic.yaml
