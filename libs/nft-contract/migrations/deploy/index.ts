acb1216ad89a3efaa277c941db37eeb4393a2067
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-10-30 17:07:18 -0500

    working deployment (tested on local only)

--- libs/nft-contract/migrations/deploy/index.ts
+++ libs/nft-contract/migrations/deploy/index.ts
@@ -1,9 +1,9 @@
 import { getSigner } from '@thecointech/signers';
-import { TheGreenNFTL1Instance, TheGreenNFTL2Instance } from '../types';
-import {getContractL1} from './deploy_L1'
-import {getContractL2} from './deploy_L2'
+import { TheGreenNFTL1Instance, TheGreenNFTL2Instance } from '../types/index';
+import { getContractL1 } from './deploy_L1'
+import { getContractL2 } from './deploy_L2'
 
-let deployed: TheGreenNFTL2Instance|TheGreenNFTL1Instance|undefined = undefined;
+let deployed: TheGreenNFTL2Instance | TheGreenNFTL1Instance | undefined = undefined;
 export async function getContract(deployer: Truffle.Deployer, network: String) {
   if (!deployed) {
     const minter = await getSigner("NFTMinter");
