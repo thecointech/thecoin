540a9fcd515163578f65f77d077f4f108ae5b59f
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-01 16:46:59 -0500

    poor-mans A/B testing setup.  Unfortunately results in content flash, so ditching for now

--- apps/site-nft-landing/src/app.js
+++ apps/site-nft-landing/src/app.js
@@ -4,7 +4,7 @@ import "./index.html";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,3 +20,30 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 const analytics = getAnalytics();
+
+// Track when entering iframe.  Cheap 'n dirty hack to get conversion data on free tier of prefinery.
+// main document must be focused in order for window blur to fire when the iframe is interacted with.
+// There's still an issue that if user interacts outside of the page and then click iframe first without clicking page, the following logic won't run. But since the OP is only concerned about first click this shouldn't be a problem.
+var monitor = setInterval(function () {
+  var elem = document.activeElement;
+  if (elem && elem.tagName?.toLowerCase() == 'iframe') {
+    clearInterval(monitor);
+    // Is top or bottom iframe?
+    const iframeIdx = document.activeElement.parentElement.parentElement.className === "jacketForm"
+      ? 1
+      : 2;
+    statsig.logEvent(`iframeClicked${iframeIdx}`);
+    logEvent(analytics, `iframeClicked${iframeIdx}`);
+  }
+}, 100);
+
+// // Simple A/B test on different copy variants.
+// document.addEventListener("DOMContentLoaded", async () => {
+//   // Ensure window has finished loading.
+//   await window.testsInit;
+//   console.log("Beginning tests");
+//   if (statsig.checkGate('textvariants') || true) {
+//     const span = document.getElementById("headerL1");
+//     span.textContent = "A fair-trade jacket"
+//   }
+// });
