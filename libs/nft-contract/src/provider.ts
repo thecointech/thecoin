3bd385554ed0f2dd4e430c73e953259510800334
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-02 16:29:08 -0500

    working connection to NFTContract

--- libs/nft-contract/src/provider.ts
+++ libs/nft-contract/src/provider.ts
@@ -2,13 +2,18 @@ import { providers } from "ethers";
 
 export const getProvider = () => {
 
-  if (!process.env.DEPLOY_NETWORK)
+  const network = process.env.DEPLOY_POLYGON_NETWORK == "polygon-mumbai"
+    ? "maticmum"
+    : process.env.DEPLOY_POLYGON_NETWORK;
+
+  const projectId = process.env.INFURA_PROJECT_ID;
+  if (!network)
     throw new Error("Missing deploy network, cannot connect to blockchain");
 
-  if (process.env.NODE_ENV === "production" && !process.env.INFURA_PROJECT_ID)
-    throw new Error(`Missing INFURA project ID, cannot connect to ${process.env.DEPLOY_NETWORK}`);
+  if (process.env.NODE_ENV === "production" && !projectId)
+    throw new Error(`Missing INFURA project ID, cannot connect to ${network}`);
 
-  return (process.env.INFURA_PROJECT_ID)
-    ? new providers.InfuraProvider(process.env.DEPLOY_NETWORK, process.env.INFURA_PROJECT_ID)
+  return (projectId)
+    ? new providers.InfuraProvider(network, projectId)
     : new providers.JsonRpcProvider(`http://localhost:${process.env.DEPLOY_NETWORK_PORT}`);
 }
