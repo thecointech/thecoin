f01b01127684b3b2d3d8cc89a267a2eb6052b7c9
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-12 16:59:54 -0600

    fix bad path on site-landing

--- apps/site-landing/internals/server.mjs
+++ apps/site-landing/internals/server.mjs
@@ -1,14 +1,16 @@
 
 import { run } from "@thecointech/site-base/internal/server";
-import { resolve, dirname } from "path";
+import { join, dirname } from "path";
 import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
 
-const __dirname = dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 run(app => {
 
   // serve CSV data needed by comparison graph
-  const csvpath = resolve(__dirname, '..', 'src', 'sp500_monthly.csv');
-  console.log(`1: ${__dirname}\n2: ${csvpath}`);
+  const csvpath = join(__dirname, '..', 'src', 'sp500_monthly.csv');
   const sp500buffer = readFileSync(csvpath);
   const sp500string = sp500buffer.toString();
 
