93e957c524889cb87f31921586e1f8e199b9dd36
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-13 14:11:11 -0600

    fix storybook build

---
+++ libs/site-semantic-theme/internal/webpack.less.js
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////
+//
+// This file defines the LESS loaders common to our site build and
+// to our storybook webpack based at the root
+//
+const { paths, modifyVars } = require('./vars.js');
+
+module.exports = {
+
+  css_module_loader: {
+    // CSS/LESS module matching
+    test: /.*\.module\.(le|c)ss$/,
+    use: [
+      {
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          sourceMap: true,
+          modules: true,
+        },
+      },
+      {
+        loader: 'less-loader',
+        options: {
+          sourceMap: true,
+          lessOptions: {
+            modifyVars,
+          }
+        },
+      },
+    ],
+  },
+  semantic_less_loader: {
+    // Explicitly process Semantics LESS files
+    test: /semantic\.less$/,
+    use: [
+      {
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          sourceMap: true,
+        },
+      },
+      {
+        loader: 'less-loader',
+        options: {
+          sourceMap: true,
+          lessOptions: {
+            paths,
+            modifyVars,
+          }
+        },
+      },
+    ],
+  }
+}
