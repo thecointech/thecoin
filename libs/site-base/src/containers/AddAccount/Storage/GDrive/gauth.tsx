251521b8c3aa428f96f7892f1cb6a6a0505244a5
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-15 17:29:58 -0600

    added basic page for storing accounts if cross-window comms is not available

--- libs/site-base/src/containers/AddAccount/Storage/GDrive/gauth.tsx
+++ libs/site-base/src/containers/AddAccount/Storage/GDrive/gauth.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import { getUrlParameterByName } from '../../../../utils/localState';
 import { IWindow } from './googleUtils';
+import { NoTabStoreList } from './NoTabStoreList';
 
 const translations = defineMessages({
   tokenWaiting: {
@@ -13,14 +14,12 @@ const translations = defineMessages({
   tokenFailure: {
     defaultMessage: 'This page expects to recieve google authentication information, but none was found.',
     description: 'app.storeOnline.google.tokenFailure'},
-  cookieSuccess: {
-    defaultMessage: 'Authentication passed, please return to your account and complete connecting to google',
-    description: 'app.storeOnline.google.cookieSuccess'}
 });
 
 export const GAuth = () => {
 
   const [message, setMessage] = useState(translations.tokenWaiting);
+  const [code, setCode] = useState<MaybeString>();
 
   useEffect(() => {
     const code = getUrlParameterByName('code');
@@ -33,16 +32,17 @@ export const GAuth = () => {
         // the message lets people know  they can ignore this page.
         window.close()
       } else {
-        // If we can't call that function, set the cookie data instead
-        document.cookie = `gauth=${encodeURI(code)}; path=/`;
-				setMessage(translations.cookieSuccess);
+        // If we can't call that function, show the page
+        // to allow uploading any local account
+        setCode(code);
       }
-			//setTimeout(() => window.close(), 1500);
 
     } else {
       setMessage(translations.tokenFailure);
     }
   }, [])
 
-    return <FormattedMessage {...message} />;
+  return code
+    ? <NoTabStoreList code={code} />
+    : <FormattedMessage {...message} />;
 }
