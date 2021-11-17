c88d61634f0d78396f0685068ec6b6a480ec3cac
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-17 16:57:11 -0600

    fixed deployment issues that meant truffle develop 'lost' it's deployed contract

---
+++ libs/contract-core/migrations/deploy.ts
@@ -0,0 +1,27 @@
+import { getSigner } from '@thecointech/signers';
+
+export const getName = (network: string) =>
+  network === 'polygon'
+  ? "TheCoinL2"
+  : "TheCoinL1";
+
+// ChildChainManager calls the deposit function on the polygon chain
+// See https://static.matic.network/network/testnet/mumbai/index.json
+const childChainManager_mumbai = '0xb5505a6d998549090530911180f38aC5130101c6';
+
+// The address of the Goerli bridge contract
+// see MintableERC721PredicateProxy https://static.matic.network/network/testnet/mumbai/index.json
+const goerliPredicate = '0x37c3bfC05d5ebF9EBb3FF80ce0bd0133Bf221BC8';
+
+export async function getArguments(network: String) {
+  const theCoin = await getSigner("TheCoin");
+  const theCoinAddress = await theCoin.getAddress();
+  return [
+    theCoinAddress,
+    network === 'polygon'
+      // ChildChainManager calls the deposit function on the polygon chain
+      // See https://static.matic.network/network/testnet/mumbai/index.json
+      ? childChainManager_mumbai
+      : goerliPredicate
+  ]
+}
