6c4496d7836325edb12b129efa7a5f9ab2bf7918
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-11-26 14:52:04 -0600

    deleting intermediary layer

--- apps/site-landing/src/containers/MainNavigation/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { MainNavigationGreaterThanMobile } from './MainNavigationGreaterThanMobile';
-
-export const MainNavigation = () => <MainNavigationGreaterThanMobile />
