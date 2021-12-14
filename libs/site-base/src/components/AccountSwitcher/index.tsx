d38006998b37c4be2a7ac43d25f7353804be76a4
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-14 10:33:39 -0600

    fixing menu items

--- libs/site-base/src/components/AccountSwitcher/index.tsx
+++ libs/site-base/src/components/AccountSwitcher/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, DropdownItemProps } from "semantic-ui-react";
 import { NavLink, Link } from "react-router-dom";
 import { AccountMap } from "@thecointech/shared/containers/AccountMap";
 import { getAvatarLink } from '@thecointech/shared/components/Avatars';
@@ -18,19 +18,6 @@ const addAccount = defineMessage({
   defaultMessage: "Add an Account",
   description: "Title for the Add an Account in the menu"
 });
-const see = defineMessage({
-  defaultMessage: "See",
-  description: "Title for the See in the menu"
-});
-const settings = defineMessage({
-  defaultMessage: "Settings",
-  description: "Title for the Settings in the menu"
-});
-const signout = defineMessage({
-  defaultMessage: "Sign Out",
-  description: "Title for the Sign Out in the menu"
-});
-
 
 export const AccountSwitcher = () => {
 
@@ -41,31 +28,21 @@ export const AccountSwitcher = () => {
   // Build the title of the dropdown - LOGIN text or avatar and account name
   const intl = useIntl();
   const trigger = activeAccount
-    ? <span><img src={getAvatarLink("14")} className={styles.avatars} /> {activeAccount.name.substring(0, 10)}</span>
-    : <span>{intl.formatMessage(titleMsg)}</span>
-
+    ? <><img src={getAvatarLink("14")} className={styles.avatars} /><span>{activeAccount.name.substring(0, 16)}</span></>
+    : intl.formatMessage(titleMsg)
+  const onClick = (_: unknown, data: DropdownItemProps) => accountsApi.setActiveAccount(data.address)
   return (
     <Dropdown trigger={trigger} className={styles.dropdown} >
       <Dropdown.Menu>
         <Dropdown.Header>
           <FormattedMessage {...myAccounts} />
         </Dropdown.Header>
-        <ActiveAccount account={activeAccount} />
         {
           accounts
-            .filter(account => account.address !== activeAccount?.address)
             .sort((a, b) => a.name.localeCompare(b.name))
             .map(account => (
-              <Dropdown.Item
-                trigger={trigger}
-                key={account.address}
-                address={account.address}
-                text={account.name}
-                as={Link}
-                onClick={(_, data) => accountsApi.setActiveAccount(data.address)}
-                to="/" />
-            )
-            )
+              <AccountItem account={account} onClick={onClick} active={account.address == activeAccount?.address}/>
+            ))
         }
         <Dropdown.Divider />
         <Dropdown.Item key='add' as={NavLink} to="/addAccount/">
@@ -77,23 +54,18 @@ export const AccountSwitcher = () => {
 }
 
 type ActiveProps = {
-  account: AccountState | undefined
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>, data: DropdownItemProps) => void,
+  account: AccountState | undefined,
+  active: boolean,
 }
-const ActiveAccount = ({ account }: ActiveProps) =>
+const AccountItem = ({active, onClick, account}: ActiveProps) =>
   account
-    ? <Dropdown.Item key={account.name}>
-      <Dropdown trigger={<span><img src={getAvatarLink("14")} className={styles.avatars} /> {account.name.substring(0, 14) + '...'}</span>}>
-        <Dropdown.Menu direction='right'>
-          <Dropdown.Item key="see" account={account.name} as={Link} to="/" >
-            <FormattedMessage {...see} />
-          </Dropdown.Item>
-          <Dropdown.Item key="sett" as={NavLink} to="settings" >
-            <FormattedMessage {...settings} />
-          </Dropdown.Item>
-          <Dropdown.Item key="sout" as={NavLink} to="signout" >
-            <FormattedMessage {...signout} />
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </Dropdown.Item>
+    ? <Dropdown.Item
+        active={active}
+        key={account.address}
+        address={account.address}
+        text={account.name}
+        as={Link}
+        onClick={onClick}
+        to="/" />
     : null
