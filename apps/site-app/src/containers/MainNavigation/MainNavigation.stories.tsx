b9751f375df630e0423be2ffcc2dee09aaf20bff
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-15 10:38:26 -0600

    app main nav working

--- apps/site-app/src/containers/MainNavigation/MainNavigation.stories.tsx
+++ apps/site-app/src/containers/MainNavigation/MainNavigation.stories.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import { MainNavigation } from '.';
-import { withAccounts, withLanguageProvider, withMediaContext } from '@thecointech/storybookutils';
+import { withAccounts, withLanguageProvider, withMediaContext, withReducer } from '@thecointech/storybookutils';
 import styles from '../App/styles.module.less';
+import { SidebarItemsReducer } from '@thecointech/shared/containers/PageSidebar/reducer';
 
 export default {
   title: 'App/Navigation',
   component: MainNavigation,
   decorators: [
+    withReducer(SidebarItemsReducer),
     withMediaContext,
     withLanguageProvider,
     withAccounts(),
   ]
 } as Meta;
 
+SidebarItemsReducer.initialize({
+  items: {
+    header: null,
+    links: [
+      {
+        "name": { "id": 2, "defaultMessage": "Home" },
+        "to": "/",
+        "icon": "home"
+      },
+      {
+        "name": { "id": 3, "defaultMessage": "Top up Balance" },
+        "to": "/balance",
+        "icon": "arrow circle up"
+      },
+    ]
+  }
+})
 export const Navigation = () => (
   <div id={styles.app}>
     <MainNavigation />
--- apps/site-app/src/containers/MainNavigation/index.tsx
+++ apps/site-app/src/containers/MainNavigation/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Menu, Dropdown, Icon, SemanticICONS } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
 import { AccountSwitcher } from '@thecointech/site-base/components/AccountSwitcher';
-import { defineMessages, FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 import { HeaderLink } from '@thecointech/site-base/components/HeaderLink';
 import { LanguageSwitcher } from '@thecointech/site-base/containers/LanguageSwitcher';
 import { NavLink } from 'react-router-dom';
 import styles from './styles.module.less';
 import { ModalOperation } from '@thecointech/shared/containers/ModalOperation';
+import { SidebarItemsReducer } from '@thecointech/shared/containers/PageSidebar/reducer';
 
 const menuItems = defineMessages({
   home: {
@@ -23,78 +24,63 @@ const menuItems = defineMessages({
   }
 });
 
-const mobileItems = defineMessages({
-  home: {
-    defaultMessage: 'Overview',
-    icon: 'home',
-    description: 'app.MainNavigation.home: Title for the entry in the sidebar menu'
-  },
-  transferin: {
-    defaultMessage: 'Top up balance',
-    icon: 'arrow circle up',
-    description: 'app.MainNavigation.transferin: Title for the entry in the sidebar menu'
-  },
-  makepayments: {
-    defaultMessage: 'Make payments',
-    icon: 'arrow circle right',
-    description: 'app.MainNavigation.makepayments: Title for the entry in the sidebar menu'
-  },
-  settings: {
-    defaultMessage: 'Settings',
-    icon: 'setting',
-    description: 'app.MainNavigation.settings: Title for the entry in the sidebar menu'
-  },
-  contact: {
-    defaultMessage: 'Contact Us',
-    icon: 'envelope outline',
-    description: 'app.MainNavigation.contact: Title for the entry in the sidebar menu'
-  }
-});
-
 export const MainNavigation = () => {
-  const [modalVisible, setModalVisible] = React.useState(false);
   return (
     <>
       <div className={styles.background} />
       <div className={styles.mainMenu}>
-        <Menu text className={styles.mainMenu} >
-          <Menu.Item>
-            <a href={process.env.URL_SITE_LANDING} className={styles.logoLink}>
-              <div className={styles.logo} />
-            </a>
-          </Menu.Item>
-          <HeaderLink to="/" exact className="onlyBigScreen">
-            <FormattedMessage {...menuItems.home} />
-          </HeaderLink>
-          <HeaderLink to="/help" exact>
-            <FormattedMessage {...menuItems.help} />
-          </HeaderLink>
-          <HeaderLink to="/contact" exact className="onlyBigScreen">
-            <FormattedMessage {...menuItems.contact} />
-          </HeaderLink>
-        </Menu>
-        <Menu text>
-          <Menu.Item>
-            <AccountSwitcher />
-          </Menu.Item>
-          <Menu.Item>
-            <LanguageSwitcher />
-          </Menu.Item>
-          <Menu.Item>
-            <Icon name="content" onClick={() => setModalVisible(true)} className={`onlySmallScreen ${styles.burgerIcon}`} />
-            <ModalOperation isOpen={modalVisible} closeIconFct={() => setModalVisible(false)}>
-              <Menu vertical id={styles.mobileMenu}>
-                {Object.entries(mobileItems).map(([key, msg]) =>
-                  <Menu.Item as={NavLink} key={key} to={`/${key}`} onClick={() => setModalVisible(false)}>
-                    <Icon name={msg.icon as SemanticICONS} size={"big"} className={styles.mobileIcon}/>
-                    <FormattedMessage {...msg} />
-                  </Menu.Item>
-                )}
-              </Menu>
-            </ModalOperation>
-          </Menu.Item>
-        </Menu>
+        <LeftMenuItems />
+        <RightMenuItems />
       </div>
     </>
   )
 };
+
+const LeftMenuItems = () => (
+  <Menu text className={styles.mainMenu} >
+    <Menu.Item>
+      <a href={process.env.URL_SITE_LANDING} className={styles.logoLink}>
+        <div className={styles.logo} />
+      </a>
+    </Menu.Item>
+    <HeaderLink to="/" exact className="onlyBigScreen">
+      <FormattedMessage {...menuItems.home} />
+    </HeaderLink>
+    <HeaderLink to="/help" exact>
+      <FormattedMessage {...menuItems.help} />
+    </HeaderLink>
+    <HeaderLink to="/contact" exact className="onlyBigScreen">
+      <FormattedMessage {...menuItems.contact} />
+    </HeaderLink>
+  </Menu>
+)
+
+const RightMenuItems = () => {
+  // Mirror the sidebar menu items in our burger menu
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const sidebarData = SidebarItemsReducer.useData();
+  const intl = useIntl();
+  return (
+    <Menu text>
+      <Menu.Item>
+        <AccountSwitcher />
+      </Menu.Item>
+      <Menu.Item>
+        <LanguageSwitcher />
+      </Menu.Item>
+      <Menu.Item>
+        <Icon name="content" onClick={() => setModalVisible(true)} className={`onlySmallScreen ${styles.burgerIcon}`} />
+        <ModalOperation isOpen={modalVisible} closeIconFct={() => setModalVisible(false)}>
+          <Menu vertical id={styles.mobileMenu}>
+            {Object.entries(sidebarData.items.links).map(([key, link]) =>
+              <Menu.Item as={NavLink} key={key} to={link.to} exact onClick={() => setModalVisible(false)}>
+                <Icon name={link.icon} size={"big"} className={styles.mobileIcon} />
+                {intl.formatMessage(link.name)}
+              </Menu.Item>
+            )}
+          </Menu>
+        </ModalOperation>
+      </Menu.Item>
+    </Menu>
+  )
+}
