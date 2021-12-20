c6d2bec63df38c1b258fb1e8e33e76c36ac70779
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-20 09:33:05 -0600

    landscape background desktop

--- apps/site-landing/src/containers/HomePage/landscape/LandscapeGreaterThanMobile/Background.tsx
+++ apps/site-landing/src/containers/HomePage/landscape/LandscapeGreaterThanMobile/Background.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import sky from '../images/sky.svg';
-import landscapeSkyCloudsHigh from '../images/cloudsBackgroundHigh.svg';
-import landscapeSkyCloudsLeft from '../images/cloudsLeft.svg';
-import landscapeSkyCloudsRight from '../images/cloudsRight.svg';
-import Birds from '../images/birds.svg';
-import Sun from '../images/sun.svg';
-import SunAura from '../images/sunAura.svg';
-import landscapeGreenPart from '../images/landscape.svg';
+import sky from './images/sky.svg';
+import landscapeSkyCloudsHigh from './images/cloudsBackgroundHigh.svg';
+import landscapeSkyCloudsLeft from './images/cloudsLeft.svg';
+import landscapeSkyCloudsRight from './images/cloudsRight.svg';
+import Birds from './images/birds.svg';
+import Sun from './images/sun.svg';
+import SunAura from './images/sunAura.svg';
+import landscapeGreenPart from './images/landscape.svg';
 import styles from './background.module.less';
 
 export const Background = () => (
   <div className={styles.background}>
-    <img className={styles.birds} src={Birds} />
     <img className={styles.sky} src={sky} />
     <img className={styles.cloudsHigh} src={landscapeSkyCloudsHigh} />
     <img className={styles.cloudsLeft} src={landscapeSkyCloudsLeft} />
     <img className={styles.cloudsRight} src={landscapeSkyCloudsRight} />
     <img className={styles.sunAura} src={SunAura} />
     <img className={styles.sun} src={Sun} />
+    <img className={styles.birds} src={Birds} />
     <img className={styles.greenery} src={landscapeGreenPart} />
   </div>
 )
