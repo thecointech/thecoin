37366c99669cbec13a65b4624a9df6b3bc00f14e
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-21 09:56:16 -0600

    building time-out must be infinite recursion somehow - adding limit

--- libs/site-semantic-theme/internal/vars.js
+++ libs/site-semantic-theme/internal/vars.js
@@ -6,7 +6,13 @@ const basePath = path.join(siteBaseRoot, 'src', 'semantic', 'na', 'na');
 const findSitePath = (fullPath) => {
   const parsed = path.parse(fullPath);
   if (parsed.base == 'src') return parsed.dir;
+  if (!parsed.dir) return false;
   return findSitePath(parsed.dir);
+}
+
+const getSemanticPath = (rsrcPath)  => {
+  const sitePath = findSitePath(rsrcPath) || process.cwd();
+  return path.join(sitePath, 'src', 'semantic');
 }
 
 // Vars to pass to LESS
@@ -14,6 +20,6 @@ module.exports = (resourcePath) => ({
   paths: [basePath],
   modifyVars: {
     projectRoot: `"${siteBaseRoot}"`,
-    siteFolder: `"${path.join(findSitePath(resourcePath), 'src', 'semantic')}"`,
+    siteFolder: `"${getSemanticPath(resourcePath)}"`,
   }
 })
