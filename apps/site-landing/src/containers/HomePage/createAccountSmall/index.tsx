0dbbe9e118b173d4a3d4b11eb5a5c0e017135aa4
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-21 10:20:15 -0600

    CTA scalable

--- apps/site-landing/src/containers/HomePage/createAccountSmall/index.tsx
+++ apps/site-landing/src/containers/HomePage/createAccountSmall/index.tsx
@@ -1,32 +1,22 @@
 import * as React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
-import { defineMessages, FormattedMessage } from 'react-intl';
+import { Header } from 'semantic-ui-react';
+import { defineMessage, FormattedMessage } from 'react-intl';
 import styles from './styles.module.less';
 import { CreateAccountButton } from '../../../components/AppLinks/CreateAccount';
 
-const translations = defineMessages({
-  title : {
-      defaultMessage: 'TheCoin is a revolutionary new kind of account.',
-      description: 'site.homepage.createAccountSmall.title: Title / content for the small create account banner'}
-  });
+const title = defineMessage({
+  defaultMessage: 'TheCoin is a revolutionary new kind of account.',
+  description: 'site.homepage.createAccountSmall.title: Title / content for the small create account banner',
+});
 
-export const CreateAccountSmall = () => {
-
-  return (
-      <Grid textAlign='center' verticalAlign='middle' padded className={ `${styles.content}` }>
-        <Grid.Row>
-          <Grid.Column>
-            <Header as='h3'>
-                <FormattedMessage {...translations.title} />
-              </Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <CreateAccountButton size="large" />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-  );
-}
+export const CreateAccountSmall = () => (
+  <div className={styles.content}>
+    <Header as='h3'>
+      <FormattedMessage {...title} />
+    </Header>
+    <div className={styles.button}>
+      <CreateAccountButton size="large" />
+    </div>
+  </div>
+);
 
