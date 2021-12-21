2ec101212f5465b051eea8cea7dc240a5727be24
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-12-21 15:59:16 -0600

    scalable bottom CTA

--- apps/site-landing/src/containers/CreateAccountBanner/index.tsx
+++ apps/site-landing/src/containers/CreateAccountBanner/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { defineMessage, FormattedMessage } from 'react-intl';
-import { Grid, Header } from 'semantic-ui-react';
-import illustrationPeople from './images/illust_grow.svg';
-import illustrationPlant from './images/illust_flowers.svg';
-
+import { Header } from 'semantic-ui-react';
 import styles from './styles.module.less';
 import { CreateAccountButton } from '../../components/AppLinks/CreateAccount';
 
@@ -12,9 +9,9 @@ export enum TypeCreateAccountBanner {
     Plants
 }
 
-const images = {
-  [TypeCreateAccountBanner.People]: illustrationPeople,
-  [TypeCreateAccountBanner.Plants]: illustrationPlant,
+const bgStyles = {
+  [TypeCreateAccountBanner.People]: styles.withPeople,
+  [TypeCreateAccountBanner.Plants]: styles.withPlants,
 }
 
 export type Props = {
@@ -27,24 +24,11 @@ const title = defineMessage({
   description: 'site.createAccountBanner.title: The benefits of a chequing, savings, and investing account all in one!'
 });
 
-export const CreateAccountBanner = (props: Props) => {
-
-  const illustration = images[props.Type];
-  return (
-    <Grid className={`${styles.content} ${props.className ?? ''} x20spaceBefore`} id={styles.createAccountBanner} columns='equal' textAlign='center' verticalAlign='middle' stackable>
-      <Grid.Row>
-        <Grid.Column>
-          <Header as='h3'>
-            <FormattedMessage {...title} />
-          </Header>
-          <CreateAccountButton size='large' />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column textAlign='left'>
-          <img src={illustration} />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  );
-}
+export const CreateAccountBanner = (props: Props) => (
+  <div className={`${styles.content} ${bgStyles[props.Type]} ${props.className ?? ''}`} >
+    <Header as='h3' className={styles.title}>
+      <FormattedMessage {...title} />
+    </Header>
+    <CreateAccountButton size='large' />
+  </div>
+);
