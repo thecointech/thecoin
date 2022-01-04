46f2d2d49460448c3d86cdf247eb2285c01e73f5
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-01-04 14:16:23 -0600

    translations for site-base/site-app

--- apps/site-landing/src/translations/index.ts
+++ apps/site-landing/src/translations/index.ts
@@ -1,17 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 import { formatTranslations } from "@thecointech/shared/containers/LanguageProvider/initialize"
-
 import en from './en.json';
 import fr from './en.json';
 import shared from "@thecointech/shared/translations";
+import base from "@thecointech/site-base/translations";
 
 export const translations = formatTranslations({
   en: {
     ...shared.en,
+    ...base.en,
     ...en,
   },
   fr: {
     ...shared.fr,
+    ...base.fr,
     ...fr,
   }
 });
