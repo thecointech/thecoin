1cab74d0013390e7b79202db3a13dd4f37afd009
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-01-14 09:30:13 -0600

    fix the build

--- libs/tx-blockchain/src/fetch.test.ts
+++ libs/tx-blockchain/src/fetch.test.ts
@@ -1,41 +1,29 @@
-import { loadAndMergeHistory, calculateTxBalances } from "./fetch";
+import { loadAndMergeHistory } from "./fetch";
 import { GetContract, TheCoin } from '@thecointech/contract-core';
-import { BigNumber } from "ethers";
-import { Transaction } from "./types";
-import { cadbrokerIn, cadbrokerOut } from "./thecoin";
 import { describe, IsManualRun } from '@thecointech/jestutils';
 
 jest.unmock('@thecointech/contract-core');
 
-async function fetchAndTestBalance(adress: string, contract: TheCoin) {
-  const history = await loadAndMergeHistory(adress, 0, contract);
-  const balance = await contract.balanceOf(adress) as BigNumber;
-  validateHistory(history, balance);
+async function fetchAndTestBalance(contract: TheCoin) {
+  const history = await loadAndMergeHistory(0, contract);
   return history;
 }
 
-function validateHistory(history: Transaction[], balance: BigNumber) {
-  calculateTxBalances(balance, history);
-  expect(history[0].balance).toBe(history[0].change);
-  expect(history[history.length - 1].balance).toBe(balance.toNumber());
-}
+// function validateHistory(history: Transaction[], balance: BigNumber) {
+//   calculateTxBalances(balance, history);
+//   expect(history[0].balance).toBe(history[0].change);
+//   expect(history[history.length - 1].balance).toBe(balance.toNumber());
+// }
 
 //
 // Easy-to-debug fetch of all history
 // NOTE: This is probably non-functional
 describe("Fetch live data", () => {
 
-  it('Can fetch out transactions', async () => {
+  it('Can fetch transactions', async () => {
     jest.setTimeout(10 * 60 * 1000); // 10 mins to process this (todo: cache the data)
     const contract = GetContract();
     console.log("Loading out txs");
-    await fetchAndTestBalance(cadbrokerOut, contract);
-  })
-
-  it('Can fetch in transactions', async () => {
-    jest.setTimeout(10 * 60 * 1000); // 10 mins to process this (todo: cache the data)
-    const contract = GetContract();
-    console.log("Loading In txs");
-    await fetchAndTestBalance(cadbrokerIn, contract);
+    await fetchAndTestBalance(contract);
   })
 }, IsManualRun);
