82001f21c5bddf300694235b62cc00f5fe6ba3b3
MarieTheCoin <68251967+MarieTheCoin@users.noreply.github.com>
2022-02-06 17:54:59 -0500

    Styles for the graph form

--- apps/site-landing/src/containers/BenefitsSimulator/Form/index.tsx
+++ apps/site-landing/src/containers/BenefitsSimulator/Form/index.tsx
@@ -1,25 +1,25 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import { Button } from 'semantic-ui-react';
 import { defineMessages, FormattedMessage } from 'react-intl';
-import { SimulationParameters } from '../../ReturnProfile/data/params';
 import { RangeFieldAndScale } from 'components/RangeFieldAndScale';
-import styles from './styles.module.less';
 import { debounce } from 'lodash';
+import { SimulationParameters } from '../../ReturnProfile/data/params';
+import styles from './styles.module.less';
 
 const translations = defineMessages({
-    startingValue : {
-      defaultMessage: 'Starting value:',
-      description: 'site.compare.label.rangeStarting: label for Starting Value in the compare page'},
-    rangeDuration : {
-      defaultMessage: 'Duration:',
-      description: 'site.compare.label.rangeDuration: label for range duration in the compare page'},
-    rangeDurationYear : {
-      defaultMessage: 'Years',
-      description: 'site.compare.label.rangeDurationYear: label for range Duration Year field in the compare page'},
-    button : {
-        defaultMessage: 'Show Me',
-        description: 'site.compare.button: label for the button in the compare page'}
-  });
+  startingValue: {
+    defaultMessage: 'Starting value:',
+    description: 'site.compare.label.rangeStarting: label for Starting Value in the compare page',
+  },
+  rangeDuration: {
+    defaultMessage: 'Duration:',
+    description: 'site.compare.label.rangeDuration: label for range duration in the compare page',
+  },
+  rangeDurationYear: {
+    defaultMessage: 'Years',
+    description: 'site.compare.label.rangeDurationYear: label for range Duration Year field in the compare page',
+  },
+});
 
 type Props = {
   params: SimulationParameters;
@@ -28,14 +28,14 @@ type Props = {
   setYears: Dispatch<SetStateAction<number>>;
 };
 
-export const FormCompare = ({params, setParams, years, setYears}: Props) => {
-
+export const FormCompare = ({
+  params, setParams, years, setYears,
+}: Props) => {
   const debounceInterval = 250;
-  const dbOnChangeNamed = (name: keyof SimulationParameters) =>
-    debounce((val: any) => setParams((prev: SimulationParameters) => ({
-      ...prev,
-      [name]: val,
-    })), debounceInterval);
+  const dbOnChangeNamed = (name: keyof SimulationParameters) => debounce((val: any) => setParams((prev: SimulationParameters) => ({
+    ...prev,
+    [name]: val,
+  })), debounceInterval);
 
   const dbOnSetYears = debounce(v => setYears(Math.max(1, v)), debounceInterval);
 
@@ -49,7 +49,7 @@ export const FormCompare = ({params, setParams, years, setYears}: Props) => {
         maximum={1000}
         step={1}
         initial={params.initialBalance}
-        onChange={dbOnChangeNamed("initialBalance")}
+        onChange={dbOnChangeNamed('initialBalance')}
       />
 
       <RangeFieldAndScale
@@ -61,9 +61,6 @@ export const FormCompare = ({params, setParams, years, setYears}: Props) => {
         onChange={dbOnSetYears}
       />
 
-      <Button secondary className={`${styles.buttonContainer} x16spaceBefore`}>
-        <FormattedMessage {...translations.button} />
-      </Button>
     </div>
   );
 };
