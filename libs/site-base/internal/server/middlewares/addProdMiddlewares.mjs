51e49103ed7caaff0cff0f5d64494c5f83c34d6e
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-02-21 12:14:51 -0600

    updated assets to latest CSS version

--- libs/site-base/internal/server/middlewares/addProdMiddlewares.mjs
+++ libs/site-base/internal/server/middlewares/addProdMiddlewares.mjs
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import { static } from 'express';
+import { static as exstatic} from 'express';
 import compression from 'compression';
 
 export function addProdMiddlewares(app, options) {
@@ -10,7 +10,7 @@ export function addProdMiddlewares(app, options) {
   // smaller (applies also to assets). You can read more about that technique
   // and other good practices on official Express.js docs http://mxs.is/googmy
   app.use(compression());
-  app.use(publicPath, static(outputPath));
+  app.use(publicPath, exstatic(outputPath));
 
   // deepcode ignore NoRateLimitingForExpensiveWebOperation: not valid for testing server
   app.get('*', (req, res) =>
