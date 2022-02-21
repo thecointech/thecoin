0dc8962321c21bea396c3ed774aac73ff9345f21
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-02-21 14:39:21 -0600

    doc-only change

--- apps/site-landing/src/containers/BenefitsSimulator/Form/Advanced.tsx
+++ apps/site-landing/src/containers/BenefitsSimulator/Form/Advanced.tsx
@@ -96,7 +96,7 @@ const tooltips = {
   }),
   credit: defineMessages({
     weekly: {
-      defaultMessage: 'Any weekly spending that can be put{br}on the credit card, eg groceries and gas',
+      defaultMessage: 'Any weekly spending that can be put{br}on a credit card, eg groceries and gas',
       description: 'Simulator Tooltip: Weekly Cash',
       values: {br: "\n"},
     },
