15bfb8206b096067fdd8d29a9686b18393959ce5
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-03-31 10:18:15 -0500

    Error-tolerance in IDX restart

--- tools/restartidx.ps1
+++ tools/restartidx.ps1
@@ -1,5 +1,18 @@
 echo "Restart IDX script at $(Get-Date)"
 $cg = Get-AzContainerGroup -ResourceGroupName idx-resources -Name ipfs-daemon
 echo "Begin Action"
-Invoke-AzResourceAction -ResourceId $cg.Id -Action restart -Force
+try {
+  Invoke-AzResourceAction -ResourceId $cg.Id -Action restart -Force
+  if (!$?) {
+    throw "Restart call failed"
+  }
+}
+catch {
+  echo "Retrying with start command"
+  Invoke-AzResourceAction -ResourceId $cg.Id -Action start -Force
+  if (!$?) {
+    throw "Unknown error happened, give up and go home..."
+  }
+}
+
 echo "Done"
