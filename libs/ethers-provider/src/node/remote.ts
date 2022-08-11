d05740e5a17f3026d538bb3f3b1b632dace75950
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-08-11 17:15:33 -0500

    providers may be required as CJS

--- libs/ethers-provider/src/node/remote.ts
+++ libs/ethers-provider/src/node/remote.ts
@@ -1,6 +1,5 @@
 import { InfuraProvider } from "@ethersproject/providers";
 import { Network } from '../types';
-import { log } from '@thecointech/logging';
 
 function getInfuraNetwork(deployTo: Network) {
   switch(process.env[`DEPLOY_${deployTo}_NETWORK`])
@@ -16,9 +15,8 @@ export const getProvider = (deployTo: Network = "POLYGON") => {
   if (!network)
     throw new Error("Missing deploy network, cannot connect to blockchain");
 
+  // In some circumstances (CI) we do not have a projectId
+  // There is a default one, so no real biggy
   const projectId = process.env.INFURA_PROJECT_ID;
-  if (!projectId)
-    log.warn(`Missing INFURA project ID, cannot connect to ${network}`);
-
   return new InfuraProvider(network, projectId);
 }
