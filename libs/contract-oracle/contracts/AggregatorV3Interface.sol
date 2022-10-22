dc4e1b0fd8629dc500cda1f38d87faaedd95676a
Stephen Taylor <stephen.taylor.dev@gmail.com>
2022-10-22 16:08:04 -0500

    A long, wasted effort thinking I could optimize casts in Solidity

--- libs/contract-oracle/contracts/AggregatorV3Interface.sol
+++ libs/contract-oracle/contracts/AggregatorV3Interface.sol
@@ -57,7 +57,7 @@ interface AggregatorV3Interface {
 
   // Custom TC function.  Only the Oracle can convert from
   // timestamp to roundId, so we might as well encapsulate it here.
-  function getRoundFromTimestamp(int timestamp)
+  function getRoundFromTimestamp(uint timestamp)
     external
     view
     returns (uint answer);
