f04f24def7c128bc147e629c3800fd1f53fa4669
Stephen Taylor <stephen.taylor.dev@gmail.com>
2023-02-27 09:45:17 -0600

    contract-core updated for prodtest

--- libs/contract-core/scripts/update.ts
+++ libs/contract-core/scripts/update.ts
@@ -1,15 +1,21 @@
 import hre from 'hardhat';
-import { getSigner } from '@thecointech/signers';
 import { log } from '@thecointech/logging';
 import '@nomiclabs/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
 import { getContractAddress } from '../src';
+import { getProvider } from '@thecointech/ethers-provider';
+import { getSigner } from '@thecointech/signers';
 
 async function main() {
 
   const network = hre.config.defaultNetwork;
   const name = getName(network);
-  const owner = await getSigner("Owner");
+  let owner = await getSigner('Owner');
+  // If not devlive, then add a provider
+  if (process.env.CONFIG_NAME !== 'devlive') {
+    const provider = getProvider();
+    owner = owner.connect(provider);
+  }
   const address = await getContractAddress();
 
   const TheCoin = await hre.ethers.getContractFactory(name, owner);
