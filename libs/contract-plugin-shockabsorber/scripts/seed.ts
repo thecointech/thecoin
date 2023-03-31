e4c0be2466510b3c6c6461b8c4cc16397fb3dd5c
Stephen Taylor <stephen.taylor.dev@gmail.com>
2023-03-31 10:47:38 -0500

    devlive (WIP)

--- libs/contract-plugin-shockabsorber/scripts/seed.ts
+++ libs/contract-plugin-shockabsorber/scripts/seed.ts
@@ -2,19 +2,21 @@ import { getSigner } from '@thecointech/signers';
 import { ConnectContract } from '@thecointech/contract-core';
 import { ALL_PERMISSIONS } from '@thecointech/contract-plugins';
 import { log } from '@thecointech/logging';
-import { DateTime } from 'luxon';
-import { fetchRate, weSellAt } from '@thecointech/fx-rates';
-import Decimal from 'decimal.js-light';
-import { toCoinDecimal } from '@thecointech/utilities';
-import { connect } from '@thecointech/contract-base/connect';
-import { sleep } from '@thecointech/async';
-
+import { getContract } from '@thecointech/contract-plugin-shockabsorber';
 // Assume devlive
 if (process.env.CONFIG_NAME !== 'devlive') throw new Error('Not Sufficiently Tested');
 
 log.debug('Seeding ShockAbsorber');
 async function main() {
+  const tester = await getSigner("uberTester");
+  const theCoin = await getSigner("TheCoin");
+  const testAddress = await tester.getAddress();
 
+  const tcCore = await ConnectContract(theCoin);
+  const shockAbsorber = await getContract();
+
+  // In DevLive, we assign the converter to uberTester
+  await tcCore.pl_assignPlugin(testAddress, shockAbsorber.address, ALL_PERMISSIONS, "0x1234");
 }
 
 main();
