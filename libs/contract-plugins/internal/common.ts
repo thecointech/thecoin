f42e2728c33c8e918fdc6515b0500b42f4b6cc56
Stephen Taylor <stephen.taylor.dev@gmail.com>
2023-04-01 15:49:20 -0500

    refactor to allow simultaneous mocking of SA & UC

--- libs/contract-plugins/internal/common.ts
+++ libs/contract-plugins/internal/common.ts
@@ -1,7 +1,9 @@
-import { toCoin, toFiat } from '../src/modifier';
+import { getPluginModifier, toCoin, toFiat } from '../src/modifier';
 import { DateTime } from 'luxon';
 import type { FXRate } from '@thecointech/pricing';
 import type { PluginBalanceMod } from '../src/types';
+import { ALL_PERMISSIONS } from '../src/constants';
+import { BigNumber } from '@ethersproject/bignumber';
 import Decimal from 'decimal.js-light';
 
 export function runModifier(balanceOf: PluginBalanceMod, fiat: number, timestamp: number) {
@@ -16,4 +18,14 @@ export function runModifier(balanceOf: PluginBalanceMod, fiat: number, timestamp
   const coin = toCoin([new Decimal(fiat), ts], rates);
   const rcoin = balanceOf(coin, DateTime.fromMillis(timestamp), rates);
   return toFiat([rcoin, ts], rates);
+}
+
+export const user = "0x1234567890123456789012345678901234567890";
+const permissions = BigNumber.from(ALL_PERMISSIONS);
+export const getModifier = async (plugin: string) => {
+  const r = await getPluginModifier(user, { plugin, permissions } as any);
+
+  return (fiat: number, timestamp: number) => {
+    return runModifier(r, fiat, timestamp);
+  }
 }
