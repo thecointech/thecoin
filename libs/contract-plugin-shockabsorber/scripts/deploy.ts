6f4125f607a05441c9e6cbc668d5a5e6f1c6756d
Stephen Taylor <stephen.taylor.dev@gmail.com>
2023-04-03 09:59:23 -0500

    ShockAbsorber deployed

--- libs/contract-plugin-shockabsorber/scripts/deploy.ts
+++ libs/contract-plugin-shockabsorber/scripts/deploy.ts
@@ -1,28 +1,45 @@
 import hre from 'hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@openzeppelin/hardhat-upgrades';
 import { writePlugin } from '@thecointech/contract-plugins/writePlugin';
 import { getSigner } from '@thecointech/signers';
 import { log } from '@thecointech/logging';
-import '@nomiclabs/hardhat-ethers';
-import '@openzeppelin/hardhat-upgrades';
 import { getProvider } from '@thecointech/ethers-provider';
 import { getArguments } from './arguments';
+import { fetchRate, weSellAt } from '@thecointech/fx-rates';
+import { toCoinDecimal } from '@thecointech/utilities';
+import { ConnectContract } from '@thecointech/contract-core';
+import Decimal from 'decimal.js-light';
 
 async function main() {
-  let owner = await getSigner('TheCoin');
+  // BrokerCAD directly owns this contract (and associated benefits)
+  let brokerCAD = await getSigner('BrokerCAD');
   // If not devlive, then add a provider
   if (hre.network.config.chainId !== 31337) {
     const provider = getProvider();
-    owner = owner.connect(provider);
+    brokerCAD = brokerCAD.connect(provider);
   }
 
   const deployArgs = await getArguments();
-  const ShockAbsorber = await hre.ethers.getContractFactory('ShockAbsorber', owner);
+  const ShockAbsorber = await hre.ethers.getContractFactory('ShockAbsorber', brokerCAD);
   const shockAbsorber = await hre.upgrades.deployProxy(ShockAbsorber, deployArgs);
   log.info(`Deployed ShockAbsorber at ${shockAbsorber.address} with args: ${deployArgs}`);
 
   // Serialize our contract addresses
   const contractUrl = new URL('../contracts/ShockAbsorber.sol', import.meta.url);
   writePlugin(shockAbsorber.address, contractUrl);
+
+  // Once deployed, the contract is going to need a bit of funding
+  // Transfer $10,000 worth from BrokerCAD immediately.
+  const now = new Date();
+  const fxRate = await fetchRate(now)
+  const sellRate = weSellAt([fxRate], now)
+  const coin = toCoinDecimal(
+    new Decimal(10_000).div(sellRate)
+  );
+
+  const bcCore = await ConnectContract(brokerCAD);
+  await bcCore.exactTransfer(shockAbsorber.address, coin.toNumber(), now.getTime());
 }
 
 main().catch((error) => {
