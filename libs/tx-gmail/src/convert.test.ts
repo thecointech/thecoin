c045a01cfb2b65cec77aae88038ba752d82acaf1
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-02-02 06:25:23 -0600

    allow prodtest (only) to override recieved time for emails

--- libs/tx-gmail/src/convert.test.ts
+++ libs/tx-gmail/src/convert.test.ts
@@ -1,6 +1,7 @@
 import { toDepositData } from "./convert";
 //@ts-ignore Import directly from mocks to skip auth part.
 import emails from '../../__mocks__/googleapis/emails.get.json' assert {type: "json"};
+import { DateTime } from "luxon";
 
 it('processes emails correctly', async () => {
   for (const email of emails) {
@@ -11,4 +12,17 @@ it('processes emails correctly', async () => {
     // ensure we have sourceID;
     expect(eTransfer?.id).toBeTruthy();
   }
+})
+
+
+it('Allows prodtest override', async () => {
+  // Find the one with an override
+  const { data } = emails.find(e => e.data.labelIds.includes('ProdTestOverride'));
+  expect(data).toBeTruthy();
+
+  const regData = toDepositData(data);
+  process.env.CONFIG_NAME = "prodtest";
+  const ptData = toDepositData(data);
+  expect(regData.recieved).toEqual(DateTime.fromISO('2021-07-19T18:51:56.000-04:00'));
+  expect(ptData.recieved).toEqual(DateTime.fromISO('2023-01-02T09:35:00.000-05:00'))
 })
