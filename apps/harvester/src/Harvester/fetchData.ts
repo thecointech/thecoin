64b94a659c3e8603b3da587547fbd9a588006041
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-03-24 14:33:10 -0500

    cleanup

--- apps/harvester/src/Harvester/fetchData.ts
+++ apps/harvester/src/Harvester/fetchData.ts
@@ -43,15 +43,12 @@ export async function getVisaData(lastTxDate?: DateTime) : Promise<VisaBalanceRe
 // to consider: merging the logic?
 // Date visa billing period ends
 const firstBillingPeriodStarts = DateTime.fromISO('2024-02-26');
-const weeksInBillingPeriod = 4; //Duration.fromObject({week: 4});
-const weeksInGracePeriod = 3; //Duration.fromObject({week: 3});
-// const firstDueDate = firstBillingPeriodStarts.plus({ weeks: weeksInBillingPeriod + weeksInGracePeriod });
+const weeksInBillingPeriod = 4;
+const weeksInGracePeriod = 3;
 
 const weeklySpending = 350;
 // Due amount is constant each period
 const dueAmount = weeklySpending * weeksInBillingPeriod;
-// First run is the last tx created by initDemoAccount
-// const firstRun = DateTime.fromISO('2024-03-18');
 
 export function getEmulatedVisaData(atDate: DateTime, lastTxDate?: DateTime) {
 
@@ -68,23 +65,6 @@ export function getEmulatedVisaData(atDate: DateTime, lastTxDate?: DateTime) {
   const periods = Math.floor(weeksBetween / weeksInBillingPeriod);
   const dueDate = firstBillingPeriodStarts.plus({ weeks: weeksInGracePeriod + (periods * weeksInBillingPeriod) });
 
-  // Current balance climbs from dueAmount to 2x dueAmount
-  // This assumes we run 2x each week
-  // const increments = Math.round(2 * (weeksBetween % 4))
-  // const balance = baseDue + thisWeeksSpending; //dueAmount + increments * (weeklySpending / 2);
-  // const weeksLastChecked = lastTxDate ? atDate.diff(lastTxDate, 'weeks').weeks : 0;
-  // const priorRepayments = (1 + weeksLastChecked) / weeksInBillingPeriod
-  // const hasPaymentHappened = Math.floor(repayments) > Math.floor(priorRepayments);
-  // const history: HistoryRow[] = hasPaymentHappened
-  //   ? [
-  //     {
-  //       date: atDate.set({ hour: 0, minute: 0, second: 0, millisecond: 0 }),
-  //       description: "Payment",
-  //       credit: currency(dueAmount),
-  //       balance: currency(balance),
-  //     }
-  //   ]
-  //   : []
   return {
     balance: currency(balance),
     dueDate,
@@ -93,6 +73,7 @@ export function getEmulatedVisaData(atDate: DateTime, lastTxDate?: DateTime) {
   };
 }
 
+// If a payment should have happened since lastTxDate, add it to history
 export function getEmulatedVisaHistory(repayments: number, balance: number, lastTxDate?: DateTime) : HistoryRow[] {
   const weeksLastChecked = lastTxDate ? lastTxDate.diff(firstBillingPeriodStarts, 'weeks').weeks : 0;
   const priorRepayments = (1 + weeksLastChecked) / weeksInBillingPeriod
