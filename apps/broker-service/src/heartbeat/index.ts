4d6aa4ae40514a6279a38544b3cb0dae0688faeb
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-04-11 16:03:48 -0500

    Fixes heartbeat failure

--- apps/broker-service/src/heartbeat/index.ts
+++ apps/broker-service/src/heartbeat/index.ts
@@ -1,4 +1,5 @@
 import { setHeartbeat } from '@thecointech/broker-db/user';
+import { log } from '@thecointech/logging';
 import { GetSigner } from '@thecointech/utilities/SignedMessages';
 
 export type Heartbeat = {
@@ -9,6 +10,7 @@ export type Heartbeat = {
 
 export async function heartbeat(request: Heartbeat) {
   if (request.timeMs < (Date.now() - 5000)) {
+    log.error({signedTime: request.timeMs}, 'Heartbeat too old: {signedTime}')
     return false;
   }
   const signer = await GetSigner({
@@ -16,6 +18,7 @@ export async function heartbeat(request: Heartbeat) {
     signature: request.signature,
   });
 
+  log.info({signer}, 'Heartbeat From: {signer}')
   await setHeartbeat(signer, request.result)
   return true;
 }
