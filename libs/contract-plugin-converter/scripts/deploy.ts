e44215867b8a46c047ec20bd9bd2278f1076c71d
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-05-10 08:38:06 -0500

    Deployed converter

--- libs/contract-plugin-converter/scripts/deploy.ts
+++ libs/contract-plugin-converter/scripts/deploy.ts
@@ -4,26 +4,18 @@ import { log } from '@thecointech/logging';
 import '@nomicfoundation/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
 import { getArguments } from './arguments';
-import { getDeploySigner } from '@thecointech/contract-tools/deploySigner';
+import { getSigner } from '@thecointech/signers';
 
-async function main() {
-
-  // Who owns the converter?  Probably Owner?
-  const owner = await getDeploySigner("Owner");
-  const deployArgs = await getArguments();
-  const UberConverter = await hre.ethers.getContractFactory("UberConverter", owner);
-  const uberConverter = await hre.upgrades.deployProxy(UberConverter, deployArgs, {
-    timeout: 5 * 60 * 1000
-  });
-  const contractAddress = await uberConverter.getAddress();
-  log.info(`Deployed UberConverter at ${contractAddress} with args: ${deployArgs}`);
-
-  // Serialize our contract addresses
-  const contractUrl = new URL('../contracts/UberConverter.sol', import.meta.url);
-  writePlugin(contractAddress, contractUrl);
-}
-
-main().catch((error) => {
-  log.error(error);
-  process.exitCode = 1;
+// Who owns the converter?  Probably Owner?
+const owner = await getSigner("Owner");
+const deployArgs = await getArguments();
+const UberConverter = await hre.ethers.getContractFactory("UberConverter", owner);
+const uberConverter = await hre.upgrades.deployProxy(UberConverter, deployArgs, {
+  timeout: 5 * 60 * 1000
 });
+const contractAddress = await uberConverter.getAddress();
+log.info(`Deployed UberConverter at ${contractAddress} with args: ${deployArgs}`);
+
+// Serialize our contract addresses
+const contractUrl = new URL('../contracts/UberConverter.sol', import.meta.url);
+writePlugin(contractAddress, contractUrl);
