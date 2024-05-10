fa3b45a9b81668256cfb253da0d98c8c37e4bc26
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-05-10 08:48:35 -0500

    Published shock-absorber

--- libs/contract-plugin-shockabsorber/scripts/deploy.ts
+++ libs/contract-plugin-shockabsorber/scripts/deploy.ts
@@ -4,23 +4,16 @@ import '@openzeppelin/hardhat-upgrades';
 import { writePlugin } from '@thecointech/contract-plugins/writePlugin';
 import { log } from '@thecointech/logging';
 import { getArguments } from './arguments';
-import { getDeploySigner } from '@thecointech/contract-tools/deploySigner';
+import { getSigner } from '@thecointech/signers';
 
-async function main() {
-  // BrokerCAD directly owns this contract (and associated benefits)
-  const brokerCAD = await getDeploySigner("BrokerCAD");
-  const deployArgs = await getArguments();
-  const ShockAbsorber = await hre.ethers.getContractFactory('ShockAbsorber', brokerCAD);
-  const shockAbsorber = await hre.upgrades.deployProxy(ShockAbsorber, deployArgs);
-  const shockAbsorberAddress = await shockAbsorber.getAddress();
-  log.info(`Deployed ShockAbsorber at ${shockAbsorberAddress} with args: ${deployArgs}`);
+// BrokerCAD directly owns this contract (and associated benefits)
+const brokerCAD = await getSigner("BrokerCAD");
+const deployArgs = await getArguments();
+const ShockAbsorber = await hre.ethers.getContractFactory('ShockAbsorber', brokerCAD);
+const shockAbsorber = await hre.upgrades.deployProxy(ShockAbsorber, deployArgs);
+const shockAbsorberAddress = await shockAbsorber.getAddress();
+log.info(`Deployed ShockAbsorber at ${shockAbsorberAddress} with args: ${deployArgs}`);
 
-  // Serialize our contract addresses
-  const contractUrl = new URL('../contracts/ShockAbsorber.sol', import.meta.url);
-  writePlugin(shockAbsorberAddress, contractUrl);
-}
-
-main().catch((error) => {
-  log.error(error);
-  process.exitCode = 1;
-});
+// Serialize our contract addresses
+const contractUrl = new URL('../contracts/ShockAbsorber.sol', import.meta.url);
+writePlugin(shockAbsorberAddress, contractUrl);
