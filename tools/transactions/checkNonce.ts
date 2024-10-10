27b372b945a6b5acd6f07c2c05e4b84e762f2af0
FrozenKiwi <stephen.taylor.dev@gmail.com>
2024-10-10 14:55:09 -0500

    remove accidental commit

--- tools/transactions/checkNonce.ts
+++
@@ -1,8 +0,0 @@
-import { getSigner } from "@thecointech/signers";
-
-const signer = await getSigner("testDemoAccount");
-
-const nonce = await signer.getNonce();
-console.log("Nonce: " + nonce)
-console.log("Address: " + await signer.getAddress())
-
