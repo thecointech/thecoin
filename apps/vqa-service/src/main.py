9f55d1c171917da76bfbde1815475d44d56a2abd
Stephen Taylor <stephen.taylor.dev@gmail.com>
2025-01-08 13:48:03 -0600

    added warmup route

--- apps/vqa-service/src/main.py
+++ apps/vqa-service/src/main.py
@@ -5,7 +5,7 @@ from login_routes import add_login_routes
 from modal_routes import add_modal_routes
 from twofa_routes import add_twofa_routes
 from summary_routes import add_summary_routes
-from singleton import get_model
+from warmup import add_warmup_routes
 from port import get_version, get_port
 from fastapi_tweak import use_route_names_as_operation_ids
 
@@ -25,18 +25,12 @@ app = FastAPI(
 
 app.add_middleware(KeepAliveMiddleware)
 
-@app.get("/warmup")
-def warmup():
-    # runQuery("", "")
-    get_model()
-    return {"Hello": "World"}
-
-
 add_intent_routes(app)
 add_landing_routes(app)
 add_login_routes(app)
 add_twofa_routes(app)
 add_summary_routes(app)
 add_modal_routes(app)
+add_warmup_routes(app)
 
 use_route_names_as_operation_ids(app)
