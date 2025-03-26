2baa36110e516431e0128335c147adde1b0810e8
Stephen Taylor <stephen.taylor.dev@gmail.com>
2025-03-26 10:46:30 -0500

    better logging

--- tools/publish/run-processor.sh
+++ tools/publish/run-processor.sh
@@ -1,7 +1,7 @@
 
 SCRIPT_DIR="$(dirname "$0")"
 CURRENT_DEPLOY_PATH=$(cat "$SCRIPT_DIR/current_deploy.txt")
-echo "Deployed version path: $CURRENT_DEPLOY_PATH"
+echo "Running processor on $(date) for version: $CURRENT_DEPLOY_PATH"
 
 cd $CURRENT_DEPLOY_PATH/apps/tx-processor
 yarn prod:test
