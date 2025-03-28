033b4fba32d4b60ba7decf0d34e5dbd8490aefc8
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-03-28 17:29:16 -0500

    fix OOM error in tests caused by infinite loop

--- libs/setenv/projectUrl/index.mjs
+++ libs/setenv/projectUrl/index.mjs
@@ -4,7 +4,7 @@ export const projectUrl = () => {
   let baseUrl = new URL(`../../../`, new URL(import.meta.url));
   while (!existsSync(new URL("environments", baseUrl))) {
     const parentUrl = new URL(`..`, baseUrl);
-    if (parentUrl == baseUrl) {
+    if (parentUrl.toString() == baseUrl.toString()) {
       throw new Error("Could not find project root");
     }
     baseUrl = parentUrl;
