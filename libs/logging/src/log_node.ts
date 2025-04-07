a69cc7f37a1ea0ad4ec84ac78b459ab7eaa942e1
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-04-07 13:08:07 -0500

    minor changes, force republish packages

--- libs/logging/src/log_node.ts
+++ libs/logging/src/log_node.ts
@@ -1,6 +1,7 @@
 import { BunyanLogger } from './logger';
 import { init_node } from './node';
 
+
 export const log: BunyanLogger = init_node(process.env.LOG_NAME ?? process.env.CONFIG_NAME ?? "dev");
 if (process.env.LOG_LEVEL !== undefined)
   log.level(Number(process.env.LOG_LEVEL));
