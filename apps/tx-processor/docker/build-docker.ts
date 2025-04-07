04f82e1c03b68afc440499c75a5c6ad751c43541
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-04-07 11:15:17 -0500

    Fix tx-processor environments folder

--- apps/tx-processor/docker/build-docker.ts
+++ apps/tx-processor/docker/build-docker.ts
@@ -1,69 +1,30 @@
-// import { spawnSync } from 'child_process';
-// import { getSecret } from '@thecointech/secrets';
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-import { copyFileSync, mkdirSync, readFileSync, writeFileSync, cpSync } from 'fs';
+import { spawnSync } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { rmSync } from 'fs';
 
 // Get current directory
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Verify token
-const githubToken = process.env.GH_PACKAGES_READ;
-if (!githubToken) {
-  throw new Error('GH_PACKAGES_READ not found in environment');
+// Note: This requires the GH_PACKAGES_READ
+await import('./prep-docker');
+
+// Build the Docker image
+const result = spawnSync('docker', [
+  'build',
+  '-t', 'tx-processor',
+  '-f', `${__dirname}/Dockerfile`,
+  '--rm=false',
+  '..'  // Build context is the tx-processor directory
+], {
+  stdio: 'inherit',
+  cwd: __dirname  // Run from the docker directory
+});
+
+// Clean up temporary files
+rmSync('./temp', { recursive: true, force: true });
+
+// Check if the build was successful
+if (result.status !== 0) {
+  throw new Error('Failed to build Docker image');
 }
-
-// Create temp directory
-mkdirSync('./temp', { recursive: true });
-
-// Copy yarn.lock locally
-copyFileSync("../../yarn.lock", "./temp/yarn.lock");
-
-// Copy environments
-cpSync("../../environments", "./temp/environments", { recursive: true });
-
-// Add packageManager to package.json & move here
-const packageJson = JSON.parse(readFileSync("package.json", "utf-8"));
-const rootPackageJson = JSON.parse(readFileSync("../../package.json", "utf-8"));
-packageJson.packageManager = rootPackageJson.packageManager;
-// packageJson.devDependencies = {};
-writeFileSync("./temp/package.json", JSON.stringify(packageJson, null, 2));
-
-// Create .yarnrc.yml with GitHub auth
-const yarnConfig = {
-  nodeLinker: "node-modules",
-  checksumBehavior: "ignore",
-  npmScopes: {
-    thecointech: {
-      npmRegistryServer: "https://npm.pkg.github.com",
-      npmAlwaysAuth: true,
-      npmAuthToken: githubToken
-    }
-  }
-};
-
-writeFileSync("./temp/.yarnrc.yml", JSON.stringify(yarnConfig, null, 2));
-
-// Add an npmrc as well why not?
-// writeFileSync("./temp/.npmrc", `//npm.pkg.github.com/:_authToken=${githubToken}\n@thecointech:registry=https://npm.pkg.github.com`);
-
-// // Build the Docker image
-// const result = spawnSync('docker', [
-//   'build',
-//   '-t', 'tx-processor',
-//   '--build-arg', `GITHUB_TOKEN=${githubToken}`,
-//   '-f', `${__dirname}/Dockerfile`,
-//   '--rm=false',
-//   '..'  // Build context is the tx-processor directory
-// ], {
-//   stdio: 'inherit',
-//   cwd: __dirname  // Run from the docker directory
-// });
-
-// // Clean up temporary files
-// rmSync('./temp', { recursive: true, force: true });
-
-// // Check if the build was successful
-// if (result.status !== 0) {
-//   throw new Error('Failed to build Docker image');
-// }
---
+++ apps/tx-processor/docker/prep-docker.ts
@@ -0,0 +1,37 @@
+import { copyFileSync, mkdirSync, readFileSync, writeFileSync, cpSync } from 'fs';
+
+// Verify token
+const githubToken = process.env.GH_PACKAGES_READ;
+if (!githubToken) {
+  throw new Error('GH_PACKAGES_READ not found in environment');
+}
+
+// Create temp directory
+mkdirSync('./temp', { recursive: true });
+
+// Copy yarn.lock locally
+copyFileSync("../../yarn.lock", "./temp/yarn.lock");
+
+// Copy environments
+cpSync("../../environments", "./temp/environments", { recursive: true });
+
+// Add packageManager to package.json & move here
+const packageJson = JSON.parse(readFileSync("package.json", "utf-8"));
+const rootPackageJson = JSON.parse(readFileSync("../../package.json", "utf-8"));
+packageJson.packageManager = rootPackageJson.packageManager;
+writeFileSync("./temp/package.json", JSON.stringify(packageJson, null, 2));
+
+// Create .yarnrc.yml with GitHub auth
+const yarnConfig = {
+  nodeLinker: "node-modules",
+  checksumBehavior: "ignore",
+  npmScopes: {
+    thecointech: {
+      npmRegistryServer: "https://npm.pkg.github.com",
+      npmAlwaysAuth: true,
+      npmAuthToken: githubToken
+    }
+  }
+};
+
+writeFileSync("./temp/.yarnrc.yml", JSON.stringify(yarnConfig, null, 2));
