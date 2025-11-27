7abb12751448d3d930eb455871458a1e2647409b
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-11-27 11:54:09 -0600

    updated site-fake-deposit, cause why not?

--- apps/site-fake-deposit/src/app.tsx
+++ apps/site-fake-deposit/src/app.tsx
@@ -5,32 +5,30 @@
  * code.
  */
 // Import all the third party stuff
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'sanitize.css/sanitize.css';
-
-// Import root app
-import { Page } from './Page';
 
 // Import Language Provider
 import { LanguageProvider, Languages } from '@thecointech/shared/containers/LanguageProvider';
 import { translations } from './translations';
+import { Page } from './Page';
 
 // Create redux store with history
-import {configureLandingStore } from './reducers';
-const store = configureLandingStore();
+import { configureStore } from '@thecointech/shared/store';
+const store = configureStore();
 
 const MOUNT_NODE = document.getElementById('app') as HTMLElement;
-const render = (languages: Languages, Component = Page) => {
-  ReactDOM.render(
-    // tslint:disable-next-line:jsx-wrap-multiline
+const root = createRoot(MOUNT_NODE);
+
+const render = (languages: Languages) => {
+  root.render(
     <Provider store={store}>
-    <LanguageProvider languages={languages}>
-      <Component />
-    </LanguageProvider>,
-    </Provider>,
-    MOUNT_NODE,
+      <LanguageProvider languages={languages}>
+        <Page />
+      </LanguageProvider>
+    </Provider>
   );
 };
 
@@ -38,10 +36,7 @@ const render = (languages: Languages, Component = Page) => {
 declare const module: any;
 if (module.hot) {
   module.hot.accept(['./Page'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    // tslint:disable-next-line:max-line-length
-    const refresh = require('./Page').default; // https://github.com/webpack/webpack-dev-server/issues/100
-    render(translations, refresh);
+    render(translations);
   });
 }
 
