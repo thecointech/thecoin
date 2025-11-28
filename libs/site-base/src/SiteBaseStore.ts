5607fd5da7d306f07588be73ec106c0d3fb01700
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-11-28 17:57:31 -0600

    remove obsolete code, disable tests in site_base

--- libs/site-base/src/SiteBaseStore.ts
+++
@@ -1,6 +0,0 @@
-import { ContentState } from './components/ContentHeightMeasure/types';
-
-// Your root reducer type, which is your redux state types also
-export interface SiteBaseStore {
-  readonly content: ContentState;
-}
