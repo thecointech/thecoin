b047fb595e00d11ac5bc97a264ed21efaa34fcb5
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-11-28 18:08:48 -0600

    fix last compiler issues

--- apps/site-landing/src/types/index.ts
+++ apps/site-landing/src/types/index.ts
@@ -1,9 +1,8 @@
-import { SiteBaseStore } from '@thecointech/site-base/SiteBaseStore';
 import { PrismicState } from 'components/Prismic/types';
 import { AreaDatum } from 'containers/AreaGraph/types';
 
 // Your root reducer type, which is your redux state types also
-export interface ApplicationRootState extends SiteBaseStore {
+export interface ApplicationRootState {
   readonly documents: PrismicState;
 
   // We store the current hover state in Redux to avoid
