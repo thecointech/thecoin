321476063359e4dabea79ec6da9fd4c6674d05b4
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-12-08 15:50:13 -0600

    compile-only fix to prismic mock (not working)

--- libs/__mocks__/@prismicio/client.ts
+++ libs/__mocks__/@prismicio/client.ts
@@ -1,12 +1,9 @@
-import type {BuildQueryURLArgs, Client} from "@prismicio/client";
+import type {Client} from "@prismicio/client";
 import data from './data.json' with { type: "json" }
-import { PrismicDocument, Query } from '@prismicio/types';
 
-class MockClient implements Pick<Client<PrismicDocument>, "query"|"getByUID"> {
-  query<TDocument extends PrismicDocument<Record<string, any>, string, string>>(predicates: string | string[], params?: (Partial<Omit<BuildQueryURLArgs, 'predicates'>> & { signal?: { aborted: any; addEventListener: any; removeEventListener: any; dispatchEvent: any; onabort: any; } | undefined; }) | undefined): Promise<Query<TDocument>> {
-    return Promise.resolve(data as any)
-  }
-  getByUID<TDocument extends PrismicDocument<Record<string, any>, string, string>, TDocumentType extends TDocument['type'] = TDocument['type']>(documentType: TDocumentType, uid: string, params?: (Partial<BuildQueryURLArgs> & { signal?: { aborted: any; addEventListener: any; removeEventListener: any; dispatchEvent: any; onabort: any; } | undefined; }) | undefined): Promise<Extract<TDocument, { type: TDocumentType; }> extends never ? TDocument : Extract<TDocument, { type: TDocumentType; }>> {
+// NOTE: Not yet updated to match the latest API
+class MockClient implements Pick<Client, "getByUID"> {
+  getByUID(documentType: string, uid: string) {
     return Promise.resolve(
       data.results.find(r => r.uid === uid) as any
     );
