2e317e79898f9c2aef1e4e43cd8c7067ad342c06
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-12-11 12:03:09 -0600

    added translations to mocked data

--- libs/__mocks__/@prismicio/updateMocks.ts
+++ libs/__mocks__/@prismicio/updateMocks.ts
@@ -2,8 +2,8 @@ import { createClient } from "@thecointech/site-prismic/client";
 import { writeFileSync } from "fs";
 
 const client = createClient({}, false);
-const articles = await client.getAllByType("article")
-const faqs = await client.getAllByType("faq")
+const articles = await client.getAllByType("article", {lang: `*`})
+const faqs = await client.getAllByType("faq", {lang: `*`})
 
 const cleanedArticles = articles.map(article => ({...article, href: undefined}))
 const cleanedFaqs = faqs.map(faq => ({...faq, href: undefined}))
