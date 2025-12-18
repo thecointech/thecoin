de6867a68e6e0bf716ed1e3133c3ff9c9bf62029
FrozenKiwi <stephen.taylor.dev@gmail.com>
2025-12-18 16:15:03 -0600

    Rename "Your Benefits" to "Simulator", cause it was a dumb name

--- libs/site-base/internal/webpack/index.ts
+++ libs/site-base/internal/webpack/index.ts
@@ -1,7 +1,7 @@
 import type { SecretKeyType } from '@thecointech/secrets/*';
 import type webpack from 'webpack';
-import { getDevConfig } from './webpack.dev.ts';
-import { getProdConfig } from './webpack.prod.ts';
+import { getDevConfig } from './webpack.dev';
+import { getProdConfig } from './webpack.prod';
 
 export function getConfig(secrets: SecretKeyType[], devServer: Partial<webpack.Configuration['devServer']>) {
   return process.env.NODE_ENV === 'production'
