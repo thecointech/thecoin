49df29d0765f4b21bb305ab000db02b0e5eb331c
FrozenKiwi <stephen.taylor.dev@gmail.com>
2026-01-06 13:53:32 -0600

    (temp) allow passing secrets to main process

--- libs/secrets/src/browser/getSecrets.ts
+++ libs/secrets/src/browser/getSecrets.ts
@@ -4,13 +4,13 @@ import { SecretNotFoundError } from "../errors";
 // In web environments, we need to include a few secrets (eg etherscan key etc)
 declare global {
   // This is replaced by webpack
-  var __COMPILER_REPLACE_SECRETS__: Record<string, string>;
+  var __COMPILER_REPLACE_SECRETS__: Record<string, string>|undefined;
 }
-const secrets: Record<string, string> = __COMPILER_REPLACE_SECRETS__
+const secrets = __COMPILER_REPLACE_SECRETS__
 
 export async function getSecret(name: SecretKeyType, _config?: ConfigType) {
   // First, check if this secret has been added to env
-  if (secrets[name]) {
+  if (secrets?.[name]) {
     return secrets[name];
   }
   throw new SecretNotFoundError(name);
