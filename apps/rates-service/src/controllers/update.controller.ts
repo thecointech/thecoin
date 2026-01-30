e5a36a847b0e83f1a2e020caeed29c1172365e38
FrozenKiwi <stephen.taylor.dev@gmail.com>
2026-01-30 11:29:03 -0600

    fix: added RequestId logging & Async context logging

--- apps/rates-service/src/controllers/update.controller.ts
+++ apps/rates-service/src/controllers/update.controller.ts
@@ -1,32 +1,32 @@
 import { Controller, Get, Route, Response, Tags } from '@tsoa/runtime';
-import { updateRates }  from '../internals/rates';
+import { updateRates } from '../internals/rates';
 
 @Route('')
 @Tags('Updates')
 export class UpdateController extends Controller {
 
-    /**
-     * Get rates.
-     *
-     **/
-    @Get('doUpdate')
-    @Response('204', 'Success')
-    @Response('500', 'unknown exception')
-    async doUpdate() {
-      try {
-          return await updateRates();
-      } catch (e) {
-          console.error("Details fetch failed: " + JSON.stringify(e));
-          throw e;
-      }
+  /**
+   * Get rates.
+   *
+   **/
+  @Get('doUpdate')
+  @Response('204', 'Success')
+  @Response('500', 'unknown exception')
+  async doUpdate() {
+    try {
+      return await updateRates();
+    } catch (e) {
+      console.error("Details fetch failed: " + JSON.stringify(e));
+      throw e;
     }
+  }
 
-    /**
-    * Test exception emailing
-    */
-    @Get('keepAlive')
-    @Response('204', 'Success')
-    async keepAlive() {
-      return "Ok";
-    }
+  /**
+  * Test exception emailing
+  */
+  @Get('keepAlive')
+  @Response('204', 'Success')
+  async keepAlive() {
+    return "Ok";
+  }
 }
