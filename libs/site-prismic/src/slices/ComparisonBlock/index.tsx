bf6bf6ae4718c0a989fa85972e0f207d09b2bc69
FrozenKiwi <stephen.taylor.dev@gmail.com>
2026-07-22 15:21:50 -0500

    Simple mocked component

--- libs/site-prismic/src/slices/ComparisonBlock/index.tsx
+++ libs/site-prismic/src/slices/ComparisonBlock/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import styles from "./index.module.css";
+import { RichText } from "@/components";
 
 /**
  * Props for `ComparisonBlock`.
@@ -14,17 +16,27 @@ export type ComparisonBlockProps =
 const ComparisonBlock: FC<ComparisonBlockProps> = ({ slice }) => {
   return (
     <section
+      className={styles.comparisonBlock}
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for comparison_block (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <div className={styles.header}>
+        <span>{slice.primary.titleleft}</span>
+        <span>- vs -</span>
+        <span>{slice.primary.titleright}</span>
+      </div>
+      {
+        slice.primary.elements.map((item, index) => (
+          <div key={index} className={styles.element}>
+            <span className={styles.left}>
+              <RichText field={item.leftelement} />
+            </span>
+            <span className={styles.right}>
+              <RichText field={item.rightelement} />
+            </span>
+          </div>
+        ))
+      }
     </section>
   );
 };
