eb07b2bb98bd8124ce049d9749f57d359f63972b
FrozenKiwi <stephen.taylor.dev@gmail.com>
2026-09-21 16:48:05 -0500

    Update harvester so it can update after external changes occur.

--- apps/harvester/src/Harvester/initialize.ts
+++ apps/harvester/src/Harvester/initialize.ts
@@ -4,8 +4,10 @@ import { getCurrentState } from './state';
 import { getAccountData } from './fetchData';
 import type { HarvestData, UserData } from './types';
 import type { HarvesterReplayCallbacks } from './replay/replayCallbacks';
-import { ContractCore } from '@thecointech/contract-core';
+import { ContractCore, type TheCoin } from '@thecointech/contract-core';
 import { useSigner } from './signer';
+import { reconcileDemoStateIfNeeded } from './initialize.prodtest';
+
 
 export async function initialize(callback: HarvesterReplayCallbacks) {
 
@@ -40,6 +42,7 @@ export async function initialize(callback: HarvesterReplayCallbacks) {
   const { chq, visa } = await getAccountData(callback, lastTxDate);
   const tcCore = await ContractCore.get();
   const coin = await tcCore.balanceOf(address);
+
   let state: HarvestData = {
     chq,
     visa,
@@ -47,8 +50,19 @@ export async function initialize(callback: HarvesterReplayCallbacks) {
     date: DateTime.now(),
 
     delta: [],
-    state: lastRun?.state ?? {},
+    state: await getState(address, lastRun, tcCore),
   }
 
   return { stages, state, user };
+}
+
+async function getState(
+  address: string,
+  lastRun: HarvestData|undefined,
+  tcCore: TheCoin,
+): Promise<HarvestData['state']> {
+  if (process.env.CONFIG_NAME === 'prodtest' && address.toLowerCase() === process.env.WALLET_TestDemoAccount_ADDRESS?.toLowerCase()) {
+    return reconcileDemoStateIfNeeded(lastRun, address, tcCore);
+  }
+  return lastRun?.state ?? {};
 }
